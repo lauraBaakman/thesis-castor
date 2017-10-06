6b42cf1fd63f9aefe3ba7e2cbb4a4428a780e339
Laura@Eve <laurabaakman@gmail.com>
2017-10-06 15:51:26 +0200

    Disable the settings and load buttons.

--- castor/Assets/mainMenu/scripts/MenuController.cs
+++ castor/Assets/mainMenu/scripts/MenuController.cs
@@ -2,27 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class MenuController : MonoBehaviour
 {
 
 	public void Start ()
 	{
-		//TODO disable the settings button
-		//TODO disable the load reduction button
+		Button loadButton = GetButtonByName ("Load Fracture Reduction Button");
+		loadButton.interactable = false;
+
+		Button settingsButton = GetButtonByName ("Settings Button");
+		settingsButton.interactable = false;
+		
 		AttachListeners ();
 	}
 
 	public void AttachListeners ()
 	{
-
-		Button button;
-
-		button = findButtonByName ("New Fracture Reduction Button");
-		button.onClick.AddListener (OnNewReductionButton);
+		AttachOnClickListenerToButton ("New Fracture Reduction Button", OnNewReductionButton);
 	}
 
-	private Button findButtonByName (string name)
+	private void AttachOnClickListenerToButton (string name, UnityAction listener)
+	{
+		Button button = GetButtonByName (name);
+		button.onClick.AddListener (listener);
+	}
+
+	private Button GetButtonByName (string name)
 	{
 		GameObject gameObject = GameObject.Find (name);
 		Button button = gameObject.GetComponent<Button> ();
