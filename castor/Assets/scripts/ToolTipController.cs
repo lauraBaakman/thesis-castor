79689dd2fa8dd69672757795594a2a1d16f9221e
Laura@Eve <laurabaakman@gmail.com>
2017-10-06 16:10:14 +0200

    Detect entering and exiting the UI element.

---
+++ castor/Assets/scripts/ToolTipController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ToolTipController : MonoBehaviour, 
+	IPointerEnterHandler, 
+	IPointerExitHandler
+{
+
+	public string tooltiptext;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	public void OnPointerEnter (PointerEventData data)
+	{
+		Debug.Log ("Enter");
+	}
+
+	public void OnPointerExit (PointerEventData data)
+	{
+		Debug.Log ("Exit");
+	}
+}
