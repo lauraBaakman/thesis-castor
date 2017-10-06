6e89f75af50328f9600e58106fa0b7f4edc77724
Laura@Eve <laurabaakman@gmail.com>
2017-10-06 15:41:23 +0200

    Attach a listener to the new reduction scene button.

---
+++ castor/Assets/mainMenu/scripts/MenuController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+
+	public void Start ()
+	{
+		//TODO disable the settings button
+		//TODO disable the load reduction button
+		AttachListeners ();
+	}
+
+	public void AttachListeners ()
+	{
+
+		Button button;
+
+		button = findButtonByName ("New Fracture Reduction Button");
+		button.onClick.AddListener (OnNewReductionButton);
+	}
+
+	private Button findButtonByName (string name)
+	{
+		GameObject gameObject = GameObject.Find (name);
+		Button button = gameObject.GetComponent<Button> ();
+		return button;
+	}
+
+	public void OnNewReductionButton ()
+	{
+		Debug.Log ("Button Pressed!");
+	}
+}
