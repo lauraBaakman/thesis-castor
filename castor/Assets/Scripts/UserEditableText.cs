904006c041ec02788fee17aa507466dc4644f978
Laura Baakman <laurabaakman@gmail.com>
2017-10-23 13:01:51 +0200

    Move the editable stuff to its own behavior.

---
+++ castor/Assets/Scripts/UserEditableText.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class UserEditableText : MonoBehaviour
+{
+
+	public InputField field;
+	private Text text;
+
+	void Start ()
+	{
+		text = GetComponent<Text> ();
+		field.text = text.text;
+
+		toggleEditMode (false);
+
+		setUpEventTriggers ();
+	}
+
+	void Update ()
+	{
+		if (field.isFocused) {
+			Debug.Log ("Field has focus!");
+		}
+	}
+
+	private void setUpEventTriggers ()
+	{
+		setUpTextEventTriggers ();
+		setUpFieldEventTriggers ();
+	}
+
+	private void setUpTextEventTriggers ()
+	{
+		//Add Event Trigger to the game object
+		EventTrigger eventTrigger = text.gameObject.AddComponent (typeof(EventTrigger)) as EventTrigger;
+		
+		
+		EventTrigger.TriggerEvent triggerEvent = new EventTrigger.TriggerEvent ();
+		triggerEvent.AddListener ((PointerEventData) => onPointerClick ());
+		EventTrigger.Entry entry = new EventTrigger.Entry () {
+			callback = triggerEvent, eventID = EventTriggerType.PointerClick
+		};
+		eventTrigger.triggers.Add (entry);
+	}
+
+	private void setUpFieldEventTriggers ()
+	{
+		InputField.SubmitEvent submitEvent = new InputField.SubmitEvent ();
+		submitEvent.AddListener (onSubmit);
+		field.onEndEdit = submitEvent;
+	}
+
+	public void onPointerClick ()
+	{
+		enterEditMode ();
+	}
+
+	public void onSubmit (string inputText)
+	{
+		Debug.Log ("On Submit!");
+	}
+
+	public void toggleEditMode (bool toggle)
+	{
+		text.gameObject.SetActive (!toggle);
+		field.gameObject.SetActive (toggle);
+	}
+
+	private void enterEditMode ()
+	{
+		toggleEditMode (true);
+		field.Select ();
+		field.ActivateInputField ();
+	}
+
+	private void exitEditMode ()
+	{
+		toggleEditMode (false);
+		text.text = field.text;
+	}
+}
--- castor/Assets/Scripts/sidemenu/FragmentListElementController.cs
+++ castor/Assets/Scripts/sidemenu/FragmentListElementController.cs
@@ -7,16 +7,8 @@ using UnityEngine.EventSystems;
 
 public class FragmentListElementController : MonoBehaviour
 {
-
-	private EditableTextField description;
-
 	void Start ()
 	{
-		description = new EditableTextField (
-			text: transform.Find ("Description").GetComponent<Text> (), 
-			input: transform.Find ("Description Input").GetComponent<InputField> ()
-		);
-		description.toggleEditMode (false);
 	}
 
 	public void onDeleteFragmentClick ()
@@ -33,63 +25,4 @@ public class FragmentListElementController : MonoBehaviour
 	{
 		Debug.Log ("Pressed the toggle fragment checkmark");
 	}
-}
-
-public class EditableTextField
-{
-	private Text text;
-	private InputField input;
-
-	public EditableTextField (Text text, InputField input)
-	{
-		this.text = text;
-		this.input = input;
-
-		setUpEventTriggers ();
-	}
-
-	private void setUpEventTriggers ()
-	{
-		setUpTextEventTriggers ();
-	}
-
-	private void setUpTextEventTriggers ()
-	{
-		//Add Event Trigger to the game object
-		EventTrigger eventTrigger = text.gameObject.AddComponent (typeof(EventTrigger)) as EventTrigger;
-
-
-		EventTrigger.TriggerEvent triggerEvent = new EventTrigger.TriggerEvent ();
-		triggerEvent.AddListener ((PointerEventData) => onPointerClick ());
-		EventTrigger.Entry entry = new EventTrigger.Entry () {
-			callback = triggerEvent, eventID = EventTriggerType.PointerClick
-		};
-		eventTrigger.triggers.Add (entry);				
-	}
-
-	public void toggleEditMode (bool toggle)
-	{
-		text.gameObject.SetActive (!toggle);
-		input.gameObject.SetActive (toggle);
-	}
-
-	public void onPointerClick ()
-	{
-		enterEditMode ();
-	}
-
-	private void enterEditMode ()
-	{
-		toggleEditMode (true);
-	}
-
-	private void exitEditMode ()
-	{
-		toggleEditMode (false);
-		text.text = input.text;
-	}
-
-	//Levae edit mode upon:
-	// tab
-	// Enter
 }
