47bb8d75db75cddeb706b591b429f21a6777cd3e
Laura Baakman <laurabaakman@gmail.com>
2017-10-23 09:27:41 +0200

    Implemented the home button.

--- castor/Assets/Scripts/sidemenu/FileMenuBoxController.cs
+++ castor/Assets/Scripts/sidemenu/FileMenuBoxController.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FileMenuBoxController : MonoBehaviour {
 
 	public void onHomeClick(){
-		Debug.Log ("Home Button Clicked");
+		SceneManager.LoadScene ("mainMenu");
 	}
 
 	public void onSaveClick(){
