e7d8e77e2d99c15f7e7579a9b598328a572ca1b3
Laura Baakman <laurabaakman@gmail.com>
2017-10-23 11:16:00 +0200

    Stub for the add fragment button.

---
+++ castor/Assets/Scripts/sidemenu/FragmentsMenuBoxController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FragmentsMenuBoxController : MonoBehaviour {
+
+	public void onAddFragmentClick(){
+		Debug.Log ("Pressed the add fragment button.");
+	}
+}
