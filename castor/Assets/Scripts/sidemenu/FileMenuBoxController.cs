df58fcc876dd3a2538d19194b7108df3709f5bd8
Laura Baakman <laurabaakman@gmail.com>
2017-10-23 09:24:35 +0200

    Attatched dummy functions to the scripts in the file box of the side menu.

---
+++ castor/Assets/Scripts/sidemenu/FileMenuBoxController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FileMenuBoxController : MonoBehaviour {
+
+	public void onHomeClick(){
+		Debug.Log ("Home Button Clicked");
+	}
+
+	public void onSaveClick(){
+		Debug.Log ("Save Button Clicked");
+	}
+
+	public void onQuitClick(){
+		Debug.Log ("Quit Button Clicked");
+	}
+}
