2e009dab405bdb413b9bb18c7d8ba097371c56ac
Laura Baakman <laurabaakman@gmail.com>
2017-10-23 17:10:32 +0200

    The implementation for the fracture fragment gives an error, lets leave it empty for now.

--- castor/Assets/Scripts/FractureFragments.cs
+++ castor/Assets/Scripts/FractureFragments.cs
@@ -1,19 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 
-//source: https://msdn.microsoft.com/en-us/library/ff650316.aspx
-
 public sealed class FractureFragments
 {
-	private static readonly FractureFragments instance = FractureFragments ();
-
-	private FractureFragments ()
-	{
-	}
-
-	public static FractureFragments intance {
-		get {
-			return instance; 
-		}
-	}
 }
