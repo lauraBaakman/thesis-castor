9c533d72a3ade1578f44dc89b428bb202e2ab424
Laura Baakman <laurabaakman@gmail.com>
2017-10-23 11:46:47 +0200

    Create an editable text element.

--- castor/Assets/Scripts/sidemenu/FragmentListElementController.cs
+++ castor/Assets/Scripts/sidemenu/FragmentListElementController.cs
@@ -1,8 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FragmentListElementController : MonoBehaviour {
+
+	private EditableTextField description;
+
+	void Start (){
+		description = new EditableTextField (
+			text: transform.Find ("Description").GetComponent<Text>(), 
+			input: transform.Find ("Description Input").GetComponent<InputField>()
+		);
+		description.toggleEditMode (false);
+	}
 
 	public void onDeleteFragmentClick(){
 		Debug.Log ("Pressed the delete fragment button.");
@@ -15,4 +26,31 @@ public class FragmentListElementController : MonoBehaviour {
 	public void onToggleFragmentValueChanged(bool toggle){
 		Debug.Log ("Pressed the toggle fragment checkmark");
 	}
+}
+
+public class EditableTextField {
+	private Text text;
+	private InputField input;		
+
+	public EditableTextField(Text text, InputField input){
+		this.text = text;
+		this.input = input;
+	}
+
+	public void toggleEditMode(bool toggle){
+		text.gameObject.SetActive (!toggle);
+		input.gameObject.SetActive (toggle);
+	}
+
+	//Hide the input field
+
+	//Onclick of the text field: 
+	// hide the text field, 
+	// show the input field
+
+
+	//On leaving the input field:
+	// hide the input field
+	// store the text in the input field in the text field
+	// show the text field
 }
