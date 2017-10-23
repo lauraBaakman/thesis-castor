5b9617429bac063cedb08fa81b275e5781ffe81c
Laura Baakman <laurabaakman@gmail.com>
2017-10-23 11:22:23 +0200

    Prefab for the fragment list element with stubs for the buttons.

---
+++ castor/Assets/Scripts/sidemenu/FragmentListElementController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FragmentListElementController : MonoBehaviour {
+
+	public void onDeleteFragmentClick(){
+		Debug.Log ("Pressed the delete fragment button.");
+	}
+
+	public void onChangeColorFragmentClick(){
+		Debug.Log ("Pressed the change color fragment button.");
+	}
+
+	public void onToggleFragmentValueChanged(bool toggle){
+		Debug.Log ("Pressed the toggle fragment checkmark");
+	}
+}
