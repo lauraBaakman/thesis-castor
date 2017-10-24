756cf14279ab63400c0d73266890ba01ae5db77b
Laura Baakman <laurabaakman@gmail.com>
2017-10-24 14:33:33 +0200

    Set up fragments singleton.

--- castor/Assets/Scripts/data/FractureFragments.cs
+++ castor/Assets/Scripts/data/FractureFragments.cs
@@ -3,4 +3,32 @@ using System.Collections.Generic;
 
 public sealed class FractureFragments
 {
+	private static readonly FractureFragments instance = new FractureFragments ();
+
+	private FractureFragments ()
+	{
+		//The Constructor
+	}
+
+	public static FractureFragments Instance {
+		get {
+			return instance;
+		}
+	}
 }
+
+
+//public sealed class Singleton
+//{
+//	private static readonly Singleton instance = new Singleton();
+//
+//	private Singleton(){}
+//
+//	public static Singleton Instance
+//	{
+//		get
+//		{
+//			return instance;
+//		}
+//	}
+//}
