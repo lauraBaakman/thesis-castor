bc7574c089b36fb640c541feee7507ccecdc77e9
Laura Baakman <laurabaakman@gmail.com>
2017-12-13 16:51:09 +0100

    Formattin.

--- castor/Assets/Scripts/sidemenu/FragmentListElementController.cs
+++ castor/Assets/Scripts/sidemenu/FragmentListElementController.cs
@@ -7,77 +7,81 @@ using UnityEngine.EventSystems;
 
 public class FragmentListElementController : MonoBehaviour
 {
-	/// <summary>
-	/// The UI element that displays the name of the fragment.
-	/// </summary>
-	public Text FragmentNameText;
+    /// <summary>
+    /// The UI element that displays the name of the fragment.
+    /// </summary>
+    public Text FragmentNameText;
 
-	private FragmentController FragmentController;
+    private FragmentController FragmentController;
 
-	void Start ()
-	{
-	}
+    void Start()
+    {
+    }
 
-	/// <summary>
-	/// Populate the properties of the fragmentController.
-	/// </summary>
-	/// <param name="fragmentController">Fragment controller.</param>
-	public void Populate (FragmentController fragmentController){
-		this.FragmentController = fragmentController;
-	}
+    /// <summary>
+    /// Populate the properties of the fragmentController.
+    /// </summary>
+    /// <param name="fragmentController">Fragment controller.</param>
+    public void Populate(FragmentController fragmentController)
+    {
+        this.FragmentController = fragmentController;
+    }
 
-	/// <summary>
-	/// On the delete fragment button click the fragment is deleted from <Fragments cref="Fragments">, the 3D view and the list view.
-	/// </summary>
-	public void OnDeleteFragmentClick ()
-	{
-		Debug.Log ("Pressed the delete fragment button.");
-		FragmentController.DeleteFragment ();
-	}
+    /// <summary>
+    /// On the delete fragment button click the fragment is deleted from <Fragments cref="Fragments">, the 3D view and the list view.
+    /// </summary>
+    public void OnDeleteFragmentClick()
+    {
+        Debug.Log("Pressed the delete fragment button.");
+        FragmentController.DeleteFragment();
+    }
 
-	/// <summary>
-	/// On the change fragment color button click the user should be able to change the color of the fragment.
-	/// </summary>
-	public void OnChangeColorFragmentClick ()
-	{
-		Debug.Log ("Pressed the change color fragment button.");
-	}
+    /// <summary>
+    /// On the change fragment color button click the user should be able to change the color of the fragment.
+    /// </summary>
+    public void OnChangeColorFragmentClick()
+    {
+        Debug.Log("Pressed the change color fragment button.");
+    }
 
-	/// <summary>
-	/// Ons the toggle fragment value changed, the fragment should be shown or hidden depending on its previous state.
-	/// </summary>
-	/// <param name="toggle">If set to <c>true</c> the fragment should be shown.</param>
-	public void OnToggleVisibility (bool toggle)
-	{
-		Debug.Log ("Pressed the toggle visibility checkmark");
-	}
+    /// <summary>
+    /// Ons the toggle fragment value changed, the fragment should be shown or hidden depending on its previous state.
+    /// </summary>
+    /// <param name="toggle">If set to <c>true</c> the fragment should be shown.</param>
+    public void OnToggleVisibility(bool toggle)
+    {
+        Debug.Log("Pressed the toggle visibility checkmark, toggle: " + toggle);
+    }
 
-	/// <summary>
-	/// Toggle the selection of the associated fragment.
-	/// </summary>
-	/// <param name="toggle">If set to <c>true</c> the object is marked as selected.</param>
-	public void OnToggleSelected(bool toggle){
-		Debug.Log ("Pressed the toggle selection checkmark");
-		FragmentController.ToggleSelection (toggle);
-	}
+    /// <summary>
+    /// Toggle the selection of the associated fragment.
+    /// </summary>
+    /// <param name="toggle">If set to <c>true</c> the object is marked as selected.</param>
+    public void OnToggleSelected(bool toggle)
+    {
+        Debug.Log("Pressed the toggle selection checkmark, toggle: " + toggle);
+        FragmentController.ToggleSelection(toggle);
+    }
 
-	/// <summary>
-	/// Change the name of the fragment.
-	/// </summary>
-	/// <param name="name">the new name of the fragment.</param>
-	public void ChangeFragmentName (string newName)
-	{
-		this.FragmentNameText.text = newName;
-	}
+    /// <summary>
+    /// Change the name of the fragment.
+    /// </summary>
+    /// <param name="name">the new name of the fragment.</param>
+    public void ChangeFragmentName(string newName)
+    {
+        this.FragmentNameText.text = newName;
+    }
 
-	/// <summary>
-	/// Delete the GameObject associated with this controller.
-	/// </summary>
-	public void Delete(){
-		Destroy (gameObject);
-	}
+    /// <summary>
+    /// Delete the GameObject associated with this controller.
+    /// </summary>
+    public void Delete()
+    {
+        Destroy(gameObject);
+    }
 
-	public void Deselect(){
-		Debug.Log("FragmentListElementController: The fragment with name " + FragmentController.Fragment.Name + " has been deselected.");		
-	}
+    public void Deselect()
+    {
+        Debug.Log("FragmentListElementController: The fragment with name " + FragmentController.Fragment.Name + " has been deselected.");
+    }
 }
