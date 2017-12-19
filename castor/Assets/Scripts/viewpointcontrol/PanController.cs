a43c6121c5d5ae688f7efce51c11582cc576d659
Laura Baakman <laurabaakman@gmail.com>
2017-12-19 14:36:19 +0100

    Set up the Pan Controller.

---
+++ castor/Assets/Scripts/viewpointcontrol/PanController.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System.Collections;
+
+public class PanController : MonoBehaviour
+{
+    private static string KeyboardAxisName = "Keyboard Pan";
+    private static string MouseAxisName = "Mouse Pan";
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if(DetectKeyboardPan()){
+            HandleKeyBoardPan();
+        }
+
+        if(DetectMousePan()){
+            HandleMousePan();
+        }
+
+    }
+
+    private void DetectKeyboardPan(){
+        if (Input.GetButton(KeyboardAxisName)){
+            Debug.Log("Detected Keyboard Pan");
+        }
+    }
+
+    private void HandleKeyBoardPan(){
+        Debug.Log("Handled Keyboard Pan");
+    }
+
+    private void DetectMousePan(){
+        Debug.Log("Detected Mouse Pan");
+    }
+
+    private void HandleMousePan()
+    {
+        Debug.Log("Handled Mouse Pan");
+    }
+}
