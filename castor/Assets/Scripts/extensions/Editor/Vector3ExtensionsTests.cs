f3ed8f279a9c8906ce0b5fd1c5c722dffbdc19dc
Laura Baakman <laurabaakman@gmail.com>
2017-12-19 12:40:39 +0100

    More vector extensions with tests.

--- castor/Assets/Scripts/extensions/Editor/Vector3ExtensionsTests.cs
+++ castor/Assets/Scripts/extensions/Editor/Vector3ExtensionsTests.cs
@@ -6,12 +6,45 @@ public class Vector3ExtensionsTests
 {
 
     [Test]
-    public void Clamp_xyOutOfRange()
+    public void Clamped_xyOutOfRange()
     {
         Vector3 vector = new Vector3(-1.0f, 0.5f, 2.0f);
 
         Vector3 expected = new Vector3(0.0f, 0.5f, 1.0f);
-        vector.Clamp(0.0f, 1.0f);
+
+        Vector3 actual = vector.Clamped(0.0f, 1.0f);
+
+        Assert.That(actual.x, Is.EqualTo(expected.x).Within(0.000001));
+        Assert.That(actual.y, Is.EqualTo(expected.y).Within(0.000001));
+        Assert.That(actual.z, Is.EqualTo(expected.z).Within(0.000001));
+
+        Assert.That(vector.x, Is.EqualTo(-1.0f).Within(0.000001));
+        Assert.That(vector.y, Is.EqualTo(+0.5f).Within(0.000001));
+        Assert.That(vector.z, Is.EqualTo(+2.0f).Within(0.000001));
+    }
+
+    [Test]
+    public void Fill_zeroVector()
+    {
+        float value = 3.0f;
+        Vector3 vector = new Vector3().Fill(value);
+
+        Vector3 expected = new Vector3(value, value, value);
+
+
+        Assert.That(vector.x, Is.EqualTo(expected.x).Within(0.000001));
+        Assert.That(vector.y, Is.EqualTo(expected.y).Within(0.000001));
+        Assert.That(vector.z, Is.EqualTo(expected.z).Within(0.000001));
+    }
+
+    [Test]
+    public void Fill_nonZeroVector()
+    {
+        float value = 3.0f;
+        Vector3 vector = new Vector3(-1.0f, 0.5f, 2.0f).Fill(value);
+
+        Vector3 expected = new Vector3(value, value, value);
+
 
         Assert.That(vector.x, Is.EqualTo(expected.x).Within(0.000001));
         Assert.That(vector.y, Is.EqualTo(expected.y).Within(0.000001));
--- castor/Assets/Scripts/extensions/Vector3Extensions.cs
+++ castor/Assets/Scripts/extensions/Vector3Extensions.cs
@@ -2,26 +2,39 @@ using UnityEngine;
 
 public static class Vector3Extensions
 {
-    /// <summary>
-    /// Limit the values of this to the inclusive range 
-    /// <paramref name="min"/>, <paramref name = "max" /> ,
-    /// <remark>Note that this function will change the current vector. </remark>
-    /// </summary>
-    /// <param name="vector">Vector to clamp.</param>
-    /// <param name="min">Minimum of the the inclusive clamp range.</param>
-    /// <param name="max">Maximum of the inclusive clamp range.</param>
-    /// <returns>Returns zero.</returns>
-    public static Vector3 Clamp(this Vector3 vector, float min, float max)
-    {
-        vector.x = ClampFloat(vector.x, min, max);
-        vector.y = ClampFloat(vector.y, min, max);
-        vector.z = ClampFloat(vector.z, min, max);
-        return vector;
-    }
-
     private static float ClampFloat(float value, float min, float max){
         value = value < min ? min : value;
         value = value > max ? max : value;
         return value;
+    }
+
+    /// <summary>
+    /// Limit the values of this to the inclusive range 
+    /// <paramref name="min"/>, <paramref name = "max" /> ,
+    /// <remark>Note that this function creates a new clamped vector and does not change the current vector. </remark>
+    /// </summary>
+    /// <param name="min">Minimum of the the inclusive clamp range.</param>
+    /// <param name="max">Maximum of the inclusive clamp range.</param>
+    /// <returns>Returns the clamped vector.</returns>
+    public static Vector3 Clamped(this Vector3 vector, float min, float max){
+        Vector3 clampedVector = new Vector3(
+            ClampFloat(vector.x, min, max),
+            ClampFloat(vector.y, min, max),
+            ClampFloat(vector.z, min, max)
+        );
+        return clampedVector;
+    }
+
+    /// <summary>
+    /// Place <paramref name="value"/> at all vector elements.
+    /// <remark>Note that this function will change the current vector. </remark>
+    /// </summary>
+    /// <param name="value">The value that should fill the vector.</param>
+    /// <returns>Returns the filled vector.</returns>
+    public static Vector3 Fill(this Vector3 vector, float value){
+        vector.x = value;
+        vector.y = value;
+        vector.z = value;
+        return vector;        
     }
 }
