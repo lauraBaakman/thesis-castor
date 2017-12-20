4b443b73fa6b6076159b0e643e32ce2352ea6211
Laura Baakman <laurabaakman@gmail.com>
2017-12-20 14:47:05 +0100

    Extend transform to reset local transform.

---
+++ castor/Assets/Scripts/extensions/TransformExtensions.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+
+public static class TransformExtensions{
+
+    /// <summary>
+    /// Reset the local transform, i.e. localscale to one, local position to zero,
+    /// and local rotation to the identity matrix.
+    /// </summary>
+    public static void ResetLocally(this Transform transform){
+        transform.localScale = Vector3.one;
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+    }
+}
