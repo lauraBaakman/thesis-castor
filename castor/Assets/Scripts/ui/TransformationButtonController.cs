dd619c4052d0b2feaa98e19ad884efa109499795
Laura Baakman <laurabaakman@gmail.com>
2017-12-21 11:50:33 +0100

    Added a button to enable rotation mode.

---
+++ castor/Assets/Scripts/ui/TransformationButtonController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TransformationButtonController : MonoBehaviour
+{
+
+    private ColorBlock ColorBlock;
+    private Image Image;
+
+    private void Awake()
+    {
+        Toggle toggle = GetComponent<Toggle>();
+        ColorBlock = toggle.colors;
+
+        Image = GetComponent<Image>();
+    }
+
+    public void OnClick(bool clicked)
+    {
+        Image.color = clicked ? ColorBlock.pressedColor : ColorBlock.normalColor;
+    }
+}
