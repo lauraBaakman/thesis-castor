05c62859386a2b463e3e22aa78fad447a0fc443a
Laura Baakman <laurabaakman@gmail.com>
2017-12-21 14:19:48 +0100

    Detect a click on the outer circle.

---
+++ castor/Assets/Scripts/ui/widgets/rotation/ViewVectorRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewVectorRotation : MonoBehaviour {
+
+    void Start () {
+        MeshCollider meshCollider = gameObject.AddComponent<MeshCollider>();
+	}
+	
+	void Update () {
+		
+	}
+
+    public void OnMouseDown()
+    {
+        Debug.Log("Detected Mouse Click!");
+    }
+}
