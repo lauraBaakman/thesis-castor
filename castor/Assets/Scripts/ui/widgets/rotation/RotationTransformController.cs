1be9e969c063fbdbafe70ac552b439352eb9607c
Laura Baakman <laurabaakman@gmail.com>
2018-01-09 10:37:31 +0100

    Some renaming.

--- castor/Assets/Scripts/ui/widgets/rotation/RotationTransformController.cs
+++ castor/Assets/Scripts/ui/widgets/rotation/RotationTransformController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SizeController : MonoBehaviour {
+public class RotationTransformController : MonoBehaviour {
 
     public GameObject ObjectControlledByWidget;
 
