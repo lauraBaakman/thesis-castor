87d275b8a23824edd70aa5198d0a26950ddc4b33
Laura Baakman <laurabaakman@gmail.com>
2018-01-09 10:24:44 +0100

    Documented function.

--- castor/Assets/Scripts/extensions/GameObjectExtensions.cs
+++ castor/Assets/Scripts/extensions/GameObjectExtensions.cs
@@ -17,34 +17,41 @@ public static class GameObjectExtensions
 		return go.AddComponent<T> ().GetCopyOf (toAdd) as T;
 	}
 
-    //Source: https://forum.unity.com/threads/bounds-of-a-whole-hierarchy.4525/#post-1276595
-    public static Bounds Bounds(this GameObject go){
-        Bounds bounds = GetInitialBounds(go);
-        bounds = AddChildrenToBounds(go.transform, bounds);
-        return bounds;
-    }
+	//Source: https://forum.unity.com/threads/bounds-of-a-whole-hierarchy.4525/#post-1276595
+	/// <summary>
+	/// Compute the bounds the specified gameobject based on the MeshRenderers attatched to it 
+	/// and its children.
+	/// </summary>
+	/// <param name="go">The gameobject of which the bounds need to be compuated.</param>
+	public static Bounds Bounds (this GameObject go)
+	{
+		Bounds bounds = GetInitialBounds (go);
+		bounds = AddChildrenToBounds (go.transform, bounds);
+		return bounds;
+	}
 
-    private static Bounds AddChildrenToBounds(Transform child, Bounds bounds){
-        MeshRenderer renderer;
-        foreach (Transform grandChild in child){
-            renderer = grandChild.GetComponent<MeshRenderer>();
-            if(renderer){
-                bounds.Encapsulate(renderer.bounds.min);    
-                bounds.Encapsulate(renderer.bounds.max);    
-            }
+	private static Bounds AddChildrenToBounds (Transform child, Bounds bounds)
+	{
+		MeshRenderer renderer;
+		foreach (Transform grandChild in child) {
+			renderer = grandChild.GetComponent<MeshRenderer> ();
+			if (renderer) {
+				bounds.Encapsulate (renderer.bounds.min);    
+				bounds.Encapsulate (renderer.bounds.max);    
+			}
 
-            bounds = AddChildrenToBounds(grandChild, bounds);
-        }
-        return bounds;
-    }
+			bounds = AddChildrenToBounds (grandChild, bounds);
+		}
+		return bounds;
+	}
 
-    private static Bounds GetInitialBounds(GameObject go){
-        MeshRenderer renderer = go.GetComponent<MeshRenderer>();
-        if (!renderer)
-        {
-            renderer = go.GetComponentInChildren<MeshRenderer>();
-        }
-        return renderer.bounds;
-    }
+	private static Bounds GetInitialBounds (GameObject go)
+	{
+		MeshRenderer renderer = go.GetComponent<MeshRenderer> ();
+		if (!renderer) {
+			renderer = go.GetComponentInChildren<MeshRenderer> ();
+		}
+		return renderer.bounds;
+	}
 }
 
