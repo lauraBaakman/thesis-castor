2594b3806caa13830767cd2971f3f659536850d6
Laura Baakman <laurabaakman@gmail.com>
2018-01-11 11:48:47 +0100

    Default state of the translation toggle is not-interactable.

--- castor/Assets/Scripts/ui/ObjectTranslationController.cs
+++ castor/Assets/Scripts/ui/ObjectTranslationController.cs
@@ -1,8 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ObjectTranslationController : TransformController {
+
+    public Toggle TranslationToggle;
+
+    public void Start()
+    {
+        TranslationToggle.interactable = false;
+    }
 
     public override void ToggleActivity(bool toggle)
     {
