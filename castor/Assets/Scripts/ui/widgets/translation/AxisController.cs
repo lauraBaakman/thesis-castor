c4d13b0ef2d7d60f6c2833c6ca9b7bb543d24e14
Laura Baakman <laurabaakman@gmail.com>
2018-01-11 16:15:07 +0100

    Implementation of interaction with the translation widget.

--- castor/Assets/Scripts/ui/widgets/translation/AxisController.cs
+++ castor/Assets/Scripts/ui/widgets/translation/AxisController.cs
@@ -3,24 +3,51 @@
 [RequireComponent(typeof(MeshRenderer))]
 public class AxisController : MonoBehaviour {
 
+    //All stored positions are in world space
+
     public Material SelectedMaterial;
     private Material NormalMaterial;
+
+    public GameObject TranslatedObject;
 
     private bool InAxisTranslationMode = false;
 
     private MeshRenderer MeshRenderer;
 
-	void Start () {
+    private static string VerticalMouseAxis = "Mouse X";
+    private static string HorizontalMouseAxis = "Mouse Y";
+
+    private Vector3 InitialPosition;
+
+	void Awake () {
         MeshRenderer = gameObject.GetComponent<MeshRenderer>();
         NormalMaterial = MeshRenderer.material;
 	}
 	
 	void Update () {
-		
+        if (InAxisTranslationMode && CancelButtonPressed()) CancelTranslation();
+        if (InAxisTranslationMode && MouseMoved()) Translate();
 	}
+
+    private bool CancelButtonPressed()
+    {
+        return Input.GetButton("Cancel");
+    }
+
+    private bool MouseMoved()
+    {
+        bool mouseMoved = (
+            !Input.GetAxis(VerticalMouseAxis).Equals(0.0f) ||
+            !Input.GetAxis(HorizontalMouseAxis).Equals(0.0f)
+        );
+        return mouseMoved;
+    }
 
     private void OnMouseDown()
     {
+        if(!InAxisTranslationMode){
+            InitialPosition = TranslatedObject.transform.position;
+        }
         ToggleAxisTranslationMode(!InAxisTranslationMode);   
     }
 
@@ -32,5 +59,15 @@ public class AxisController : MonoBehaviour {
     private void OnEnable()
     {
         ToggleAxisTranslationMode(false);
+    }
+
+    private void CancelTranslation(){
+        Debug.Log("Cancel Translation!");
+        TranslatedObject.transform.position = InitialPosition;
+        ToggleAxisTranslationMode(false);
+    }
+
+    private void Translate(){
+        Debug.Log("Translating!");
     }
 }
