04d7c54518f1eddf9bceb4012d00aff0530c6383
Laura Baakman <laurabaakman@gmail.com>
2018-01-11 16:01:54 +0100

    Detect clicks on the axes.

---
+++ castor/Assets/Scripts/ui/widgets/translation/AxisController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class AxisController : MonoBehaviour {
+
+    public Material SelectedMaterial;
+    private Material NormalMaterial;
+
+    private bool InAxisTranslationMode = false;
+
+    private MeshRenderer MeshRenderer;
+
+	void Start () {
+        MeshRenderer = gameObject.GetComponent<MeshRenderer>();
+        NormalMaterial = MeshRenderer.material;
+	}
+	
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Detected A click!");
+    }
+
+    private void EnterAxisTranslationMode(){
+        InAxisTranslationMode = true;
+        MeshRenderer.material = SelectedMaterial;
+    }
+
+    private void ExitAxisTranslationMode(){
+        InAxisTranslationMode = false;
+        MeshRenderer.material = NormalMaterial;
+    }
+
+    private void OnEnable()
+    {
+        ExitAxisTranslationMode();
+    }
+}
