01084734bd55d1c50849807e1a20a0e3467db0bf
Laura Baakman <laurabaakman@gmail.com>
2018-01-11 16:59:44 +0100

    Implement z scaling.

--- castor/Assets/Scripts/ui/widgets/translation/ZAxisTranslationController.cs
+++ castor/Assets/Scripts/ui/widgets/translation/ZAxisTranslationController.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ZAxisTranslationController : AbstractAxisController {
+public class ZAxisTranslationController : AbstractAxisController
+{
+
+    private static string VerticalMouseAxis = "Mouse X";
+    private static string HorizontalMouseAxis = "Mouse Y";
 
     protected override void Translate()
     {
-        throw new System.NotImplementedException();
+        float verticalSpeed = Input.GetAxis(VerticalMouseAxis);
+        float horizontalSpeed = Input.GetAxis(HorizontalMouseAxis);
+
+        float diagonalSpeed = Mathf.Sqrt(
+            verticalSpeed * verticalSpeed +
+            horizontalSpeed * horizontalSpeed
+        );
+
+        Vector3 translation = DirectionVector * diagonalSpeed;
+        TranslatedObject.transform.position += translation;
     }
 }
