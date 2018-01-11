203b2c192ac8d2dd440029f8f7cae76a3bec57e6
Laura Baakman <laurabaakman@gmail.com>
2018-01-11 11:42:56 +0100

    Added a translation Mode Button that controls some method on a component on fragments.

---
+++ castor/Assets/Scripts/ui/ObjectTranslationController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectTranslationController : TransformController {
+
+    public override void ToggleActivity(bool toggle)
+    {
+        Debug.Log("ObjectTranslationController:ToggleActivity");
+    }
+}
