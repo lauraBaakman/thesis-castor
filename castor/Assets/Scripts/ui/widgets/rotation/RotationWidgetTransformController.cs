26ec9c4278f9570b268a411cde1aa773df03cf3e
Laura Baakman <laurabaakman@gmail.com>
2018-01-11 15:30:05 +0100

    Refactor the RotationWidgetTransformController to use the abstract class.

--- castor/Assets/Scripts/ui/widgets/rotation/RotationWidgetTransformController.cs
+++ castor/Assets/Scripts/ui/widgets/rotation/RotationWidgetTransformController.cs
@@ -4,26 +4,19 @@ public class RotationWidgetTransformController : WidgetTransformController
 {
     public GameObject SizeControllingWidgetElement;
 
-    private float ComputeLocalScale(Bounds ObjectBounds)
-    {
-        Bounds widgetBounds = SizeControllingWidgetElement.Bounds();
-        Vector3 widgetSize = widgetBounds.size;
-
-        Vector3 objectSize = ObjectBounds.size;
-
-        Vector3 localScale = objectSize.DivideElementWise(widgetSize);
-
-        return localScale.Max() * ScalingFactor;
-    }
-
     protected override void FitWidgetToControlledObject()
     {
         Bounds objectBounds = ObjectControlledByWidget.Bounds();
+        Bounds widgetBounds = SizeControllingWidgetElement.Bounds();
 
-        float scalingFactor = ComputeLocalScale(objectBounds);
-        scalingFactor = Mathf.Clamp(scalingFactor, MinimumScale, MaximumScale);
+        Vector3 scale = ClampScale(
+            scale: ComputeScale(objectBounds, widgetBounds),
+            widgetTransform: transform
+        );
 
+        float uniformScale = scale.Max();
+
+        transform.localScale = new Vector3().Fill(uniformScale);
         transform.position = objectBounds.center;
-        transform.localScale = transform.localScale * scalingFactor;
     }
 }
