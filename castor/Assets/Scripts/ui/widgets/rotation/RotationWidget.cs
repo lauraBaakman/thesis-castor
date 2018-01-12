eaf3fd6ec5afe339638616153f3e8441cd8faef3
Laura Baakman <laurabaakman@gmail.com>
2018-01-12 12:01:36 +0100

    Rotation widget controller that has refernces to the children of th widget.

---
+++ castor/Assets/Scripts/ui/widgets/rotation/RotationWidget.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationWidget : MonoBehaviour {
+
+    private GameObject Donut;
+    private GameObject ClickPositionIndicator;
+    private GameObject Sphere;
+
+
+	void Start () {
+        Donut = FindGameObjectByName("OuterDonut");
+        ClickPositionIndicator = FindGameObjectByName("ViewVectorClickPositionIndicator");
+        Sphere = FindGameObjectByName("InnerSphere");
+	}
+	
+    private GameObject FindGameObjectByName(string childName){
+        Transform childTransform = gameObject.transform.Find(childName);
+        if(childTransform) return childTransform.gameObject;
+
+        Debug.LogError("Could not find the gameobject with name " + childName);
+        return null;
+    }
+
+	void Update () {
+		
+	}
+}
