ae500b794957e612b6fe83deada7791db38d0a23
Laura Baakman <laurabaakman@gmail.com>
2018-01-17 12:30:24 +0100

    Continue sending the message while hovering.

--- castor 2.0/Assets/Scripts/Help/HelpMessageSender.cs
+++ castor 2.0/Assets/Scripts/Help/HelpMessageSender.cs
@@ -1,15 +1,27 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class HelpMessageSender : MonoBehaviour, IPointerEnterHandler
+public class HelpMessageSender : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
 
     public string Message;
     public string KeyBoard;
     public GameObject Receiver;
 
+    private bool Hovering = false;
+
+    private void Update()
+    {
+        if (Hovering) SendHelpMessage();
+    }
+
     public void OnPointerEnter(PointerEventData eventData)
     {
+        Hovering = true;
+        SendHelpMessage();
+    }
+
+    private void SendHelpMessage(){
         Receiver.SendMessage(
             methodName: "OnMessage",
             value: new Ticker.Message.HelpMessage(
@@ -17,6 +29,11 @@ public class HelpMessageSender : MonoBehaviour, IPointerEnterHandler
                 keyboard: KeyBoard
             ),
             options: SendMessageOptions.RequireReceiver
-        );
+        );        
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Hovering = false;
     }
 }
