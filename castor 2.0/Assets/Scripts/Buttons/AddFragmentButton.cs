9a5ed3ae078a77892d14bd25c8859a63bf97fc40
Laura Baakman <laurabaakman@gmail.com>
2018-01-17 14:35:25 +0100

    Added an AddFragmentButton with prefab functionality.

---
+++ castor 2.0/Assets/Scripts/Buttons/AddFragmentButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Buttons{
+    public class AddFragmentButton : AbstractButton
+    {
+        public override void OnClick()
+        {
+            Debug.Log("Detected A Click");
+            AddFragment();
+        }
+
+        protected override void DetectKeyBoardShortCut()
+        {
+            Debug.Log("Detected A KeyBoard Shortcut");
+        }
+
+        private void AddFragment(){
+            Debug.Log("Time to get a fragment from a file!");
+        }
+    }    
+}
+
+
