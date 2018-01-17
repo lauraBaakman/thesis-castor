2cf238cde0e881e0c675e312cb6c1666bb521d75
Laura Baakman <laurabaakman@gmail.com>
2018-01-17 14:10:15 +0100

    Added functionality and keyboard shortcut detection to the Quit Button.

---
+++ castor 2.0/Assets/Scripts/Buttons/Quit.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Buttons
+{
+    public class Quit : MonoBehaviour
+    {
+
+        private void Update()
+        {
+            DetectKeyBoardShortCut();
+        }
+
+        public void OnClick()
+        {
+            QuitApplication();
+        }
+
+        private void QuitApplication()
+        {
+            Application.Quit();
+            if (Application.isEditor) Debug.Log("Pressed the quit button, however Application.Quit() does not work in editor mode.");
+        }
+
+        private void DetectKeyBoardShortCut()
+        {
+            if (Input.GetButton("Quit")) QuitApplication();
+        }
+    }
+}
