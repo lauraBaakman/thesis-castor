d2d94af389ef56772eadc2ef83a889e83b019ae3
Laura Baakman <laurabaakman@gmail.com>
2018-01-17 14:52:12 +0100

    Set up of FragmentImporter.

---
+++ castor 2.0/Assets/Scripts/IO/FragmentImporter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+namespace IO
+{
+    public class FragmentImporter
+    {
+        //TODO Fix Colors
+        //TODO Add to fragment Singleton
+
+        public GameObject FragmentsRoot;
+
+        public void Import()
+        {
+            string file = GetFragmentFile();
+            Mesh mesh = ReadMeshFromFile(file);
+            GameObject fragment = CreateGameObject(mesh);
+            AddGameObjectToScene(fragment);
+        }
+
+        private string GetFragmentFile()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private Mesh ReadMeshFromFile(string file)
+        {
+            throw new NotImplementedException();
+        }
+
+        private GameObject CreateGameObject(Mesh mesh)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddGameObjectToScene(GameObject fragment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
