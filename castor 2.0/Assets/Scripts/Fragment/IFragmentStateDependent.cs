222206430f2baeb9a1e23a393916b21c80972222
Laura Baakman <laurabaakman@gmail.com>
2018-01-18 10:46:26 +0100

    Interface for componenents that are dependent upon the state of an object.

---
+++ castor 2.0/Assets/Scripts/Fragment/IFragmentStateDependent.cs
@@ -0,0 +1,14 @@
+namespace Fragment
+{
+    public interface IFragmentStateDependent
+    {
+        void OnStateToLocked();
+        void OnStateToUnLocked();
+
+        void OnStateToSelected();
+        void OnStateToDeselected();
+
+        void OnStateToHidden();
+        void OnStateToVisible();
+    }
+}
