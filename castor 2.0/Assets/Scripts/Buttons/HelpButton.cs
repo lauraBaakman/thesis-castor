94613a2300776f5813616043b559ba2537ebc020
Laura Baakman <laurabaakman@gmail.com>
2018-01-18 17:11:00 +0100

    Added a help button that opens a webpage with instructions.

---
+++ castor 2.0/Assets/Scripts/Buttons/HelpButton.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+namespace Buttons
+{
+    public class HelpButton : AbstractButton
+    {
+        private string HelpPage = "https://laurabaakman.github.io/thesis-castor/";
+
+        public override void OnClick()
+        {
+            OpenHelpWebPage();
+        }
+
+        private void OpenHelpWebPage(){
+            Application.OpenURL(HelpPage);
+        }
+
+        protected override void DetectKeyBoardShortCut()
+        {
+            if (Input.GetButton("Help")) OpenHelpWebPage();
+        }
+    }
+
+}
