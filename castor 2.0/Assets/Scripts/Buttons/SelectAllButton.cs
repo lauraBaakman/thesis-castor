05d7303c3a8ebe5cac94c85cc6d5077b1aba0d17
Laura Baakman <laurabaakman@gmail.com>
2018-01-19 18:29:15 +0100

    Added a select all button.

---
+++ castor 2.0/Assets/Scripts/Buttons/SelectAllButton.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace Buttons
+{
+    public class SelectAllButton : AbstractButton
+    {
+        public override void OnClick()
+        {
+            SelectAll();
+        }
+
+        protected override void DetectKeyBoardShortCut()
+        {
+            if (Input.GetButtonDown("Select All")) SelectAll();
+        }
+
+        private void SelectAll()
+        {
+            Debug.Log("Select All!");
+        }
+    }
+}
+
