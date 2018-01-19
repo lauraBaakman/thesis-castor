96a0a652cfde00796fc1f185ce4de10050c0abff
Laura Baakman <laurabaakman@gmail.com>
2018-01-19 14:01:54 +0100

    Introduced a TransformButton that implements a more general version of the TranslationButton.

---
+++ castor 2.0/Assets/Scripts/Buttons/TransformButton.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+using RTEditor;
+
+namespace Buttons
+{
+    public class TransformButton : AbstractButton
+    {
+        private Button Button;
+
+        public RTEditor.GizmoType WidgetType;
+
+        public string InputName;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            Button = GetComponent<Button>();
+            Button.interactable = false;
+        }
+
+        public override void OnClick()
+        {
+            ToggleWidget();
+        }
+
+        protected void ToggleWidget()
+        {
+            bool isActive = EditorGizmoSystem.Instance.IsGizmoActive(WidgetType);
+            string method = isActive ? "DeactivateAllGizmoObjects" : "OnChangeActiveGizmo";
+            EditorGizmoSystem.Instance.SendMessage(
+                    methodName: method,
+                value: WidgetType,
+                    options: SendMessageOptions.RequireReceiver
+                );
+        }
+
+        public void OnNumberOfSelectedObjectsChanged(int currentCount)
+        {
+            Button.interactable = ShouldButtonBeInteractable(currentCount) ? true : false;
+        }
+
+        private bool ShouldButtonBeInteractable(int numSelectedObjects)
+        {
+            return numSelectedObjects >= 1;
+        }
+
+        protected override void DetectKeyBoardShortCut()
+        {
+            if (Input.GetButtonDown(InputName)) ToggleWidget();
+        }
+    }
+}
+
+
