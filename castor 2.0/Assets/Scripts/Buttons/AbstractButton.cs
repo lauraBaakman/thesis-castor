ae377f3fc6afb895062cb2cd230f2b5aed6bccf2
Laura Baakman <laurabaakman@gmail.com>
2018-01-19 10:02:30 +0100

    Register the onclick of abstract button as a listener to the button it is attatched to.

--- castor 2.0/Assets/Scripts/Buttons/AbstractButton.cs
+++ castor 2.0/Assets/Scripts/Buttons/AbstractButton.cs
@@ -1,9 +1,21 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Buttons
 {
     public abstract class AbstractButton : MonoBehaviour
     {
+
+        protected virtual void Awake(){
+            AddListenerToButton();
+        }
+
+        protected void AddListenerToButton()
+        {
+            Button button = GetComponent<Button>();
+            if (!button) Debug.LogError("Could not get te button component, connecting a listener failed.");
+            button.onClick.AddListener(OnClick);
+        }
 
         protected virtual void Update()
         {
