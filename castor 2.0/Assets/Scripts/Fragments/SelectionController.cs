a043a8013e90dd6850922305783e824c973cca3e
Laura Baakman <laurabaakman@gmail.com>
2018-01-19 10:52:34 +0100

    Script that will track the number of selected fragments.

---
+++ castor 2.0/Assets/Scripts/Fragments/SelectionController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+namespace Fragments
+{
+    public class SelectionController : MonoBehaviour
+    {
+        // Use this for initialization
+        void Start()
+        {
+            Debug.Log("Fragments Selection Controller");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
