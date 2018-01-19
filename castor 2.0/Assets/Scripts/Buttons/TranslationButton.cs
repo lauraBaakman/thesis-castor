f80221ab04206b023df7214d827aa42bd927a6ee
Laura Baakman <laurabaakman@gmail.com>
2018-01-19 10:02:46 +0100

    Added a script for the translation button.

---
+++ castor 2.0/Assets/Scripts/Buttons/TranslationButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Buttons
+{
+    public class TranslationButton : AbstractButton
+    {
+
+        public override void OnClick()
+        {
+            ShowTranslationWidget();
+        }
+
+        private void ShowTranslationWidget()
+        {
+            Debug.Log("Time to show the translation widget");
+        }
+
+        protected override void DetectKeyBoardShortCut()
+        {
+            if (Input.GetButton("Show Translation Widget")) ShowTranslationWidget();
+        }
+    }
+
+}
