f61724e25ccef2ae1d890de964a2ca764d7344e2
Laura Baakman <laurabaakman@gmail.com>
2018-01-21 16:09:30 +0100

    Implemented the undo/redo button script.

---
+++ castor 2.0/Assets/Scripts/Buttons/RedoButton.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using RTEditor;
+
+namespace Buttons
+{
+    public class RedoButton : AbstractButton
+    {
+        public override void OnClick()
+        {
+            Redo();
+        }
+
+        protected override void DetectKeyBoardShortCut()
+        {
+            if (IsEditorUndoCombinationPressed() ||
+                IsDeploymentUndoCombinationPressed()
+               ) Redo();
+        }
+
+        private bool IsEditorUndoCombinationPressed()
+        {
+            // We are running in deployment mode, not editor mode
+            if (!Application.isEditor) return false;
+
+            return (Input.GetButtonDown("Redo") &&
+                    InputHelper.IsAnyCtrlOrCommandKeyPressed() &&
+                    InputHelper.IsAnyShiftKeyPressed());
+        }
+
+        private bool IsDeploymentUndoCombinationPressed()
+        {
+            return (Input.GetButtonDown("Redo") &&
+                    InputHelper.IsAnyCtrlOrCommandKeyPressed());
+        }
+
+        private void Redo()
+        {
+            Debug.Log("Redo!");
+        }
+    }
+
+}
+
---
+++ castor 2.0/Assets/Scripts/Buttons/UndoButton.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using RTEditor;
+
+namespace Buttons
+{
+    public class UndoButton : AbstractButton
+    {
+        public override void OnClick()
+        {
+            Undo();
+        }
+
+        protected override void DetectKeyBoardShortCut()
+        {
+            if (IsEditorUndoCombinationPressed() ||
+                IsDeploymentUndoCombinationPressed()
+               ) Undo();
+        }
+
+        private bool IsEditorUndoCombinationPressed()
+        {
+            // We are running in deployment mode, not editor mode
+            if (!Application.isEditor) return false;
+
+            return (Input.GetButtonDown("Undo") &&
+                    InputHelper.IsAnyCtrlOrCommandKeyPressed() &&
+                    InputHelper.IsAnyShiftKeyPressed());
+        }
+
+        private bool IsDeploymentUndoCombinationPressed()
+        {
+            return (Input.GetButtonDown("Undo") &&
+                    InputHelper.IsAnyCtrlOrCommandKeyPressed());
+        }
+
+        private void Undo()
+        {
+            Debug.Log("Undo!");
+        }
+    }
+}
+
