7ab5eec77249d8dc28617cd864b4fe7338e61f3c
Laura Baakman <laurabaakman@gmail.com>
2018-01-22 17:39:57 +0100

    Component that only works in edit mode, that eventually randomly transforms the object it is attatched to.

---
+++ castor 2.0/Assets/Scripts/Fragment/RandomTransformer.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+namespace Fragment {
+
+    public class RandomTransformer : MonoBehaviour {
+
+        private void Awake()
+        {
+            if (Application.isEditor) TransformRandomly();
+        }
+
+        private void TransformRandomly()
+        {
+            Debug.Log(name + "HI!");
+        }
+
+
+    }
+}
+
