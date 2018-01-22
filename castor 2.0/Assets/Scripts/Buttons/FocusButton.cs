3bafc9074675d4e7766e03a721652a8f71cebf1f
Laura Baakman <laurabaakman@gmail.com>
2018-01-22 09:03:44 +0100

    Attatched butotn script and shortcut.

---
+++ castor 2.0/Assets/Scripts/Buttons/FocusButton.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+namespace Buttons
+{
+    public class FocusButton : AbstractButton
+    {
+        protected override bool HasDetectedKeyBoardShortCut()
+        {
+            return Input.GetButtonDown("Focus");
+        }
+
+        protected override void ExecuteButtonAction()
+        {
+            Debug.Log("Time to focus!");   
+        }
+    }
+}
