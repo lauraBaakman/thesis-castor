1f429dd939e2f992a242ae6467679412170899f2
Laura Baakman <laurabaakman@gmail.com>
2018-01-22 13:45:36 +0100

    Implementation of registration button.

--- castor 2.0/Assets/Scripts/Buttons/RegistrationButton.cs
+++ castor 2.0/Assets/Scripts/Buttons/RegistrationButton.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace Buttons
-{
-    public class RegistrationButton : AbstractButton, Fragments.ISelectionControllerListener
-    {
+using Registration;
+
+namespace Buttons {
+    public class RegistrationButton : AbstractButton, Fragments.ISelectionControllerListener {
         private int RequiredNumberOfSelectedFragments = 2;
+
+        public GameObject SelectedFragments;
+
 
         public void OnEnable()
         {
@@ -16,19 +19,60 @@ namespace Buttons
         /// Called once the number of selected objects has changed, the button should only be active when two objects are selected.
         /// </summary>
         /// <param name="currentCount">Current nmber of selected fragments.</param>
-        public void OnNumberOfSelectedObjectsChanged(int currentCount)
+        public void OnNumberOfSelectedObjectsChanged( int currentCount )
         {
             Button.interactable = (currentCount == RequiredNumberOfSelectedFragments);
         }
 
         protected override void ExecuteButtonAction()
         {
-            Debug.Log("Register");
+            GameObject modelFragment, staticFragment;
+            GetModelAndStaticFragment(out modelFragment, out staticFragment);
+
+            IterativeClosestPointRegisterer registerer = new IterativeClosestPointRegisterer(
+                modelFragment: modelFragment,
+                staticFragment: staticFragment,
+                callBack: FinishedRegistrationCallBack
+            );
+
+            NotifyUserOfStartingRegistration(modelFragment.name, staticFragment.name);
+
+            registerer.Register();
+        }
+
+        private void NotifyUserOfStartingRegistration(string modelFragmentName, string staticFragmentName){
+            SendMessage(
+                methodName: "OnSendMessageToTicker",
+                value: new Ticker.Message.InfoMessage(
+                    string.Format(
+                        "Starting registration of {0} to {1}",
+                        modelFragmentName, staticFragmentName
+                    )
+                ),
+                options: SendMessageOptions.RequireReceiver
+            );            
+        }
+
+        private void GetModelAndStaticFragment( out GameObject modelFragment, out GameObject staticFragment )
+        {
+            //We are interested in all children of SelectedFragmetns that have meshrenderes, i.e. the meshes.
+            MeshRenderer[] childMeshes = SelectedFragments.GetComponentsInChildren<MeshRenderer>();
+            Debug.Assert(
+                childMeshes.Length == 2,
+                "Expected SelectedFragments to have exactly two children with MeshRenders, not " + childMeshes.Length
+            );
+            modelFragment = childMeshes[0].gameObject;
+            staticFragment = childMeshes[1].gameObject;
         }
 
         protected override bool HasDetectedKeyBoardShortCut()
         {
             return RTEditor.InputHelper.IsAnyCtrlOrCommandKeyPressed() && Input.GetButtonDown("Register");
+        }
+
+        private void FinishedRegistrationCallBack()
+        {
+            Debug.Log("FinishedRegistrationCallBack!");
         }
     }
 
