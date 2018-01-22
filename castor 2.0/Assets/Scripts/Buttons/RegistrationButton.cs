1d317c9fc9a64d21d4ab444f593e41f4eb74d405
Laura Baakman <laurabaakman@gmail.com>
2018-01-22 13:00:31 +0100

    The registration button should only be interactable if exactly two fragments are selected.

--- castor 2.0/Assets/Scripts/Buttons/RegistrationButton.cs
+++ castor 2.0/Assets/Scripts/Buttons/RegistrationButton.cs
@@ -1,9 +1,26 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Buttons
 {
-    public class RegistrationButton : AbstractButton
+    public class RegistrationButton : AbstractButton, Fragments.ISelectionControllerListener
     {
+        private int RequiredNumberOfSelectedFragments = 2;
+
+        public void OnEnable()
+        {
+            OnNumberOfSelectedObjectsChanged(RTEditor.EditorObjectSelection.Instance.NumberOfSelectedObjects);
+        }
+
+        /// <summary>
+        /// Called once the number of selected objects has changed, the button should only be active when two objects are selected.
+        /// </summary>
+        /// <param name="currentCount">Current nmber of selected fragments.</param>
+        public void OnNumberOfSelectedObjectsChanged(int currentCount)
+        {
+            Button.interactable = (currentCount == RequiredNumberOfSelectedFragments);
+        }
+
         protected override void ExecuteButtonAction()
         {
             Debug.Log("Register");
