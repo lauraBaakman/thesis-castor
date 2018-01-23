2874b04064b2b368ea22f9418f842967be151be1
Laura Baakman <laurabaakman@gmail.com>
2018-01-23 16:57:21 +0100

    Class with extensions for Vector3, namely CompareTo.

---
+++ castor 2.0/Assets/Scripts/Extensions/Vector3Extensions.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public static class Vector3Extensions
+{
+    /// <summary>
+    /// Implementation of CompareTo for Vector3, if the magnitued of this vector is greater than that of the passed vector the function returns 1, if the magnitude of the passed vector is greater it returns -1, if the magnitudes are equal 0 is returned.
+    /// </summary>
+    /// <returns>A value that indicates the relative order of the objects being compared.</returns>
+    /// <param name="vector">This vector</param>
+    /// <param name="other">A vector4 to compare with this instance.</param>
+    public static int CompareTo(this Vector3 vector, Vector3 other)
+    {
+        return vector.sqrMagnitude.CompareTo(other.sqrMagnitude);
+    }
+}
