ef1929eefb1224b787934020448e54a7af410527
Laura Baakman <laurabaakman@gmail.com>
2018-01-23 17:28:51 +0100

    Moved ICP messages to its own file.

---
+++ castor 2.0/Assets/Scripts/Registration/ICPMessages.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Registration
+{
+    public class ICPPointsSelectedMessage
+    {
+        private readonly List<Vector3> points;
+        private readonly Transform pointsTransform;
+
+        public ICPPointsSelectedMessage(List<Vector3> points, Transform pointsTransform)
+        {
+            this.points = points;
+            this.pointsTransform = pointsTransform;
+        }
+
+        public List<Vector3> Points
+        {
+            get
+            {
+                return points;
+            }
+        }
+
+        public Transform PointsTransform
+        {
+            get
+            {
+                return pointsTransform;
+            }
+        }
+    }
+
+    public class ICPCorrespondencesDeterminedMessage
+    {
+        private readonly List<Correspondence> correspondences;
+        private readonly Transform transform;
+
+        public ICPCorrespondencesDeterminedMessage(List<Correspondence> correspondences, Transform transform)
+        {
+            this.correspondences = correspondences;
+            this.transform = transform;
+        }
+
+        public List<Correspondence> Correspondences
+        {
+            get
+            {
+                return correspondences;
+            }
+        }
+
+        public Transform Transform
+        {
+            get
+            {
+                return transform;
+            }
+        }
+    }
+}
--- castor 2.0/Assets/Scripts/Registration/IICPListener.cs
+++ castor 2.0/Assets/Scripts/Registration/IICPListener.cs
@@ -1,63 +1,14 @@
-using UnityEngine;
-using System.Collections.Generic;
 using System.Collections;
 
-namespace Registration {
-    public interface IICPListener {
-        void OnICPPointsSelected( ICPPointsSelectedMessage message );
+namespace Registration
+{
+    public interface IICPListener
+    {
+        void OnICPPointsSelected(ICPPointsSelectedMessage message);
 
         void OnICPCorrespondencesDetermined(ICPCorrespondencesDeterminedMessage message);
 
         IEnumerator OnICPFinished();
-    }
-
-    public class ICPPointsSelectedMessage {
-        private readonly List<Vector3> points;
-        private readonly Transform pointsTransform;
-
-        public ICPPointsSelectedMessage( List<Vector3> points, Transform pointsTransform )
-        {
-            this.points = points;
-            this.pointsTransform = pointsTransform;
-        }
-
-        public List<Vector3> Points {
-            get {
-                return points;
-            }
-        }
-
-        public Transform PointsTransform {
-            get {
-                return pointsTransform;
-            }
-        }
-    }
-
-    public class ICPCorrespondencesDeterminedMessage {
-        private readonly List<Correspondence> correspondences;
-        private readonly Transform transform;
-
-        public ICPCorrespondencesDeterminedMessage(List<Correspondence> correspondences, Transform transform){
-            this.correspondences = correspondences;
-            this.transform = transform;
-        }
-
-        public List<Correspondence> Correspondences
-        {
-            get
-            {
-                return correspondences;
-            }
-        }
-
-        public Transform Transform
-        {
-            get
-            {
-                return transform;
-            }
-        }
     }
 }
 
