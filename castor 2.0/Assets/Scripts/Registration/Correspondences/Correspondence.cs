c4d848629057c552123e0ce021a46c9037177136
Laura Baakman <laurabaakman@gmail.com>
2018-01-24 13:27:04 +0100

    Formatting and getters for points of the correspondence.

--- castor 2.0/Assets/Scripts/Registration/Correspondences/Correspondence.cs
+++ castor 2.0/Assets/Scripts/Registration/Correspondences/Correspondence.cs
@@ -11,26 +11,31 @@ namespace Registration
     /// </summary>
     public class Correspondence : IEquatable<Correspondence>
     {
-        private readonly Vector3 ModelPoint;
-        private readonly Vector3 StaticPoint;
+        private readonly Vector3 modelPoint;
+        private readonly Vector3 staticPoint;
+
+        public Vector3 ModelPoint { get { return modelPoint; } }
+
+        public Vector3 StaticPoint { get { return staticPoint; } }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="T:Correspondence"/> class. 
         /// </summary>
         /// <param name="modelPoint">The point of the correspondence from the model mesh..</param>
         /// <param name="staticPoint">The point of the correspondence from the static mesh.</param>
-        public Correspondence( Vector3 staticPoint, Vector3 modelPoint )
+        public Correspondence(Vector3 staticPoint, Vector3 modelPoint)
         {
-            ModelPoint = modelPoint;
-            StaticPoint = staticPoint;
+            this.modelPoint = modelPoint;
+            this.staticPoint = staticPoint;
         }
 
-        public Correspondence(DistanceNode node){
-            ModelPoint = node.ModelPoint;
-            StaticPoint = node.StaticPoint;
+        public Correspondence(DistanceNode node)
+        {
+            this.modelPoint = node.ModelPoint;
+            this.staticPoint = node.StaticPoint;
         }
 
-        public override bool Equals( object obj )
+        public override bool Equals(object obj)
         {
             if (obj == null || GetType() != obj.GetType())
                 return false;
@@ -39,7 +44,7 @@ namespace Registration
             return this.Equals(other);
         }
 
-        public bool Equals( Correspondence other )
+        public bool Equals(Correspondence other)
         {
             if (other == null) return false;
 
