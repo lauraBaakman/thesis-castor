26fa996b0cfd85d1362a78b88a35ae48a47197c7
Laura Baakman <laurabaakman@gmail.com>
2018-01-24 11:54:28 +0100

    Formatting and clarified debug statements.

--- castor 2.0/Assets/Scripts/Fragment/ICPController.cs
+++ castor 2.0/Assets/Scripts/Fragment/ICPController.cs
@@ -2,57 +2,75 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
 using Registration;
+using System;
 
-public class ICPController : MonoBehaviour, IICPListener
+namespace Fragment
 {
-
-    public float ParticleSize = 0.01f;
-    public Color ParticleColor = Color.white;
-
-    private ParticleSystem ParticleSystem;
-
-    private void Awake()
+    public class ICPController : MonoBehaviour, IICPListener
     {
-        ParticleSystem = GetComponent<ParticleSystem>();
-        Debug.Assert(ParticleSystem, "Could not get the ParticleSystem component.");
-    }
 
-    public IEnumerator OnICPFinished()
-    {
-        if (Application.isEditor)
+        public float ParticleSize = 0.01f;
+        public Color ParticleColor = Color.white;
+
+        private ParticleSystem ParticleSystem;
+
+        private void Awake()
         {
-            Debug.Log("Entered OnICPFinished, yielding for a while");
-            yield return new WaitForSeconds(3);
+            ParticleSystem = GetComponent<ParticleSystem>();
+            Debug.Assert(ParticleSystem, "Could not get the ParticleSystem component.");
         }
-        ParticleSystem.Clear();
-    }
 
-    /// <summary>
-    /// If the points to be used for ICP are selected this function visualizes them.
-    /// </summary>
-    public void OnICPPointsSelected(ICPPointsSelectedMessage message)
-    {
-        SetParticleSystemTransform(message.Transform);
-        VisualizePoints(message.Points);
-    }
-
-    private void SetParticleSystemTransform(Transform newTransform)
-    {
-        ParticleSystem.MainModule particleConfiguration = ParticleSystem.main;
-        particleConfiguration.simulationSpace = ParticleSystemSimulationSpace.Custom;
-        particleConfiguration.customSimulationSpace = newTransform;
-    }
-
-    private void VisualizePoints(List<Vector3> points)
-    {
-        ParticleSystem.Particle[] particles = new ParticleSystem.Particle[points.Count];
-        for (int i = 0; i < points.Count; i++)
+        public IEnumerator OnICPFinished()
         {
-            particles[i].position = points[i];
-            particles[i].startColor = ParticleColor;
-            particles[i].startSize = ParticleSize;
-
+            if (Application.isEditor)
+            {
+                Debug.Log("Fragment:ICPController:OnICPFinished: yielding for a while");
+                yield return new WaitForSeconds(3);
+            }
+            ClearPoints();
         }
-        ParticleSystem.SetParticles(particles, particles.Length);
+
+        #region Points
+        /// <summary>
+        /// If the points to be used for ICP are selected this function visualizes them.
+        /// </summary>
+        public void OnICPPointsSelected(ICPPointsSelectedMessage message)
+        {
+            SetParticleSystemTransform(message.Transform);
+            VisualizePoints(message.Points);
+        }
+
+        private void SetParticleSystemTransform(Transform newTransform)
+        {
+            ParticleSystem.MainModule particleConfiguration = ParticleSystem.main;
+            particleConfiguration.simulationSpace = ParticleSystemSimulationSpace.Custom;
+            particleConfiguration.customSimulationSpace = newTransform;
+        }
+
+        private void VisualizePoints(List<Vector3> points)
+        {
+            ParticleSystem.Particle[] particles = new ParticleSystem.Particle[points.Count];
+            for (int i = 0; i < points.Count; i++)
+            {
+                particles[i].position = points[i];
+                particles[i].startColor = ParticleColor;
+                particles[i].startSize = ParticleSize;
+
+            }
+            ParticleSystem.SetParticles(particles, particles.Length);
+        }
+
+        private void ClearPoints()
+        {
+            ParticleSystem.Clear();
+        }
+        #endregion
+
+        #region Correspondences
+        public void OnICPCorrespondencesDetermined(ICPCorrespondencesDeterminedMessage message)
+        {
+            //Do nothing, correspondences are handled by Fragments.ICPController.
+        }
+        #endregion
     }
 }
