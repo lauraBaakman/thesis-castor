753183f507e3e36a12b882bedd503561d4e2171c
Laura Baakman <laurabaakman@gmail.com>
2018-01-24 14:00:02 +0100

    Interface for lists of correspondences.

---
+++ castor 2.0/Assets/Scripts/Registration/Correspondences/Filters/ICorrespondenceFilter.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+
+namespace Registration
+{
+    public interface ICorrespondenceFilter
+    {
+        IList<Correspondence> Filter(IList<Correspondence> correspondences);
+    }
+}
