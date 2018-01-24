cbb80c4879971685a1820391b9fe6ba07cfe25bf
Laura Baakman <laurabaakman@gmail.com>
2018-01-24 09:00:54 +0100

    Formatting.

--- castor 2.0/Assets/Scripts/Registration/PointSelection/SelectAllPointsSelector.cs
+++ castor 2.0/Assets/Scripts/Registration/PointSelection/SelectAllPointsSelector.cs
@@ -1,23 +1,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Registration {
+namespace Registration
+{
     /// <summary>
     /// The simple point selector simply selects all the points of the mesh.
     /// </summary>
-    public class SelectAllPointsSelector : IPointSelector {
+    public class SelectAllPointsSelector : IPointSelector
+    {
 
         /// <summary>
         /// The transform that the points should be sampled in.
         /// </summary>
         private readonly Transform ReferenceTransform;
 
-        public SelectAllPointsSelector( Transform referenceTransform )
+        public SelectAllPointsSelector(Transform referenceTransform)
         {
             ReferenceTransform = referenceTransform;
         }
 
-        public List<Vector3> Select( Transform fragmentTransform, Mesh fragment )
+        public List<Vector3> Select(Transform fragmentTransform, Mesh fragment)
         {
             List<Vector3> points = new List<Vector3>(
                 fragment.vertices
@@ -28,8 +30,8 @@ namespace Registration {
         private List<Vector3> ToReferenceTransfrom(List<Vector3> pointsLocalTransform, Transform localTransform)
         {
             List<Vector3> pointsReferenceTransform = new List<Vector3>(pointsLocalTransform.Count);
-            foreach (Vector3 localPoint in pointsLocalTransform) {
-
+            foreach (Vector3 localPoint in pointsLocalTransform)
+            {
                 Vector3 worldTransformPoint = localTransform.TransformPoint(localPoint);
                 Vector3 referenceTransformPoint = ReferenceTransform.InverseTransformPoint(worldTransformPoint);
 
