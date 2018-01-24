4f4d1c3fdefe252902181b4c0b8680b17155baf2
Laura Baakman <laurabaakman@gmail.com>
2018-01-24 11:18:47 +0100

    Formatting.

--- castor 2.0/Assets/Scripts/Fragment/ICPController.cs
+++ castor 2.0/Assets/Scripts/Fragment/ICPController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Collections;
 using Registration;
 
-public class ICPController : MonoBehaviour, IICPListener {
+public class ICPController : MonoBehaviour, IICPListener
+{
 
     public float ParticleSize = 0.01f;
     public Color ParticleColor = Color.white;
+
     private ParticleSystem ParticleSystem;
 
     private void Awake()
@@ -15,22 +17,10 @@ public class ICPController : MonoBehaviour, IICPListener {
         Debug.Assert(ParticleSystem, "Could not get the ParticleSystem component.");
     }
 
-
-    private void VisualizePoints( List<Vector3> points )
-    {
-        ParticleSystem.Particle[] particles = new ParticleSystem.Particle[points.Count];
-        for (int i = 0; i < points.Count; i++) {
-            particles[i].position = points[i];
-            particles[i].startColor = ParticleColor;
-            particles[i].startSize = ParticleSize;
-
-        }
-        ParticleSystem.SetParticles(particles, particles.Length);
-    }
-
     public IEnumerator OnICPFinished()
     {
-        if (Application.isEditor) {
+        if (Application.isEditor)
+        {
             Debug.Log("Entered OnICPFinished, yielding for a while");
             yield return new WaitForSeconds(3);
         }
@@ -40,20 +30,29 @@ public class ICPController : MonoBehaviour, IICPListener {
     /// <summary>
     /// If the points to be used for ICP are selected this function visualizes them.
     /// </summary>
-    public void OnICPPointsSelected( ICPPointsSelectedMessage message )
+    public void OnICPPointsSelected(ICPPointsSelectedMessage message)
     {
         SetParticleSystemTransform(message.Transform);
         VisualizePoints(message.Points);
     }
 
-    private void SetParticleSystemTransform(Transform newTransform){
+    private void SetParticleSystemTransform(Transform newTransform)
+    {
         ParticleSystem.MainModule particleConfiguration = ParticleSystem.main;
         particleConfiguration.simulationSpace = ParticleSystemSimulationSpace.Custom;
-        particleConfiguration.customSimulationSpace = newTransform;        
+        particleConfiguration.customSimulationSpace = newTransform;
     }
 
-    public void OnICPCorrespondencesDetermined(ICPCorrespondencesDeterminedMessage message)
+    private void VisualizePoints(List<Vector3> points)
     {
-        Debug.Log("Received OnICPCorrespondencesDetermined message!");
+        ParticleSystem.Particle[] particles = new ParticleSystem.Particle[points.Count];
+        for (int i = 0; i < points.Count; i++)
+        {
+            particles[i].position = points[i];
+            particles[i].startColor = ParticleColor;
+            particles[i].startSize = ParticleSize;
+
+        }
+        ParticleSystem.SetParticles(particles, particles.Length);
     }
 }
