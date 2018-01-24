09e750ea4b4c1de3e0f1fad5c4a6f2b43f10939e
Laura Baakman <laurabaakman@gmail.com>
2018-01-24 11:28:58 +0100

    Attatched an ICP controller to SelectedFragments

---
+++ castor 2.0/Assets/Scripts/Fragments/ICPController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+namespace Fragments
+{
+    public class ICPController : MonoBehaviour
+    {
+        // Use this for initialization
+        void Start()
+        {
+            Debug.Log("Fragments: ICPController");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
