3fc24550a570cab1c93505f723cd51727b0e308d
Laura Baakman <laurabaakman@gmail.com>
2018-01-30 14:12:34 +0100

    Store the maximum number of iterations.

--- castor 2.0/Assets/Scripts/Registration/Settings.cs
+++ castor 2.0/Assets/Scripts/Registration/Settings.cs
@@ -9,20 +9,28 @@ namespace Registration
         /// The transform in which the registration is performed. 
         /// </summary>
         /// <value>The reference transform.</value>
-        public Transform ReferenceTransform { get; internal set; }
+        public Transform ReferenceTransform { get; set; }
 
         /// <summary>
         /// If the error of the registration is smaller than this threshold the
         /// registration process terminates.
         /// </summary>
         /// <value>The error threshold.</value>
-        public float ErrorThreshold { get; internal set; }
+        public float ErrorThreshold { get; set; }
 
-        public Settings(Transform referenceTransform, float errorThreshold = 0.001f)
+        /// <summary>
+        /// The maximum number of iterations to execute.
+        /// </summary>
+        /// <value>The max number iterations.</value>
+        public int MaxNumIterations { get; set; }
+
+        public Settings(Transform referenceTransform, float errorThreshold = 0.001f, int maxNumIterations = 50)
         {
             ReferenceTransform = referenceTransform;
 
             ErrorThreshold = errorThreshold;
+
+            MaxNumIterations = maxNumIterations;
         }
     }
 }
