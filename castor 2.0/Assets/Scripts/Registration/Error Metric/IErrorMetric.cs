d6e0ba65b88b55b1f9b77e521e6e30292cced1b2
Laura Baakman <laurabaakman@gmail.com>
2018-01-30 12:30:42 +0100

    Defined interface.

--- castor 2.0/Assets/Scripts/Registration/Error Metric/IErrorMetric.cs
+++ castor 2.0/Assets/Scripts/Registration/Error Metric/IErrorMetric.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace Registration
 {
     public interface IErrorMetric
     {
+        float ComputeError(IEnumerable<Correspondence> correspondences);
     }
 }
--- castor 2.0/Assets/Scripts/Registration/Error Metric/PointToPointMeanSquaredError.cs
+++ castor 2.0/Assets/Scripts/Registration/Error Metric/PointToPointMeanSquaredError.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
 
 namespace Registration
 {
     public class PointToPointMeanSquaredError : IErrorMetric
     {
+        public float ComputeError(IEnumerable<Correspondence> correspondences)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
-
