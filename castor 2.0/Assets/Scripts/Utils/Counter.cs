ce4a5dd2e4d916e01915c033ec299e580bcebcb5
Laura Baakman <laurabaakman@gmail.com>
2018-01-30 14:08:33 +0100

    Implementation of a counter.

---
+++ castor 2.0/Assets/Scripts/Utils/Counter.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using System.Collections;
+
+namespace Utils
+{
+    public class Counter
+    {
+        public int CurrentCount
+        {
+            get { return currentCount; }
+        }
+        private int currentCount;
+
+        public int CountToReach
+        {
+            get { return countToReach; }
+        }
+        private int countToReach;
+
+        public Counter()
+        {
+            Reset();
+        }
+
+        public Counter(int CountToReach) : base()
+        {
+            countToReach = CountToReach;
+        }
+
+        public void Reset()
+        {
+            currentCount = 0;
+            countToReach = 0;
+        }
+
+        public void Increase()
+        {
+            if (currentCount < countToReach) currentCount++;
+        }
+
+        public bool IsCompleted()
+        {
+            if (countToReach == 0) return false;
+            return currentCount >= countToReach;
+        }
+
+        public void Set(int countToReach)
+        {
+            Reset();
+            this.countToReach = countToReach;
+        }
+    }
+
+}
---
+++ castor 2.0/Assets/Scripts/Utils/Editor/TestCounter.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using System.Collections;
+
+using Utils;
+using NUnit.Framework;
+
+[TestFixture]
+public class TestCounter
+{
+    [Test]
+    public void Test_EmptyContructor()
+    {
+        int countToReach = 3;
+
+        Counter counter = new Counter();
+        counter.Set(countToReach);
+
+        Assert.AreEqual(counter.CountToReach, countToReach);
+
+        counter.Increase();
+        Assert.AreEqual(counter.CurrentCount, 1);
+        Assert.AreEqual(counter.CountToReach, countToReach);
+        Assert.IsFalse(counter.IsCompleted());
+
+        counter.Increase();
+        Assert.AreEqual(counter.CurrentCount, 2);
+        Assert.AreEqual(counter.CountToReach, countToReach);
+        Assert.IsFalse(counter.IsCompleted());
+
+        counter.Increase();
+        Assert.AreEqual(counter.CurrentCount, 3);
+        Assert.AreEqual(counter.CountToReach, countToReach);
+        Assert.IsTrue(counter.IsCompleted());
+
+        counter.Increase();
+        Assert.AreEqual(counter.CurrentCount, 3);
+        Assert.AreEqual(counter.CountToReach, countToReach);
+        Assert.IsTrue(counter.IsCompleted());
+
+        counter.Reset();
+        Assert.AreEqual(counter.CurrentCount, 0);
+        Assert.AreEqual(counter.CountToReach, 0);
+        Assert.IsFalse(counter.IsCompleted());
+    }
+
+    [Test]
+    public void Test_NonEmptyContructor()
+    {
+        int countToReach = 3;
+
+        Counter counter = new Counter(CountToReach: countToReach);
+        Assert.AreEqual(counter.CountToReach, countToReach);
+
+        counter.Increase();
+        Assert.AreEqual(counter.CurrentCount, 1);
+        Assert.AreEqual(counter.CountToReach, countToReach);
+        Assert.IsFalse(counter.IsCompleted());
+
+        counter.Increase();
+        Assert.AreEqual(counter.CurrentCount, 2);
+        Assert.AreEqual(counter.CountToReach, countToReach);
+        Assert.IsFalse(counter.IsCompleted());
+
+        counter.Increase();
+        Assert.AreEqual(counter.CurrentCount, 3);
+        Assert.AreEqual(counter.CountToReach, countToReach);
+        Assert.IsTrue(counter.IsCompleted());
+
+        counter.Increase();
+        Assert.AreEqual(counter.CurrentCount, 3);
+        Assert.AreEqual(counter.CountToReach, countToReach);
+        Assert.IsTrue(counter.IsCompleted());
+
+        counter.Reset();
+        Assert.AreEqual(counter.CurrentCount, 0);
+        Assert.AreEqual(counter.CountToReach, 0);
+        Assert.IsFalse(counter.IsCompleted());
+    }
+
+}
