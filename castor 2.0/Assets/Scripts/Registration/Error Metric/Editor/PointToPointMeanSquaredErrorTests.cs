bf59fd1b8fac331921b1cd08549876fd52b2dd3f
Laura Baakman <laurabaakman@gmail.com>
2018-01-30 12:49:21 +0100

    Tests for the MSE.

---
+++ castor 2.0/Assets/Scripts/Registration/Error Metric/Editor/PointToPointMeanSquaredErrorTests.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+using Registration;
+
+[TestFixture]
+public class PointToPointMeanSquaredErrorTests
+{
+    [Test]
+    public void TestComputeError()
+    {
+        List<Correspondence> correspondences = new List<Correspondence>
+        {
+            new Correspondence(
+                new Vector3(1.0f, 2.0f, 3.0f),
+                new Vector3(2.0f, 3.0f, 4.0f)
+            ),
+            new Correspondence(
+                new Vector3(3.4f, 4.5f, 5.6f),
+                new Vector3(6.7f, 7.8f, 8.9f)
+            ),
+            new Correspondence(
+                new Vector3(9.1f, 2.3f, 3.4f),
+                new Vector3(4.5f, 5.6f, 6.7f)
+            )
+        };
+
+        float expected = 37.003333333333337f;
+        float actual = new PointToPointMeanSquaredError().ComputeError(correspondences);
+
+        Assert.AreEqual(expected, actual);
+    }
+}
---
+++ castor 2.0/Assets/Scripts/Registration/Error Metric/Editor/PointToPointMeanSquaredErrorTests.m
@@ -0,0 +1,15 @@
+staticPoints = [
+    1.0, 2.0, 3.0; 
+    3.4, 4.5, 5.6; 
+    9.1, 2.3, 3.4
+];
+
+modelPoints = [
+    2.0, 3.0, 4.0;
+    6.7, 7.8, 8.9;  
+    4.5, 5.6, 6.7;
+];
+
+errors = sum((staticPoints - modelPoints),2);
+squaredErrors = errors.^2
+mse = mean(squaredErrors)
