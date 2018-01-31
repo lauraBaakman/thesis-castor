92ed90223ab64f7d03114e5f06165afe226bb4ad
Laura Baakman <laurabaakman@gmail.com>
2018-01-31 14:04:06 +0100

    Interface for transform finders.

---
+++ castor 2.0/Assets/Scripts/Registration/Transform Finders/ITransformFinder.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace Registration
+{
+    public interface ITransformFinder
+    {
+        Transform FindTransform(List<Correspondence> correspondences);
+    }
+}
