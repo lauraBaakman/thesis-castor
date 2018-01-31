bebedfd68cbd817738fd023b8733ec703078e560
Laura Baakman <laurabaakman@gmail.com>
2018-01-31 14:04:54 +0100

    Set up of HornTransformFinder class.

---
+++ castor 2.0/Assets/Scripts/Registration/Transform Finders/HornTransformFinder.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace Registration
+{
+    public class HornTransformFinder : ITransformFinder
+    {
+        public Transform FindTransform(List<Correspondence> correspondences)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
+
