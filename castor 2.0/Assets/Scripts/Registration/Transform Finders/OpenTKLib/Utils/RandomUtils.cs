af020005b938c88a490fb28104ce6b46d738beee
Laura Baakman <laurabaakman@gmail.com>
2018-02-01 13:49:02 +0100

    Removed unused imports.

--- castor 2.0/Assets/Scripts/Registration/Transform Finders/OpenTKLib/Utils/RandomUtils.cs
+++ castor 2.0/Assets/Scripts/Registration/Transform Finders/OpenTKLib/Utils/RandomUtils.cs
@@ -15,14 +15,6 @@
 //
 using System;
 using System.Collections.Generic;
-//using System.ComponentModel;
-//using System.Data;
-//using System.Drawing;
-//using System.Text;
-using System.Windows.Forms;
-//using System.IO;
-//using System.Globalization;
-//using System.Windows.Media;
 using OpenTK;
 
 namespace OpenTKLib
