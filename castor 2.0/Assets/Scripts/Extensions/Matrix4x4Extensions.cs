3854be895f37d238eb7505f224869a7fbb63be64
Laura Baakman <laurabaakman@gmail.com>
2018-02-01 17:14:35 +0100

    implementation of method to extract rotation and translation.

--- castor 2.0/Assets/Scripts/Extensions/Matrix4x4Extensions.cs
+++ castor 2.0/Assets/Scripts/Extensions/Matrix4x4Extensions.cs
@@ -3,16 +3,32 @@ using System.Collections;
 
 public static class Matrix4x4Extensions
 {
-    public static Vector3 ExtractTranslation(this Matrix4x4 matrix){
-        Vector3 translation = new Vector3();
-
-        return translation;
+    /// <summary>
+    ///Extracts the translation from the matrix.
+    /// Source: https://answers.unity.com/answers/402281/view.html
+    /// </summary>
+    /// <returns>The translation.</returns>
+    /// <param name="matrix">Matrix.</param>
+    public static Vector3 ExtractPosition(this Matrix4x4 matrix){
+        Vector3 position = matrix.GetColumn(3);
+        return position;
     }
 
+    /// <summary>
+    /// Extracts the rotation from the matrix.
+    /// Source: https://answers.unity.com/answers/402281/view.html and https://forum.unity.com/threads/how-to-assign-matrix4x4-to-transform.121966/
+    /// </summary>
+    /// <returns>The rotation.</returns>
+    /// <param name="matrix">Matrix.</param>
     public static Quaternion ExtratRotation(this Matrix4x4 matrix)
     {
-        Quaternion rotation = new Quaternion();
-
+        Vector3 forward = matrix.GetColumn(2);
+        Vector3 upwards = matrix.GetColumn(1);
+                                
+        Quaternion rotation = Quaternion.LookRotation(
+            forward:forward, 
+            upwards:upwards
+        );
         return rotation;
     }
 }
