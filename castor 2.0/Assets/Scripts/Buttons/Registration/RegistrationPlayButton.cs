17792ed123da3dac0eb6f5afe3611edbe05d0626
Laura Baakman <laurabaakman@gmail.com>
2018-02-05 12:20:14 +0100

    initialize play butto to be disabled.

--- castor 2.0/Assets/Scripts/Buttons/Registration/RegistrationPlayButton.cs
+++ castor 2.0/Assets/Scripts/Buttons/Registration/RegistrationPlayButton.cs
@@ -6,6 +6,13 @@ namespace Buttons
 {
     public class RegistrationPlayButton : AbstractRegistrationButton
     {
+        protected override void Awake()
+        {
+            base.Awake();
+
+            Button.interactable = false;
+        }
+
         protected override void ExecuteButtonAction()
         {
             if (registerer == null) return;
