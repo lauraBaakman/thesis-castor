eb1d52091140bc2552330e33f6bfbd8011629f10
Laura Baakman <laurabaakman@gmail.com>
2018-02-05 09:12:09 +0100

    ICP controller to be attatched to the Registration button.

---
+++ castor 2.0/Assets/Scripts/Buttons/ICPController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+namespace Buttons
+{
+    public class ICPController : MonoBehaviour
+    {
+        public AbstractButton RegistrationButton;
+        public AbstractButton StepButton;
+        public AbstractButton PlayButton;
+        public AbstractButton StopButton;
+    }
+}
+
