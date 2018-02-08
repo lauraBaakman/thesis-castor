decf4e49b76b5261829a6d1517eb98dc60192db2
Laura Baakman <laurabaakman@gmail.com>
2018-02-08 11:19:13 +0100

    Added DeleteFragmnet button.

---
+++ castor 2.0/Assets/Scripts/Buttons/DeleteFragmentButton.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+namespace Buttons
+{
+    public class DeleteFragmentButton : AbstractButton
+    {
+        protected override void ExecuteButtonAction()
+        {
+            Debug.Log("Delete fragment!");
+        }
+
+        protected override bool HasDetectedKeyBoardShortCut()
+        {
+            return Input.GetKeyDown(KeyCode.Backspace) && RTEditor.InputHelper.IsAnyCtrlOrCommandKeyPressed();
+        }
+    }
+}
+
