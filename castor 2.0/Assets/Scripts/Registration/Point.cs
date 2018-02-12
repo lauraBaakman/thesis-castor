630415a3c077d12d6acab5b2c7dcbbc2eaa15485
Laura Baakman <laurabaakman@gmail.com>
2018-02-12 14:33:15 +0100

    Class for Points with Normals.

--- castor 2.0/Assets/Scripts/Registration/Point.cs
+++ castor 2.0/Assets/Scripts/Registration/Point.cs
@@ -57,4 +57,64 @@ namespace Registration
             );
         }
     }
+
+    public class PointWithNormal
+        : Point, IEquatable<PointWithNormal>, IComparable<PointWithNormal>, IEquatable<Point>, IComparable<Point>
+    {
+        public Vector3 Normal
+        {
+            get { return normal; }
+        }
+        private Vector3 normal;
+
+        public PointWithNormal(Vector3 position, Vector3 normal)
+            : base(position)
+        {
+            this.normal = normal;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+            return this.Equals(obj as PointWithNormal);
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = base.GetHashCode();
+
+            hashCode = hashCode * 71 + normal.GetHashCode();
+
+            return hashCode;
+        }
+
+        public override string ToString()
+        {
+            return string.Format(
+                "[PointWithNormal: Position={0}, Normal={1}]",
+                Position, Normal
+            );
+        }
+
+        public bool Equals(PointWithNormal other)
+        {
+            if (other == null) return false;
+
+            return (
+                this.position.Equals(other.position) &&
+                this.normal.Equals(other.normal)
+            );
+        }
+
+        public int CompareTo(PointWithNormal other)
+        {
+            if (other == null) return 1;
+
+            int positonComparison = this.position.CompareTo(other.position);
+            if (positonComparison != 0) return positonComparison;
+
+            return this.normal.CompareTo(other.normal);
+        }
+    }
 }
