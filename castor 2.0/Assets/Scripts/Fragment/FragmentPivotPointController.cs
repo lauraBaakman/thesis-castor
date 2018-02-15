3f0941c4c3c5edef085a987eb57b1d3e7264a0ca
Laura Baakman <laurabaakman@gmail.com>
2018-02-15 14:36:23 +0100

    Moved FragmentPivotController to the Fragment namespace.

--- castor 2.0/Assets/Scripts/Fragment/FragmentPivotPointController.cs
+++ castor 2.0/Assets/Scripts/Fragment/FragmentPivotPointController.cs
@@ -1,24 +1,31 @@
 ﻿using UnityEngine;
 
-/// <summary>
-/// The fragments do not necessarily have their pivot at their center, we add
-/// an empty gameobject placed at the center of the bounds of the parent object
-/// and use that to rotate the fragment.
-/// 
-/// Source: https://forum.unity.com/threads/change-an-objects-pivot-point.22885/#post-199538
-/// </summary>
-public class FragmentPivotPointController : MonoBehaviour {
+namespace Fragment
+{
+    /// <summary>
+    /// The fragments do not necessarily have their pivot at their center, we add
+    /// an empty gameobject placed at the center of the bounds of the parent object
+    /// and use that to rotate the fragment.
+    /// 
+    /// Source: https://forum.unity.com/threads/change-an-objects-pivot-point.22885/#post-199538
+    /// </summary>
+    public class FragmentPivotPointController : MonoBehaviour
+    {
 
-	void Start () {
-        Vector3 parentCenter = GetParentCenter(transform.parent.gameObject);
-        transform.Translate(parentCenter);
-	}
+        void Start()
+        {
+            Vector3 parentCenter = GetParentCenter(transform.parent.gameObject);
+            transform.Translate(parentCenter);
+        }
 
-    private Vector3 GetParentCenter(GameObject parent){
-        Renderer meshRenderer = parent.GetComponent<MeshRenderer>();
-        Debug.Assert(meshRenderer, "The parent needs to have a MeshRenderer.");
+        private Vector3 GetParentCenter(GameObject parent)
+        {
+            Renderer meshRenderer = parent.GetComponent<MeshRenderer>();
+            Debug.Assert(meshRenderer, "The parent needs to have a MeshRenderer.");
 
-        Vector3 center = meshRenderer.bounds.center;
-        return center;
+            Vector3 center = meshRenderer.bounds.center;
+            return center;
+        }
     }
 }
+
