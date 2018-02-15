4928708cc6bd05dd4d7942e684bb8ff0ac43dc4f
Laura Baakman <laurabaakman@gmail.com>
2018-02-15 14:34:45 +0100

    Add a child gameobject to a fragment to define a pivot center.

---
+++ castor 2.0/Assets/Scripts/Fragment/FragmentPivotPointController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// The fragments do not necessarily have their pivot at their center, we add
+/// an empty gameobject placed at the center of the bounds of the parent object
+/// and use that to rotate the fragment.
+/// 
+/// Source: https://forum.unity.com/threads/change-an-objects-pivot-point.22885/#post-199538
+/// </summary>
+public class FragmentPivotPointController : MonoBehaviour {
+
+	void Start () {
+        Vector3 parentCenter = GetParentCenter(transform.parent.gameObject);
+        transform.Translate(parentCenter);
+	}
+
+    private Vector3 GetParentCenter(GameObject parent){
+        Renderer meshRenderer = parent.GetComponent<MeshRenderer>();
+        Debug.Assert(meshRenderer, "The parent needs to have a MeshRenderer.");
+
+        Vector3 center = meshRenderer.bounds.center;
+        return center;
+    }
+}
