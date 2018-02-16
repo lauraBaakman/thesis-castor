8ff04fc532f613f5b86f32f88c1f34153793d190
Laura Baakman <laurabaakman@gmail.com>
2018-02-16 15:22:29 +0100

    Transform extension to reset a transform.

---
+++ castor 2.0/Assets/Scripts/Extensions/TransformExtensions.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public static class TransformExtensions
+{
+    /// <summary>
+    /// Reset the specified transform, i.e. position to the origin of the parent 
+    /// transform, scale back to unit scale and rotation of 0 around all angles 
+    /// repsective to the parent transform.
+    /// </summary>
+    /// <param name="transform">Transform.</param>
+    public static void Reset(this Transform transform)
+    {
+        transform.position = new Vector3();
+        transform.rotation = Quaternion.identity;
+        transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+    }
+}
