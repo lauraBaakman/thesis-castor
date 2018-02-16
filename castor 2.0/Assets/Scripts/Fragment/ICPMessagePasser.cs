62725bbca3de36dbee2e5624b8e2dbaa28d73a43
Laura Baakman <laurabaakman@gmail.com>
2018-02-16 13:58:30 +0100

    Notify ICPPointsController if an ICPMessage is passed to the Fragment.

---
+++ castor 2.0/Assets/Scripts/Fragment/ICPMessagePasser.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using System.Collections;
+using Registration;
+using System.Collections.Generic;
+
+namespace Fragment
+{
+    /// <summary>
+    /// Pass ICP messages through to the children of this object.
+    /// </summary>
+    public class ICPMessagePasser : MonoBehaviour, IICPListener
+    {
+        public List<GameObject> listeners = new List<GameObject>();
+
+        #region ICPListener
+        public void OnICPCorrespondencesChanged(ICPCorrespondencesChanged message)
+        {
+            SendMessageToListeners("OnICPCorrespondencesChanged", message, SendMessageOptions.RequireReceiver);
+        }
+
+        public void OnICPPointsSelected(ICPPointsSelectedMessage message)
+        {
+            SendMessageToListeners("OnICPPointsSelected", message, SendMessageOptions.RequireReceiver);
+        }
+
+        public void OnICPTerminated(ICPTerminatedMessage message)
+        {
+            SendMessageToListeners("OnICPTerminated", message, SendMessageOptions.RequireReceiver);
+        }
+
+        public void OnPreparetionStepCompleted()
+        {
+            SendMessageToListeners("OnPreparetionStepCompleted", null, SendMessageOptions.RequireReceiver);
+        }
+
+        public void OnStepCompleted()
+        {
+            SendMessageToListeners("OnStepCompleted", null, SendMessageOptions.RequireReceiver);
+        }
+        #endregion ICPListener
+
+        private void SendMessageToListeners(string methodName, object message, SendMessageOptions option)
+        {
+            foreach (GameObject listener in listeners)
+            {
+                listener.SendMessage(methodName, message, option);
+            }
+        }
+    }
+
+}
+
