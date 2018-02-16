c54a9b505be972fb1f0ed4803953931f821b4825
Laura Baakman <laurabaakman@gmail.com>
2018-02-16 13:12:04 +0100

    Attatched the ICP point controller to the fragment prefab.

---
+++ castor 2.0/Assets/Scripts/Fragment/ICPPointController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+using Registration;
+
+namespace Fragment
+{
+    public class ICPPointController : MonoBehaviour, IICPListener
+    {
+        #region Correspondences
+        public void OnICPCorrespondencesChanged(ICPCorrespondencesChanged message) { }
+        #endregion
+
+        #region Points
+        public void OnICPPointsSelected(ICPPointsSelectedMessage message)
+        {
+            Debug.Log("Received Points!");
+        }
+        #endregion
+
+
+        #region Progress
+        public void OnICPTerminated(ICPTerminatedMessage message) { }
+
+        public void OnPreparetionStepCompleted() { }
+
+        public void OnStepCompleted() { }
+        #endregion
+    }
+
+}
+
