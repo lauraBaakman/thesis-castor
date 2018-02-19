0d6380e9b0b9a5884659331be351ac34bcd5be3d
Laura Baakman <laurabaakman@gmail.com>
2018-02-19 14:32:31 +0100

    Added face class.

---
+++ castor 2.0/Assets/Scripts/DCEL/Editor/TestFace.cs
@@ -0,0 +1,160 @@
+using UnityEngine;
+using NUnit.Framework;
+
+using DoubleConnectedEdgeList;
+using System;
+
+[TestFixture]
+public class FaceTest
+{
+    [Test]
+    public void TestEquals_EqualsSameOrder()
+    {
+        HalfEdge a = TestAux.RandomHalfEdge();
+        HalfEdge b = TestAux.RandomHalfEdge();
+        HalfEdge c = TestAux.RandomHalfEdge();
+        HalfEdge d = TestAux.RandomHalfEdge();
+
+        Face thisFace = new Face();
+        thisFace.AddOuterComponent(a);
+        thisFace.AddOuterComponent(b);
+        thisFace.AddOuterComponent(c);
+        thisFace.AddOuterComponent(d);
+
+        Face otherFace = new Face();
+        otherFace.AddOuterComponent(a);
+        otherFace.AddOuterComponent(b);
+        otherFace.AddOuterComponent(c);
+        otherFace.AddOuterComponent(d);
+
+        Assert.IsTrue(thisFace.Equals(otherFace));
+        Assert.IsTrue(otherFace.Equals(thisFace));
+        Assert.AreEqual(thisFace.GetHashCode(), otherFace.GetHashCode());
+    }
+
+    [Test]
+    public void TestEquals_EqualsDifferentOrder()
+    {
+        HalfEdge a = TestAux.RandomHalfEdge();
+        HalfEdge b = TestAux.RandomHalfEdge();
+        HalfEdge c = TestAux.RandomHalfEdge();
+        HalfEdge d = TestAux.RandomHalfEdge();
+
+        Face thisFace = new Face();
+        thisFace.AddOuterComponent(a);
+        thisFace.AddOuterComponent(b);
+        thisFace.AddOuterComponent(c);
+        thisFace.AddOuterComponent(d);
+
+        Face otherFace = new Face();
+        otherFace.AddOuterComponent(b);
+        otherFace.AddOuterComponent(a);
+        otherFace.AddOuterComponent(d);
+        otherFace.AddOuterComponent(c);
+
+        Assert.IsTrue(thisFace.Equals(otherFace));
+        Assert.IsTrue(otherFace.Equals(thisFace));
+        Assert.AreEqual(thisFace.GetHashCode(), otherFace.GetHashCode());
+    }
+
+    [Test]
+    public void TestEquals_EqualsAddSameEdgeTwice()
+    {
+        HalfEdge a = TestAux.RandomHalfEdge();
+        HalfEdge b = TestAux.RandomHalfEdge();
+        HalfEdge c = TestAux.RandomHalfEdge();
+        HalfEdge d = TestAux.RandomHalfEdge();
+
+        Face thisFace = new Face();
+        thisFace.AddOuterComponent(a);
+        thisFace.AddOuterComponent(b);
+        thisFace.AddOuterComponent(c);
+        thisFace.AddOuterComponent(d);
+        thisFace.AddOuterComponent(d);
+        thisFace.AddOuterComponent(d);
+
+        Face otherFace = new Face();
+        otherFace.AddOuterComponent(b);
+        otherFace.AddOuterComponent(a);
+        otherFace.AddOuterComponent(d);
+        otherFace.AddOuterComponent(c);
+
+        Assert.IsTrue(thisFace.Equals(otherFace));
+        Assert.IsTrue(otherFace.Equals(thisFace));
+        Assert.AreEqual(thisFace.GetHashCode(), otherFace.GetHashCode());
+    }
+
+    [Test]
+    public void TestEquals_NotEqualThisGreater()
+    {
+        HalfEdge a = TestAux.RandomHalfEdge();
+        HalfEdge b = TestAux.RandomHalfEdge();
+        HalfEdge c = TestAux.RandomHalfEdge();
+        HalfEdge d = TestAux.RandomHalfEdge();
+
+        Face thisFace = new Face();
+        thisFace.AddOuterComponent(a);
+        thisFace.AddOuterComponent(b);
+        thisFace.AddOuterComponent(c);
+        thisFace.AddOuterComponent(d);
+
+        Face otherFace = new Face();
+        otherFace.AddOuterComponent(a);
+        otherFace.AddOuterComponent(b);
+        otherFace.AddOuterComponent(d);
+
+        Assert.IsFalse(thisFace.Equals(otherFace));
+        Assert.IsFalse(otherFace.Equals(thisFace));
+        Assert.AreNotEqual(thisFace.GetHashCode(), otherFace.GetHashCode());
+    }
+
+    [Test]
+    public void TestEquals_NotEqualThisSmaller()
+    {
+        HalfEdge a = TestAux.RandomHalfEdge();
+        HalfEdge b = TestAux.RandomHalfEdge();
+        HalfEdge c = TestAux.RandomHalfEdge();
+        HalfEdge d = TestAux.RandomHalfEdge();
+
+        Face thisFace = new Face();
+        thisFace.AddOuterComponent(a);
+        thisFace.AddOuterComponent(c);
+        thisFace.AddOuterComponent(d);
+
+        Face otherFace = new Face();
+        otherFace.AddOuterComponent(a);
+        otherFace.AddOuterComponent(b);
+        otherFace.AddOuterComponent(c);
+        otherFace.AddOuterComponent(d);
+
+        Assert.IsFalse(thisFace.Equals(otherFace));
+        Assert.IsFalse(otherFace.Equals(thisFace));
+        Assert.AreNotEqual(thisFace.GetHashCode(), otherFace.GetHashCode());
+    }
+
+    [Test]
+    public void TestEquals_NotEqualSameSizeDifferentEdges()
+    {
+        HalfEdge a = TestAux.RandomHalfEdge();
+        HalfEdge b = TestAux.RandomHalfEdge();
+        HalfEdge c = TestAux.RandomHalfEdge();
+        HalfEdge d = TestAux.RandomHalfEdge();
+        HalfEdge e = TestAux.RandomHalfEdge();
+
+        Face thisFace = new Face();
+        thisFace.AddOuterComponent(a);
+        thisFace.AddOuterComponent(b);
+        thisFace.AddOuterComponent(c);
+        thisFace.AddOuterComponent(d);
+
+        Face otherFace = new Face();
+        otherFace.AddOuterComponent(a);
+        otherFace.AddOuterComponent(b);
+        otherFace.AddOuterComponent(e);
+        otherFace.AddOuterComponent(d);
+
+        Assert.IsFalse(thisFace.Equals(otherFace));
+        Assert.IsFalse(otherFace.Equals(thisFace));
+        Assert.AreNotEqual(thisFace.GetHashCode(), otherFace.GetHashCode());
+    }
+}
---
+++ castor 2.0/Assets/Scripts/DCEL/Face.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using UnityEngine;
+
+namespace DoubleConnectedEdgeList
+{
+    public class Face : IEquatable<Face>
+    {
+
+        public ReadOnlyCollection<HalfEdge> OuterComponents
+        {
+            get { return outerComponents.AsReadOnly(); }
+        }
+        private List<HalfEdge> outerComponents;
+
+        public Face()
+        {
+            this.outerComponents = new List<HalfEdge>();
+        }
+
+        public void AddOuterComponent(HalfEdge edge)
+        {
+            if (!OuterComponents.Contains(edge)) outerComponents.Add(edge);
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 17;
+            foreach (HalfEdge edge in OuterComponents)
+            {
+                hash *= (31 + edge.NonRecursiveGetHashCode());
+            }
+            return hash;
+        }
+
+        public int NonRecursiveGetHashCode()
+        {
+            int hash = 17;
+            foreach (HalfEdge edge in OuterComponents)
+            {
+                hash *= (31 + edge.Origin.Position.GetHashCode());
+                if (edge.HasDestination) hash *= (31 + edge.Destination.Position.GetHashCode());
+            }
+            return hash;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[Face]");
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            return this.Equals(obj as Face);
+        }
+
+        public bool Equals(Face other)
+        {
+            Debug.Log("Face:Equals");
+            IEnumerable<HalfEdge> inThisButNotInOther = this.OuterComponents.Except(other.OuterComponents);
+            IEnumerable<HalfEdge> inOtherButNotInThis = other.OuterComponents.Except(this.OuterComponents);
+
+            return (
+                !inThisButNotInOther.Any() &&
+                !inOtherButNotInThis.Any()
+            );
+        }
+
+        public bool NonRecursiveEquals(Face other)
+        {
+            Debug.Log("Face:NonRecursiveEquals");
+            IEnumerable<HalfEdge> inThisButNotInOther = this.OuterComponents.Except(other.OuterComponents);
+            IEnumerable<HalfEdge> inOtherButNotInThis = other.OuterComponents.Except(this.OuterComponents);
+
+            return (
+                !inThisButNotInOther.Any() &&
+                !inOtherButNotInThis.Any()
+            );
+        }
+    }
+}
