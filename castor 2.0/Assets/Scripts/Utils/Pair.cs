3ef066ae7335bc5910169cfd0f07e2d16484e2b4
Laura Baakman <laurabaakman@gmail.com>
2018-02-19 15:30:07 +0100

    Added Equals and GetHashCode to pair.

--- castor 2.0/Assets/Scripts/Utils/Pair.cs
+++ castor 2.0/Assets/Scripts/Utils/Pair.cs
@@ -1,6 +1,8 @@
+using System;
+
 namespace Utils
 {
-    public class Pair<T, U>
+    public class Pair<T, U> : IEquatable<Pair<T, U>>
     {
         public T First { get; set; }
         public U Second { get; set; }
@@ -11,6 +13,34 @@ namespace Utils
         {
             this.First = first;
             this.Second = second;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[Pair: First={0}, Second={1}]", First, Second);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+            return this.Equals(obj as Pair<T, U>);
+        }
+
+        public bool Equals(Pair<T, U> other)
+        {
+            return (
+                this.First.Equals(other.First) &&
+                this.Second.Equals(other.Second)
+            );
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 17;
+            hash *= (31 + First.GetHashCode());
+            hash *= (31 + Second.GetHashCode());
+            return hash;
         }
     }
 }
