50022afecbc6b356093a1784ef2c25fda798d510
Laura Baakman <laurabaakman@gmail.com>
2018-02-21 21:37:09 +0100

    Extended IEnumerable with functionality that allows unordered compariosn of lists.

---
+++ castor 2.0/Assets/Scripts/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+public static class IEnumerable
+{
+    public static bool UnorderedElementsAreEqual<T>(this IEnumerable<T> thisList, IEnumerable<T> otherList) where T : IEquatable<T>
+    {
+        IEnumerable<T> inThisButNotInOther = thisList.Except(otherList);
+        IEnumerable<T> inOtherButNotInThis = otherList.Except(thisList);
+
+        return (
+            !inThisButNotInOther.Any() &&
+            !inOtherButNotInThis.Any()
+        );
+    }
+
+    public static bool UnorderedElementsAreEqual<T>(this IEnumerable<T> thisList, IEnumerable<T> otherList, IEqualityComparer<T> comparer)
+    {
+        IEnumerable<T> inThisButNotInOther = thisList.Except(otherList, comparer);
+        IEnumerable<T> inOtherButNotInThis = otherList.Except(thisList, comparer);
+
+        return (
+            !inThisButNotInOther.Any() &&
+            !inOtherButNotInThis.Any()
+        );
+    }
+}
