b7dd10ddcfa6e0e9b25e19c1e753742a437db6fe
Laura Baakman <laurabaakman@gmail.com>
2018-02-21 23:02:07 +0100

    Added an Random method to the Unity.Color.

--- castor 2.0/Assets/Scripts/Extensions/ColorExtensions.cs
+++ castor 2.0/Assets/Scripts/Extensions/ColorExtensions.cs
@@ -1,17 +1,42 @@
 using UnityEngine;
-using System.Collections;
 
-public class NewMonoBehaviour : MonoBehaviour
+public static class ColorExtensions
 {
-	// Use this for initialization
-	void Start()
-	{
+    private static System.Random random = new System.Random();
 
-	}
+    public static Color Random(
+        this Color color,
+        float hueMin = 0, float hueMax = 1,
+        float saturationMin = 0, float saturationMax = 1,
+        float valueMin = 0, float valueMax = 1,
+        float alphaMin = 0, float alphaMax = 1
+    )
+    {
+        Color rgbColor = RandomRGBColor(
+            hueMin, hueMax,
+            saturationMin, saturationMax,
+            valueMin, valueMax
+        );
+        color.r = rgbColor.r;
+        color.g = rgbColor.g;
+        color.b = rgbColor.b;
 
-	// Update is called once per frame
-	void Update()
-	{
-			
-	}
+        float alpha = random.NextInRange(alphaMin, alphaMax);
+        color.a = alpha;
+
+        return color;
+    }
+
+    private static Color RandomRGBColor(
+        float hueMin, float hueMax,
+        float saturationMin, float saturationMax,
+        float valueMin, float valueMax)
+    {
+        float hue = random.NextInRange(hueMin, hueMax);
+        float saturation = random.NextInRange(saturationMin, saturationMax);
+        float val = random.NextInRange(valueMin, valueMax);
+        Color color = Color.HSVToRGB(hue, saturation, val);
+
+        return color;
+    }
 }
--- castor 2.0/Assets/Scripts/Extensions/Editor/TestColorExtensions.cs
+++ castor 2.0/Assets/Scripts/Extensions/Editor/TestColorExtensions.cs
@@ -1,17 +1,62 @@
+using NUnit.Framework;
 using UnityEngine;
-using System.Collections;
 
-public class NewMonoBehaviour : MonoBehaviour
+[TestFixture]
+public class ColorExtensionsTests
 {
-	// Use this for initialization
-	void Start()
-	{
+    private System.Random randomGenerator;
 
-	}
+    [SetUp]
+    public void SetUp()
+    {
+        randomGenerator = new System.Random();
+    }
 
-	// Update is called once per frame
-	void Update()
-	{
-			
-	}
+    [Test, Repeat(100)]
+    public void TestRandom()
+    {
+        float hueMax, hueMin;
+        GenerateRandomRange(out hueMin, out hueMax);
+
+        float saturationMax, saturationMin;
+        GenerateRandomRange(out saturationMin, out saturationMax);
+
+        float valueMax, valueMin;
+        GenerateRandomRange(out valueMin, out valueMax);
+
+        float alphaMax, alphaMin;
+        GenerateRandomRange(out alphaMin, out alphaMax);
+
+        Color color = new Color().Random(
+            hueMin, hueMax,
+            saturationMin, saturationMax,
+            valueMin, valueMax,
+            alphaMin, alphaMax
+        );
+
+        float hue, saturation, val, alpha;
+        Color.RGBToHSV(color, out hue, out saturation, out val);
+        alpha = color.a;
+
+        Assert.That(hue, Is.GreaterThanOrEqualTo(hueMin));
+        Assert.That(hue, Is.LessThan(hueMax));
+
+        Assert.That(saturation, Is.GreaterThanOrEqualTo(saturationMin));
+        Assert.That(saturation, Is.LessThan(saturationMax));
+
+        Assert.That(val, Is.GreaterThanOrEqualTo(valueMin));
+        Assert.That(val, Is.LessThan(valueMax));
+
+        Assert.That(alpha, Is.GreaterThanOrEqualTo(alphaMin));
+        Assert.That(alpha, Is.LessThan(alphaMax));
+    }
+
+    private void GenerateRandomRange(out float min, out float max)
+    {
+        min = (float)randomGenerator.NextDouble();
+        do
+        {
+            max = (float)randomGenerator.NextDouble();
+        } while (max < min);
+    }
 }
