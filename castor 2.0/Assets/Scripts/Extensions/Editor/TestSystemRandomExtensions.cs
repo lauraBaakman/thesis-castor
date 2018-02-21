d62703ac4d79147d6775e534d3c5688f8ba6ee6e
Laura Baakman <laurabaakman@gmail.com>
2018-02-21 22:50:18 +0100

    extend system.Random to generate colors in a range.

---
+++ castor 2.0/Assets/Scripts/Extensions/Editor/TestSystemRandomExtensions.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+
+[TestFixture]
+public class SystemRandomExtensionTests
+{
+    [Test, Repeat(100)]
+    public void TestNextInRange()
+    {
+        System.Random random = new System.Random();
+        float a = random.NextInRange(0.3f, 0.5f);
+        float b = random.NextInRange(0.3f, 0.5f);
+
+        Assert.AreNotEqual(a, b);
+        Assert.That(a, Is.InRange(0.3f, 0.5f));
+        Assert.That(b, Is.InRange(0.3f, 0.5f));
+    }
+
+    [Test]
+    public void TestNextInRange_ExceptionIsThrown()
+    {
+        Assert.Throws(typeof(System.ArgumentException), new TestDelegate(TestNextInRange_ExceptionIsThrown_Helper));
+    }
+
+    public void TestNextInRange_ExceptionIsThrown_Helper(){
+        System.Random random = new System.Random();
+        float a = random.NextInRange(0.5f, 0.3f);        
+    }
+}
---
+++ castor 2.0/Assets/Scripts/Extensions/SystemRandomExtensions.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public static class SystemRandomExtensions
+{
+    static System.Random randomGenerator = new System.Random();
+
+    /// <summary>
+    /// Generate a random float in the range [min, max).
+    /// </summary>
+    /// <param name="random">Random.</param>
+    /// <param name="min">The minimum generated value, inclusive.</param>
+    /// <param name="max">The max generated value, exclusive.</param>
+    public static float NextInRange(this System.Random random, float min, float max)
+    {
+        if (min > max) throw new System.ArgumentException("Max should be greater than min.");
+
+        float value = (float) randomGenerator.NextDouble();
+
+        return min + (value * (max - min));
+    }
+}
