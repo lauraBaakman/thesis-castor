320daafdca7b4f20c78a708d73fbf30bb917547c
Laura Baakman <laurabaakman@gmail.com>
2018-02-22 10:31:26 +0100

    Renamed file.

---
+++ castor 2.0/Assets/Scripts/Fragment/DoubleConnectedEdgeListStorage.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+namespace Fragment
+{
+    public class DoubleConnectedEdgeListStorage : MonoBehaviour
+    {
+        void Start()
+        {
+            Debug.Log("DoubleConnectedEdgeListStorage: Start");
+        }
+
+        void Update()
+        {
+            Debug.Log("DoubleConnectedEdgeListStorage: Update");
+        }
+    }
+}
+
+
