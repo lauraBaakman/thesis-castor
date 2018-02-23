100418031b3fb886320c63c0a2840693028e0bfb
Laura Baakman <laurabaakman@gmail.com>
2018-02-23 15:29:50 +0100

    Extract model points and static points into properties of the ICPPreparationStepCompletedMessage, and replace getters with readonly properties.

--- castor 2.0/Assets/Scripts/Registration/ICPMessages.cs
+++ castor 2.0/Assets/Scripts/Registration/ICPMessages.cs
@@ -1,26 +1,38 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace Registration
 {
     public class ICPPreparationStepCompletedMessage
     {
-        public Transform Transform
-        {
-            get { return transform; }
-        }
-        private readonly Transform transform;
+        public readonly Transform Transform;
 
-        public List<Correspondence> Correspondences
-        {
-            get { return correspondences; }
-        }
-        private readonly List<Correspondence> correspondences;
+        public readonly ReadOnlyCollection<Correspondence> Correspondences;
+        public readonly ReadOnlyCollection<Point> ModelPoints;
+        public readonly ReadOnlyCollection<Point> StaticPoints;
 
         public ICPPreparationStepCompletedMessage(List<Correspondence> correspondences, Transform transform)
         {
-            this.correspondences = correspondences;
-            this.transform = transform;
+            this.Correspondences = correspondences.AsReadOnly();
+            this.Transform = transform;
+
+            List<Point> modelPoints = new List<Point>(correspondences.Count);
+            List<Point> staticPoints = new List<Point>(correspondences.Count);
+
+            ExtractPoints(correspondences, ref modelPoints, ref staticPoints);
+
+            this.ModelPoints = modelPoints.AsReadOnly();
+            this.StaticPoints = staticPoints.AsReadOnly();
+        }
+
+        private void ExtractPoints(List<Correspondence> correspondenceList, ref List<Point> modelPoints, ref List<Point> staticPoints)
+        {
+            foreach (Correspondence correspondence in correspondenceList)
+            {
+                modelPoints.Add(correspondence.ModelPoint);
+                staticPoints.Add(correspondence.StaticPoint);
+            }
         }
     }
 
