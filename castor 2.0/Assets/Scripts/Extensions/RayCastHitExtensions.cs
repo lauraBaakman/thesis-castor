c139127f71ce0f67dde80ca68095c21da3acf8a6
Laura Baakman <laurabaakman@gmail.com>
2018-02-26 16:52:09 +0100

    Mark the RTEditor as the main camera.

---
+++ castor 2.0/Assets/Scripts/Extensions/RayCastHitExtensions.cs
@@ -0,0 +1,22 @@
+using Registration;
+using UnityEngine;
+
+public static class RayCastHitExtensions
+{
+    /// <summary>
+    /// To a Point in the passed transform.
+    /// </summary>
+    /// <returns>The point.</returns>
+    /// <param name="rayCastHit">Ray cast hit.</param>
+    /// <param name="pointTransform">The transform of the point.</param>
+    public static Point ToPoint(this RaycastHit rayCastHit, Transform pointTransform)
+    {
+        Vector3 positonInPointTransform = pointTransform.InverseTransformPoint(rayCastHit.point);
+        Vector3 normalInPointTransform = pointTransform.InverseTransformDirection(rayCastHit.normal);
+
+        return new Point(
+            position: positonInPointTransform,
+            normal: normalInPointTransform
+        );
+    }
+}
