3157985fe940773e8eb1a269b97a635fa7c9e9df
Laura Baakman <laurabaakman@gmail.com>
2018-02-27 09:47:05 +0100

    Find the correspondences and remove debug code.

--- castor 2.0/Assets/Scripts/Registration/Correspondences/NormalShootingCorrespondenceFinder.cs
+++ castor 2.0/Assets/Scripts/Registration/Correspondences/NormalShootingCorrespondenceFinder.cs
@@ -11,10 +11,27 @@ namespace Registration
     /// 
     /// This method shoots rays from the staticpoints, along the normal of that 
     /// point, if that ray intersects with the model fragment the intersection 
-    /// point and the static point form a correspondence.
+    /// point and the static point form a correspondence, if the distance 
+    /// between the two points is smaller than MaxDistance.
     /// </summary>
     public class NormalShootingCorrespondenceFinder : ICorrespondenceFinder
     {
+        /// <summary>
+        /// The maximum distance between a static point and the intersection point on the model fragment.
+        /// </summary>
+        private readonly float MaxDistance;
+
+        /// <summary>
+        /// The reference transform.
+        /// </summary>
+        private readonly Transform ReferenceTransform;
+
+        public NormalShootingCorrespondenceFinder(Settings settings)
+        {
+            MaxDistance = settings.MaxWithinCorrespondenceDistance;
+            ReferenceTransform = settings.ReferenceTransform;
+        }
+
         public List<Correspondence> Find(ReadOnlyCollection<Point> staticPoints, ReadOnlyCollection<Point> modelPoints)
         {
             throw new System.ArgumentException("The NormalShootingCorrespondenceFinder cannot find correspondences between two sets of points.");
@@ -23,9 +40,6 @@ namespace Registration
         public List<Correspondence> Find(ReadOnlyCollection<Point> staticPoints, SamplingInformation modelSamplingInformation)
         {
             Correspondence correspondence;
-
-            Debug.Log("NormalShootingCorrespondenceFinder:Find");
-
             List<Correspondence> correspondences = new List<Correspondence>();
             foreach (Point staticPoint in staticPoints)
             {
@@ -37,20 +51,28 @@ namespace Registration
             return correspondences;
         }
 
-        private Correspondence FindCorrespondence(Point staticPoint, SamplingInformation modelSamplingInformation)
+        private Correspondence FindCorrespondence(Point staticPoint, SamplingInformation model)
         {
-            Point intersection = FindIntersection(staticPoint, modelSamplingInformation);
-            if (intersection == null) return null;
+            Point intersectionInReferenceTransform = FindIntersection(staticPoint, model);
 
-            //TODO convert intersection to model point;
-            Point modelPoint = null;
+            if (intersectionInReferenceTransform == null) return null;
 
-            return new Correspondence(staticPoint: staticPoint, modelPoint: modelPoint);
+            return new Correspondence(
+                staticPoint: staticPoint,
+                modelPoint: intersectionInReferenceTransform
+            );
         }
 
-        private Point FindIntersection(Point staticPoint, SamplingInformation modelSamplingInformation)
+        private Point FindIntersection(Point staticPoint, SamplingInformation model)
         {
-            return null;
+            Ray rayInWorldSpace = staticPoint.ToWorldSpaceRay(model.Transform);
+
+            RaycastHit hit;
+            bool collided = model.Collider.Raycast(rayInWorldSpace, out hit, MaxDistance);
+
+            if (!collided) return null;
+
+            return hit.ToPoint(ReferenceTransform);
         }
     }
 }
