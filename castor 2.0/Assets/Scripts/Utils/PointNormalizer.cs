fd7b0627a78a0826b04ea67c48b2516dd2e4d7ae
Laura Baakman <laurabaakman@gmail.com>
2018-03-01 14:34:14 +0100

    Compute the translation.

--- castor 2.0/Assets/Scripts/Utils/PointNormalizer.cs
+++ castor 2.0/Assets/Scripts/Utils/PointNormalizer.cs
@@ -13,7 +13,7 @@ namespace Utils
     {
         public Matrix4x4 ComputeNormalizationMatrix(IEnumerable<Point> points)
         {
-            throw new System.NotImplementedException();
+            return new _PointNormalizer(points).NormalizationMatrix;
         }
 
         public IEnumerable<Point> Normalize(IEnumerable<Point> points)
@@ -22,4 +22,59 @@ namespace Utils
         }
     }
 
+    internal class _PointNormalizer
+    {
+        private static Vector3 origin = new Vector3(0, 0, 0);
+
+        IEnumerable<Point> Points;
+
+        RangeF xRange = new RangeF();
+        RangeF yRange = new RangeF();
+        RangeF zRange = new RangeF();
+
+        public readonly Matrix4x4 NormalizationMatrix;
+
+        public _PointNormalizer(IEnumerable<Point> points)
+        {
+            this.Points = points;
+
+            NormalizationMatrix = new Matrix4x4();
+            NormalizationMatrix.SetTRS(
+                pos: ComputeTranslation(),
+                q: Quaternion.identity,
+                s: ComputeScale()
+            );
+        }
+
+        private Vector3 ComputeTranslation()
+        {
+            Vector3 center = ComputeCenter();
+            return origin - center;
+        }
+
+        private Vector3 ComputeCenter()
+        {
+            ComputeRange(Points, 0, ref xRange);
+            ComputeRange(Points, 1, ref yRange);
+            ComputeRange(Points, 2, ref zRange);
+
+            return new Vector3(
+                xRange.Center, yRange.Center, zRange.Center
+            );
+        }
+
+        private void ComputeRange(IEnumerable<Point> points, int idx, ref RangeF range)
+        {
+            foreach (Point point in points)
+            {
+                range.UpdateMin(point.Position[idx]);
+                range.UpdateMax(point.Position[idx]);
+            }
+        }
+
+        private Vector3 ComputeScale()
+        {
+            return new Vector3(1, 1, 1);
+        }
+    }
 }
