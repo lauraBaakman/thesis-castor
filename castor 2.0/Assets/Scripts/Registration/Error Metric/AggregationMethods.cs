ff3f3d9e8bd4545549fe46754736e0768f363568
Laura Baakman <laurabaakman@gmail.com>
2018-03-06 16:35:02 +0100

    Add docstrings.

--- castor 2.0/Assets/Scripts/Registration/Error Metric/AggregationMethods.cs
+++ castor 2.0/Assets/Scripts/Registration/Error Metric/AggregationMethods.cs
@@ -9,11 +9,25 @@ namespace Registration
         {
             public delegate float AggregationMethod(IEnumerable<float> errors);
 
+            /// <summary>
+            /// Computes the sum of the specified errors.
+            /// 
+            /// If an empty list is input the resulting error is 0.0.
+            /// </summary>
+            /// <returns>The sum of the errors</returns>
+            /// <param name="errors">The errors.</param>
             public static float Sum(IEnumerable<float> errors)
             {
                 return errors.Sum();
             }
 
+            /// <summary>
+            /// Computes the mean the specified errors.
+            /// 
+            /// If an empty list is input the resulting error is 0.0.
+            /// </summary>
+            /// <returns>The mean of the erros.</returns>
+            /// <param name="errors">The errors</param>
             public static float Mean(IEnumerable<float> errors)
             {
                 return errors.Average();
