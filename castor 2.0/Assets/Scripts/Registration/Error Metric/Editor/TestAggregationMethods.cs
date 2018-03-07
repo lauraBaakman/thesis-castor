2fe4a15ffddc5ad5cb680085a4fa29b012d15a51
Laura Baakman <laurabaakman@gmail.com>
2018-03-07 10:04:28 +0100

    Add namespaces within tests.

--- castor 2.0/Assets/Scripts/Registration/Error Metric/Editor/TestAggregationMethods.cs
+++ castor 2.0/Assets/Scripts/Registration/Error Metric/Editor/TestAggregationMethods.cs
@@ -4,59 +4,65 @@ using Registration.Error;
 
 namespace Tests
 {
-    [TestFixture]
-    public class SumAggregationMethodTests
+    namespace Registration
     {
-        private static double tolerance = 0.0001;
-
-        [Test]
-        public void TestSum_NormalList()
+        namespace Error
         {
-            List<float> errors = new List<float> { 1, 2, 3, 4, 5.5f };
+            [TestFixture]
+            public class SumAggregationMethodTests
+            {
+                private static double tolerance = 0.0001;
 
-            float expected = 15.5f;
-            float actual = AggregationMethods.Sum(errors);
+                [Test]
+                public void TestSum_NormalList()
+                {
+                    List<float> errors = new List<float> { 1, 2, 3, 4, 5.5f };
 
-            Assert.That(actual, Is.EqualTo(expected).Within(tolerance));
-        }
+                    float expected = 15.5f;
+                    float actual = AggregationMethods.Sum(errors);
 
-        [Test]
-        public void TestSum_EmptyList()
-        {
-            List<float> errors = new List<float>();
+                    Assert.That(actual, Is.EqualTo(expected).Within(tolerance));
+                }
 
-            float expected = 0.0f;
-            float actual = AggregationMethods.Sum(errors);
+                [Test]
+                public void TestSum_EmptyList()
+                {
+                    List<float> errors = new List<float>();
 
-            Assert.That(actual, Is.EqualTo(expected).Within(tolerance));
-        }
-    }
+                    float expected = 0.0f;
+                    float actual = AggregationMethods.Sum(errors);
 
-    [TestFixture]
-    public class MeanAggregationMethodTests
-    {
-        private static double tolerance = 0.0001;
+                    Assert.That(actual, Is.EqualTo(expected).Within(tolerance));
+                }
+            }
 
-        [Test]
-        public void TestMean_NormalList()
-        {
-            List<float> errors = new List<float> { 1, 2, 3, 4, 5.5f };
+            [TestFixture]
+            public class MeanAggregationMethodTests
+            {
+                private static double tolerance = 0.0001;
 
-            float expected = 3.1f;
-            float actual = AggregationMethods.Mean(errors);
+                [Test]
+                public void TestMean_NormalList()
+                {
+                    List<float> errors = new List<float> { 1, 2, 3, 4, 5.5f };
 
-            Assert.That(actual, Is.EqualTo(expected).Within(tolerance));
-        }
+                    float expected = 3.1f;
+                    float actual = AggregationMethods.Mean(errors);
 
-        [Test]
-        public void TestMean_EmptyList()
-        {
-            List<float> errors = new List<float>();
+                    Assert.That(actual, Is.EqualTo(expected).Within(tolerance));
+                }
 
-            float expected = 0.0f;
-            float actual = AggregationMethods.Mean(errors);
+                [Test]
+                public void TestMean_EmptyList()
+                {
+                    List<float> errors = new List<float>();
 
-            Assert.That(actual, Is.EqualTo(expected).Within(tolerance));
+                    float expected = 0.0f;
+                    float actual = AggregationMethods.Mean(errors);
+
+                    Assert.That(actual, Is.EqualTo(expected).Within(tolerance));
+                }
+            }
         }
     }
 }
