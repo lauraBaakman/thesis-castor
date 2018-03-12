b2422d035b6c3c4c4049acf5d53c915ac60864c4
Laura Baakman <laurabaakman@gmail.com>
2018-03-12 10:53:52 +0100

    Fixed SVD method and its tests.

--- castor 2.0/Assets/Scripts/Utils/ArrayMatrixUtils.cs
+++ castor 2.0/Assets/Scripts/Utils/ArrayMatrixUtils.cs
@@ -69,7 +69,28 @@ namespace Utils
                 w: out singularValues, vt: out Vt, u: out U
             );
 
-            S = ToDiagonalMatrix(singularValues);
+            S = ToDiagonalMatrix(singularValues, A.GetLength(0), A.GetLength(1));
+        }
+
+        /// <summary>
+        /// Computes the pseudo inverse by taking the inverse of the non-zero 
+        /// elements and leaving the zero elements unchanged.
+        /// </summary>
+        /// <returns>The inverse.</returns>
+        public static double[,] PseudoInverseOfDiagonalMatrix(double[,] matrix)
+        {
+            if (matrix.GetLength(0) != matrix.GetLength(1)) throw new System.ArgumentException("Input matrix needs to be square.");
+
+            double[,] pseudoInverse = new double[matrix.GetLength(0), matrix.GetLength(1)];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                if (System.Math.Abs(matrix[i, i]) > EPSILON)
+                {
+                    pseudoInverse[i, i] = 1.0f / matrix[i, i];
+                }
+            }
+            return pseudoInverse;
         }
     }
 }
--- castor 2.0/Assets/Scripts/Utils/Editor/TestArrayMatrixUtils.cs
+++ castor 2.0/Assets/Scripts/Utils/Editor/TestArrayMatrixUtils.cs
@@ -1,5 +1,6 @@
 using System;
 using NUnit.Framework;
+using UnityEngine;
 using Utils;
 
 namespace Tests.Utils
@@ -97,6 +98,73 @@ namespace Tests.Utils
             double[,] array = { { 2, 0, 0 }, { 0, 0, 0 } };
 
             ArrayMatrixUtils.PseudoInverseOfDiagonalMatrix(array);
+        }
+
+        [Test]
+        public void Test_SVD_Square_Matrix()
+        {
+            double[,] A = {
+                {-29.809999999999999, -84.807000000000002, -63.218000000000004},
+                {+02.650000000000000, -52.017000000000003, -52.009000000000000},
+                {-19.638000000000002, -75.335999999999999, -16.547000000000001},
+            };
+
+            double[,] expected_U = {
+                {-0.729908087739305, -0.123505763730895, -0.672294957424195},
+                {-0.465764528149259, -0.630004873055934, +0.621415532670109},
+                {-0.500297499264836, +0.766707366764547, +0.402321048390866},
+            };
+            double[,] expected_S = {
+                {149.7913869700865, 000.0000000000000, 000.0000000000000},
+                {000.0000000000000, 034.0373717936407, 000.0000000000000},
+                {000.0000000000000, 000.0000000000000, 015.2993660966482},
+            };
+            double[,] expected_Vt = {
+                {+0.202609288827379, +0.826612287707770, +0.525034857786977},
+                {-0.383237737754462, -0.426459172115321, +0.819305444190112},
+                {+0.901153878336938, -0.367212064520276, +0.230384433564968},
+            };
+
+            double[,] actual_U, actual_S, actual_Vt;
+
+            ArrayMatrixUtils.SVD(A, out actual_U, out actual_S, out actual_Vt);
+
+            Assert.That(actual_U, Is.EqualTo(expected_U).Within(precision));
+            Assert.That(actual_S, Is.EqualTo(expected_S).Within(precision));
+            Assert.That(actual_Vt, Is.EqualTo(expected_Vt).Within(precision));
+        }
+
+        [Test]
+        public void Test_SVD_Rectangular_Matrix()
+        {
+            double[,] A = {
+                {79.2200, 03.5700},
+                {95.9500, 84.9100},
+                {65.5700, 93.4000},
+            };
+
+            double[,] expected_U = {
+                {-0.343225213778742, +0.878072041895140, +0.333445560577190},
+                {-0.710225885367531, -0.010313122653119, -0.703898310308420},
+                {-0.614634561654683, -0.478417316516910, +0.627169217098794},
+            };
+            double[,] expected_S = {
+                {180.3990135626532, 000.0000000000000},
+                {000.0000000000000, 056.4255678360587},
+                {000.0000000000000, 000.0000000000000},
+            };
+            double[,] expected_Vt = {
+                {-0.751878076634585, -0.659302174936711},
+                {+0.659302174936711, -0.751878076634585},
+            };
+
+            double[,] actual_U, actual_S, actual_Vt;
+
+            ArrayMatrixUtils.SVD(A, out actual_U, out actual_S, out actual_Vt);
+
+            Assert.That(actual_U, Is.EqualTo(expected_U).Within(precision));
+            Assert.That(actual_S, Is.EqualTo(expected_S).Within(precision));
+            Assert.That(actual_Vt, Is.EqualTo(expected_Vt).Within(precision));
         }
     }
 }
