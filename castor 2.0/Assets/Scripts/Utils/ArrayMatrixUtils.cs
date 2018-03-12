70b78bc48bc8adec161ec50e770e83c915fc55e9
Laura Baakman <laurabaakman@gmail.com>
2018-03-12 09:51:47 +0100

    Build a non-square diagonal matrix.

--- castor 2.0/Assets/Scripts/Utils/ArrayMatrixUtils.cs
+++ castor 2.0/Assets/Scripts/Utils/ArrayMatrixUtils.cs
@@ -6,34 +6,19 @@ namespace Utils
 
         public static double[,] ToDiagonalMatrix(double[] diagonal)
         {
-            int rank = diagonal.Length;
-
-            double[,] matrix = new double[rank, rank];
-
-            for (int i = 0; i < rank; i++) matrix[i, i] = diagonal[i];
-
-            return matrix;
+            return ToDiagonalMatrix(diagonal, diagonal.Length, diagonal.Length);
         }
 
-        /// <summary>
-        /// Computes the pseudo inverse by taking the inverse of the non-zero 
-        /// elements and leaving the zero elements unchanged.
-        /// </summary>
-        /// <returns>The inverse.</returns>
-        public static double[,] PseudoInverseOfDiagonalMatrix(double[,] matrix)
+        public static double[,] ToDiagonalMatrix(double[] diagonal, int numRows, int numCols)
         {
-            if (matrix.GetLength(0) != matrix.GetLength(1)) throw new System.ArgumentException("Input matrix needs to be square.");
+            if (diagonal.Length > numRows) throw new System.ArgumentException("Cannot fit " + diagonal.Length + "elements in a matrix with " + numRows + "rows.");
+            if (diagonal.Length > numCols) throw new System.ArgumentException("Cannot fit " + diagonal.Length + "elements in a matrix with " + numCols + "columns.");
 
-            double[,] pseudoInverse = new double[matrix.GetLength(0), matrix.GetLength(1)];
+            double[,] matrix = new double[numRows, numCols];
 
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                if (System.Math.Abs(matrix[i, i]) > EPSILON)
-                {
-                    pseudoInverse[i, i] = 1.0f / matrix[i, i];
-                }
-            }
-            return pseudoInverse;
+            for (int i = 0; i < diagonal.Length; i++) matrix[i, i] = diagonal[i];
+
+            return matrix;
         }
 
         public static double[,] Transpose(double[,] matrix)
--- castor 2.0/Assets/Scripts/Utils/Editor/TestArrayMatrixUtils.cs
+++ castor 2.0/Assets/Scripts/Utils/Editor/TestArrayMatrixUtils.cs
@@ -26,6 +26,55 @@ namespace Tests.Utils
         }
 
         [Test]
+        public void Test_ToDiagonalMatrix_NonSquareOutput()
+        {
+            double[] diagonal = new double[] { 1, 2, 3 };
+            int numRows = 4;
+            int numCols = 3;
+
+            double[,] expected = new double[,] {
+                {1, 0, 0},
+                {0, 2, 0},
+                {0, 0, 3},
+                {0, 0, 0},
+            };
+
+            double[,] actual = ArrayMatrixUtils.ToDiagonalMatrix(diagonal, numRows, numCols);
+
+            Assert.That(actual, Is.EqualTo(expected).Within(precision));
+        }
+
+        [Test]
+        public void Test_ToDiagonalMatrix_NonSquareOutput_InvalidNumRows()
+        {
+            Assert.Throws(typeof(System.ArgumentException), new TestDelegate(Test_ToDiagonalMatrix_NonSquareOutput_InvalidNumRows_Helper));
+        }
+
+        public void Test_ToDiagonalMatrix_NonSquareOutput_InvalidNumRows_Helper()
+        {
+            double[] diagonal = new double[] { 1, 2, 3 };
+            int numRows = 2;
+            int numCols = 3;
+
+            ArrayMatrixUtils.ToDiagonalMatrix(diagonal, numRows, numCols);
+        }
+
+        [Test]
+        public void Test_ToDiagonalMatrix_NonSquareOutput_InvalidNumCols()
+        {
+            Assert.Throws(typeof(System.ArgumentException), new TestDelegate(Test_ToDiagonalMatrix_NonSquareOutput_InvalidNumCols_Helper));
+        }
+
+        public void Test_ToDiagonalMatrix_NonSquareOutput_InvalidNumCols_Helper()
+        {
+            double[] diagonal = new double[] { 1, 2, 3 };
+            int numRows = 3;
+            int numCols = 2;
+
+            ArrayMatrixUtils.ToDiagonalMatrix(diagonal, numRows, numCols);
+        }
+
+        [Test]
         public void Test_PseudoInverse_ValidArray()
         {
             double[,] array = { { 2, 0 }, { 0, 2 * float.Epsilon } };
