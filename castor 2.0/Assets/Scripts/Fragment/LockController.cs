93f0a970f16263c39add2c6c52ad33a01d7ef71d
Laura Baakman <laurabaakman@gmail.com>
2018-03-13 16:42:30 +0100

    Things

---
+++ castor 2.0/Assets/Scripts/Fragment/LockController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using Fragment;
+
+namespace Fragment
+{
+    [RequireComponent(typeof(StateTracker))]
+    public class LockController : MonoBehaviour
+    {
+        StateTracker stateTracker;
+
+        void Start()
+        {
+            stateTracker = GetComponent<StateTracker>();
+        }
+
+        private void Update()
+        {
+            if (stateTracker.State.Locked && transform.hasChanged) Debug.LogError("The transform of a locked object has changed, that should not be possible!");
+        }
+    }
+}
+
+
