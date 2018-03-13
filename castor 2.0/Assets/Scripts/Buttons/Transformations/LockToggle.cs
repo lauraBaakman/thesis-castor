253ecfdc1981ea69ba491cc92c1fc001148b59fa
Laura Baakman <laurabaakman@gmail.com>
2018-03-13 13:33:33 +0100

    Only enable the lock toggle if a single object is selected.

--- castor 2.0/Assets/Scripts/Buttons/Transformations/LockToggle.cs
+++ castor 2.0/Assets/Scripts/Buttons/Transformations/LockToggle.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
+using RTEditor;
 
 namespace Buttons
 {
-    public class LockToggle : AbstractToggle
+    public class LockToggle : AbstractToggle, Fragments.ISelectionControllerListener
     {
+        public void OnNumberOfSelectedObjectsChanged(int currentCount)
+        {
+            ///The lock/unlocking functionality is only available for single objects
+            Toggle.interactable = (currentCount == 1);
+        }
+
         public override void OnToggleValueChangedAction(bool isOn)
         {
             Debug.Log("LockToggle: OnToggleValueChangedAction");
@@ -11,7 +18,7 @@ namespace Buttons
 
         protected override void OnEnableAction()
         {
-            Debug.Log("LockToggle: OnEnableAction");
+            OnNumberOfSelectedObjectsChanged(EditorObjectSelection.Instance.NumberOfSelectedObjects);
         }
     }
 }
