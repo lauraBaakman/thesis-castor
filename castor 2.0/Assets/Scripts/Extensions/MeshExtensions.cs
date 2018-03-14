429c5a2470bbf568b1dbc2332b579937159177c4
Laura Baakman <laurabaakman@gmail.com>
2018-03-14 16:04:53 +0100

    MeshTransformTests and define a transform method on mesh that uses it.

--- castor 2.0/Assets/Scripts/Extensions/MeshExtensions.cs
+++ castor 2.0/Assets/Scripts/Extensions/MeshExtensions.cs
@@ -5,6 +5,16 @@ public static class MeshExtensions
 {
     public static bool ToFile(this Mesh mesh, string path)
     {
-        throw new NotImplementedException();
+        Debug.Log("Returning True, but should be implemented!");
+
+        return true;
+    }
+
+    public static Mesh Transform(this Mesh mesh, Matrix4x4 transformation)
+    {
+        Utils.MeshTransformer transformer = new Utils.MeshTransformer(transformation);
+        transformer.Transform(mesh);
+
+        return mesh;
     }
 }
---
+++ castor 2.0/Assets/Scripts/Utils/Editor/TestMeshTransformer.cs
@@ -0,0 +1,98 @@
+using System.IO;
+using NUnit.Framework;
+using UnityEngine;
+using Utils;
+using RTEditor;
+
+namespace Tests.Utils
+{
+    [TestFixture]
+    public class MeshTransformerTests
+    {
+        Mesh mesh;
+
+        Quaternion rotation;
+        Vector3 translation, scale;
+
+        private Mesh ReadMesh(string fileName)
+        {
+            string path = BuildPath(fileName);
+            return ObjFileReader.ImportFile(path);
+        }
+
+        private string BuildPath(string fileName)
+        {
+            string objFileDir = Path.Combine(Application.dataPath, "Scripts/Utils/Editor/objFiles");
+            return Path.Combine(objFileDir, fileName + ".obj");
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            mesh = ReadMesh("NoTransform");
+
+            rotation = new Quaternion();
+            rotation.eulerAngles = new Vector3(+45, -45, +90);
+
+            translation = new Vector3(+45, -45, +90);
+            scale = new Vector3(1, 1, 1);
+        }
+
+        [Test]
+        public void Test_Transform_NoTransform()
+        {
+            Matrix4x4 transform = Matrix4x4.identity;
+            MeshTransformer transformer = new MeshTransformer(transform);
+
+            Mesh actual = transformer.Transform(mesh);
+
+            Mesh expected = ReadMesh("NoTransform");
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Test_Transform_OnlyTranslation()
+        {
+            Matrix4x4 transform = new Matrix4x4().SetTranslation(translation);
+            MeshTransformer transformer = new MeshTransformer(transform);
+
+            Mesh actual = transformer.Transform(mesh);
+
+            Mesh expected = ReadMesh("Translation");
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Test_Transform_OnlyRotation()
+        {
+            Matrix4x4 transform = new Matrix4x4().SetRotation(rotation);
+
+            MeshTransformer transformer = new MeshTransformer(transform);
+
+            Mesh actual = transformer.Transform(mesh);
+
+            Mesh expected = ReadMesh("Rotation");
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Test_Transform_TranslationAndRotation()
+        {
+            Matrix4x4 transform = new Matrix4x4();
+            transform.SetTRS(pos: translation,
+                             q: rotation,
+                             s: scale);
+
+            MeshTransformer transformer = new MeshTransformer(transform);
+
+            Mesh actual = transformer.Transform(mesh);
+
+            Mesh expected = ReadMesh("TranslationRotation");
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
