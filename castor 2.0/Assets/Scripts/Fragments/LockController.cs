c1e5b90999868eb05d5d775db59ee9a4d633df72
Laura Baakman <laurabaakman@gmail.com>
2018-03-14 11:27:18 +0100

    If objects are locked, choose these as the static fragment.

--- castor 2.0/Assets/Scripts/Fragments/LockController.cs
+++ castor 2.0/Assets/Scripts/Fragments/LockController.cs
@@ -1,11 +1,23 @@
 ﻿using UnityEngine;
 using Fragment;
+using System.Collections.Generic;
+using System;
 
 namespace Fragments
 {
     public class LockController : MonoBehaviour
     {
         public GameObject TransformPanel;
+
+        public List<GameObject> LockedObjects
+        {
+            get { return GetLockedObjects(); }
+        }
+
+        public List<GameObject> UnLockedObjects
+        {
+            get { return GetUnLockedObjects(); }
+        }
 
         public void OnChildFragmentStateChanged()
         {
@@ -23,6 +35,26 @@ namespace Fragments
                 if (child.GetComponent<StateTracker>().State.Locked) return true;
             }
             return false;
+        }
+
+        private List<GameObject> GetLockedObjects()
+        {
+            List<GameObject> lockedObjects = new List<GameObject>();
+            foreach (Transform child in transform)
+            {
+                if (child.GetComponent<StateTracker>().State.Locked) lockedObjects.Add(child.gameObject);
+            }
+            return lockedObjects;
+        }
+
+        private List<GameObject> GetUnLockedObjects()
+        {
+            List<GameObject> unLockedObjects = new List<GameObject>();
+            foreach (Transform child in transform)
+            {
+                if (child.GetComponent<StateTracker>().State.UnLocked) unLockedObjects.Add(child.gameObject);
+            }
+            return unLockedObjects;
         }
 
         private void TurnOfGizmos()
