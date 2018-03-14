d9bd72a8e4f2fd06daa360d851da30c1d7b45848
Laura Baakman <laurabaakman@gmail.com>
2018-03-14 10:05:47 +0100

    Use a lockcontroller attatched to the SelectedFragments, instead of individual fragments.

--- castor 2.0/Assets/Scripts/Fragments/LockController.cs
+++ castor 2.0/Assets/Scripts/Fragments/LockController.cs
@@ -1,9 +1,42 @@
 ﻿using UnityEngine;
+using Fragment;
 
 namespace Fragments
 {
     public class LockController : MonoBehaviour
     {
+        public GameObject TransformPanel;
+
+        private void Update()
+        {
+            bool anyLockedFragments = AreAnySelectedFragmentsLocked();
+            if (anyLockedFragments) TurnOfGizmos();
+
+            ToggleTransformPanel(anyLockedFragments);
+        }
+
+        private bool AreAnySelectedFragmentsLocked()
+        {
+            foreach (Transform child in transform)
+            {
+                if (child.GetComponent<StateTracker>().State.Locked) return true;
+            }
+            return false;
+        }
+
+        private void TurnOfGizmos()
+        {
+            RTEditor.EditorGizmoSystem.Instance.TurnOffGizmos();
+        }
+
+        private void ToggleTransformPanel(bool anyLockedFragments)
+        {
+            TransformPanel.BroadcastMessage(
+                methodName: "OnToggleButtonInteractability",
+                parameter: !anyLockedFragments,
+                options: SendMessageOptions.RequireReceiver
+            );
+        }
     }
 }
 
