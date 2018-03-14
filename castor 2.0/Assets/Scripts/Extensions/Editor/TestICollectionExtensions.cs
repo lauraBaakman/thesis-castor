1114183ec7ffadfdbe368aeb497d9f57d8c52857
Laura Baakman <laurabaakman@gmail.com>
2018-03-14 14:22:42 +0100

    ICollection extension to check if a collection is empty.

---
+++ castor 2.0/Assets/Scripts/Extensions/Editor/TestICollectionExtensions.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class ICollectionExtensionTests
+    {
+        [Test]
+        public void Test_IsEmpty_EmptyList()
+        {
+            ICollection<int> list = new List<int>();
+
+            Assert.IsTrue(list.IsEmpty());
+        }
+
+        [Test]
+        public void Test_IsEmpty_EmptyList_WithNonZeroCapacity()
+        {
+            ICollection<int> list = new List<int> { 1 };
+            list.Clear();
+
+            Assert.IsTrue(list.IsEmpty());
+        }
+
+        [Test]
+        public void Test_IsEmpty_NonEmptyList()
+        {
+            ICollection<int> list = new List<int> { 1, 2, 3 };
+
+            Assert.IsFalse(list.IsEmpty());
+        }
+
+    }
+}
---
+++ castor 2.0/Assets/Scripts/Extensions/ICollectionExtensions.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+public static class ICollectionExtension
+{
+    public static bool IsEmpty<T>(this ICollection<T> collection)
+    {
+        return collection.Count == 0;
+    }
+}
