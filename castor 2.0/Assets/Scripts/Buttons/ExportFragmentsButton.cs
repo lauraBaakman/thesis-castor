718b97409fbef27c7e05034795f8ddfdbeea88be
Laura Baakman <laurabaakman@gmail.com>
2018-03-14 12:11:04 +0100

    Include a export button.

---
+++ castor 2.0/Assets/Scripts/Buttons/ExportFragmentsButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Buttons
+{
+    public class ExportFragmentsButton : AbstractButton
+    {
+        protected override void ExecuteButtonAction()
+        {
+            Debug.Log("Time to do some exporting!");
+        }
+
+        protected override bool HasDetectedKeyBoardShortCut()
+        {
+            return false;
+        }
+    }
+}
+
