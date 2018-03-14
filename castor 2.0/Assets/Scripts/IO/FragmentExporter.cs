ea55b71906626d0cd04411dd0450abd8f8d047d9
Laura Baakman <laurabaakman@gmail.com>
2018-03-14 11:54:47 +0100

    Set up the fragments exporter.

---
+++ castor 2.0/Assets/Scripts/IO/FragmentExporter.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+namespace IO
+{
+    public delegate void Callback();
+
+    public class FragmentsExporter
+    {
+        private readonly GameObject FragmentsRoot;
+
+        private readonly CallBack CallBack;
+
+        public FragmentsExporter(GameObject fragmentsRoot, CallBack callback)
+        {
+            FragmentsRoot = fragmentsRoot;
+            CallBack = callback;
+        }
+
+        public void Export()
+        {
+            GetOutputDirectory();
+        }
+
+        private void GetOutputDirectory()
+        {
+            Debug.Log("lets get the output directory.");
+        }
+    }
+}
