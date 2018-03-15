ed19cba307224ca26e0947581af060b6d503ec88
Laura Baakman <laurabaakman@gmail.com>
2018-03-15 14:08:37 +0100

    use the ObjFile wrapper.

--- castor 2.0/Assets/Scripts/Extensions/MeshExtensions.cs
+++ castor 2.0/Assets/Scripts/Extensions/MeshExtensions.cs
@@ -4,9 +4,7 @@ public static class MeshExtensions
 {
     public static bool ToFile(this Mesh mesh, string path)
     {
-        Debug.Log("Returning True, should be implemented!");
-
-        return true;
+        return ObjFile.Write(mesh, path);
     }
 
     public static Mesh Transform(this Mesh mesh, Matrix4x4 transformation)
