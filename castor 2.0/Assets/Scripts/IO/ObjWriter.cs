d392a91544d850a371a78dff482763f2c108f3a8
Laura Baakman <laurabaakman@gmail.com>
2018-03-16 10:45:52 +0100

    Removed uv data and refactored.

--- castor 2.0/Assets/Scripts/IO/ObjWriter.cs
+++ castor 2.0/Assets/Scripts/IO/ObjWriter.cs
@@ -4,42 +4,53 @@ using System.Text;
 
 namespace IO
 {
+#pragma warning disable XS0001 // Find APIs marked as TODO in Mono
     //Adapted from http://wiki.unity3d.com/index.php?title=ObjExporter
     internal static class ObjExporter
     {
 
         public static string MeshToString(Mesh mesh)
         {
-#pragma warning disable XS0001 // Find APIs marked as TODO in Mono
             StringBuilder sb = new StringBuilder();
-#pragma warning restore XS0001 // Find APIs marked as TODO in Mono
 
-            sb.Append("g ").Append(mesh.name).Append("\n");
-            foreach (Vector3 v in mesh.vertices)
+            WriteObjectName(sb, mesh.name);
+            WriteVertices(sb, mesh.vertices);
+            WriteNormals(sb, mesh.normals);
+            WriteTriangles(sb, mesh.triangles);
+
+            return sb.ToString();
+        }
+
+        private static void WriteObjectName(StringBuilder sb, string name)
+        {
+            sb.Append("o ").Append(name).Append("\n");
+        }
+
+        private static void WriteVertices(StringBuilder sb, Vector3[] vertices)
+        {
+            foreach (Vector3 v in vertices)
             {
                 sb.Append(string.Format("v {0} {1} {2}\n", v.x, v.y, v.z));
             }
             sb.Append("\n");
-            foreach (Vector3 v in mesh.normals)
+        }
+
+        private static void WriteNormals(StringBuilder sb, Vector3[] normals)
+        {
+            foreach (Vector3 v in normals)
             {
                 sb.Append(string.Format("vn {0} {1} {2}\n", v.x, v.y, v.z));
             }
             sb.Append("\n");
-            foreach (Vector3 v in mesh.uv)
+        }
+
+        private static void WriteTriangles(StringBuilder sb, int[] triangles)
+        {
+            for (int i = 0; i < triangles.Length; i += 3)
             {
-                sb.Append(string.Format("vt {0} {1}\n", v.x, v.y));
+                sb.Append(string.Format("f {0}//{0} {1}//{1} {2}//{2}\n",
+                    triangles[i] + 1, triangles[i + 1] + 1, triangles[i + 2] + 1));
             }
-            for (int material = 0; material < mesh.subMeshCount; material++)
-            {
-                sb.Append("\n");
-                int[] triangles = mesh.GetTriangles(0);
-                for (int i = 0; i < triangles.Length; i += 3)
-                {
-                    sb.Append(string.Format("f {0}/{0}/{0} {1}/{1}/{1} {2}/{2}/{2}\n",
-                        triangles[i] + 1, triangles[i + 1] + 1, triangles[i + 2] + 1));
-                }
-            }
-            return sb.ToString();
         }
 
         public static void MeshToFile(Mesh mesh, string path)
@@ -57,4 +68,5 @@ namespace IO
             if (mesh.subMeshCount != 1) throw new System.ArgumentException("The writer can only handle meshes without submeshes.");
         }
     }
+#pragma warning restore XS0001 // Find APIs marked as TODO in Mono
 }
