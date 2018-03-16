0d6107f50c3b4c87a9b043227060225094b609be
Laura Baakman <laurabaakman@gmail.com>
2018-03-16 11:32:31 +0100

    Added test with a balk.

--- castor 2.0/Assets/Scripts/IO/Editor/TestObjFile.cs
+++ castor 2.0/Assets/Scripts/IO/Editor/TestObjFile.cs
@@ -8,15 +8,21 @@ namespace Tests.IO
     [TestFixture]
     public class ObjFileTests
     {
-        string testObjFile = "Scripts/IO/Editor/cube.obj";
-        string inputPath;
+        string cube = "Scripts/IO/Editor/cube.obj";
+        string balk = "Scripts/IO/Editor/balk.obj";
+
+        string balkPath;
         string outputPath;
 
         [SetUp]
         public void SetUp()
         {
-            this.inputPath = Path.Combine(Application.dataPath, testObjFile);
             this.outputPath = UnityEditor.FileUtil.GetUniqueTempPathInProject();
+        }
+
+        private string InputPath(string file)
+        {
+            return Path.Combine(Application.dataPath, file);
         }
 
         [TearDown]
@@ -26,14 +32,27 @@ namespace Tests.IO
         }
 
         [Test]
-        public void Test_ReadWrite()
+        public void Test_ReadWrite_Cube()
         {
-            ReadResult readResult = ObjFile.Read(this.inputPath);
+            string inputPath = InputPath(cube);
+            ReadResult readResult = ObjFile.Read(inputPath);
             Mesh mesh = readResult.Mesh;
 
             WriteResult writeResult = ObjFile.Write(mesh, this.outputPath);
 
-            FileAssert.AreEqual(this.inputPath, this.outputPath);
+            FileAssert.AreEqual(inputPath, this.outputPath);
+        }
+
+        [Test]
+        public void Test_ReadWrite_Balk()
+        {
+            string inputPath = InputPath(balk);
+            ReadResult readResult = ObjFile.Read(inputPath);
+            Mesh mesh = readResult.Mesh;
+
+            WriteResult writeResult = ObjFile.Write(mesh, this.outputPath);
+
+            FileAssert.AreEqual(inputPath, this.outputPath);
         }
     }
 }
