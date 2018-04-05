53ea76d9c9e855729ec32702013c9db2f2db9bbb
Laura Baakman <laurabaakman@gmail.com>
2018-04-05 13:57:16 +0200

    Compute the translation.

--- Simple_IGD_Prototype.m
+++ Simple_IGD_Prototype.m
@@ -3,7 +3,7 @@
 % D., and K. Ikeuchi. "Iterative estimation of rotation and translation
 % using the quaternion: School of Computer Science." (1995)".
 
-%close all; clear variables; clc;
+close all; clear variables; clc;
 
 %%Translation Vector and Rotation Matrix
 t = [3, 4, 7];
@@ -12,17 +12,17 @@ RMat = makehgtform('xrotate',pi/2,'yrotate',pi/4, 'zrotate', pi/8);
 
 %%Model Points: each column is a point
 X = [
-    -1, 2, 3, 0;
-    +5, 3, 4, 0;
-    -2, 8, 2, 0;
-    +7, 9, -1, 0 ]';
+    -1, 2, +3, 1;
+    +5, 3, +4, 1;
+    -2, 8, +2, 1;
+    +7, 9, -1, 1 ]';
 N = size(X, 2);
 
 %% Transformation Matrix 1, should result in a Transformation Matrix that is the Identity
-M = eye(4);
+% M = eye(4);
 
 %% Transformation Matrix 2, P is translated w.r.t. X with t.
-% M = TrMat;
+M = TrMat;
 
 %% Transformation Matrix 3, P is rotated w.r.t. X with R
 % M = RMat;
@@ -49,12 +49,18 @@ Ru = @(u, v, w, s)...
 ];
 R = @(q) 1 / dot(q, q) * Ru(q(1), q(2), q(3), q(4));
 
+Xc = @(x, q) R(q) * x;
+
 compute_local_error = @(q, t, x, p) (R(q) * x + t - p).^2;
+
+partialToT = @(q, t, x, p) 2 * (Xc(x, q) + t - p);
 
 t_current = [0, 0, 0, 0]';
 q_current = [0, 0, 0, 1]';
 
 M_actual = eye(4);
+
+learning_rate = 1;
 
 while 1
     t_previous = t_current;
@@ -72,4 +78,22 @@ while 1
     if(has_converged(M_actual, M)) 
         break;
     end
+    
+    % Compute the gradient of the error w.r.t. to t and q
+    t_change = [0, 0, 0, 0]';
+    for i = 1:size(X, 2)
+        t_change = t_change + partialToT(q_current, t_current, X(:, i), P(:,i));
+    end
+    t_change = (1 / (2 * N)) * t_change;
+    
+    % Update t_current and T_actual
+    t_current = t_previous - learning_rate * t_change;
+    T_actual = eye(4);
+    T_actual(:, end) = t_current;
+    
+    % Update q_current and R_actual
+    R_actual = eye(4);
+    
+    % Update M_actual
+    M_actual = T_actual * R_actual;
 end
