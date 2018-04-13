f48a18096a1492e1cf70cfd68c62465be966d8e6
Laura Baakman <laurabaakman@gmail.com>
2018-04-13 10:29:35 +0200

    Test cases for compute error.

--- castor 2.0/Assets/Scripts/Registration/Error Metric/Editor/TestIntersectionTermError.cs
+++ castor 2.0/Assets/Scripts/Registration/Error Metric/Editor/TestIntersectionTermError.cs
@@ -1,12 +1,14 @@
 using System.Collections.Generic;
 using NUnit.Framework;
 using Utils;
+using Registration.Error;
 
 namespace Tests.Registration.Error
 {
     [TestFixture]
     public class IntersectionTermErrorTests
     {
+        private double precision = 0.0001f;
 
         private static List<Vector4D> Xc_0 = new List<Vector4D>
         {
@@ -26,10 +28,10 @@ namespace Tests.Registration.Error
 
         private static List<Vector4D> P = new List<Vector4D>
         {
-            new Vector4D(-1.250919762305275e+00, +2.901428612819832e+00, +3.463987883622810e+00 1),
-            new Vector4D(+5.197316968394073e+00, +2.312037280884873e+00, +3.311989040672406e+00 1),
-            new Vector4D(-2.883832775663601e+00, +8.732352291549871e+00, +2.202230023486417e+00 1),
-            new Vector4D(+7.000000000000000e+00, +9.000000000000000e+00, -1.000000000000000e+00 1),
+            new Vector4D(-1.250919762305275e+00, +2.901428612819832e+00, +3.463987883622810e+00, 1),
+            new Vector4D(+5.197316968394073e+00, +2.312037280884873e+00, +3.311989040672406e+00, 1),
+            new Vector4D(-2.883832775663601e+00, +8.732352291549871e+00, +2.202230023486417e+00, 1),
+            new Vector4D(+7.000000000000000e+00, +9.000000000000000e+00, -1.000000000000000e+00, 1),
         };
 
         private static Vector4D translation_0 = new Vector4D(+0.000000000000000e+00, +0.000000000000000e+00, +0.000000000000000e+00, 0);
@@ -45,7 +47,115 @@ namespace Tests.Registration.Error
         private static double distance_weight_1 = 0.3;
 
         private static int xi_0 = 0;
-        private static int xi_1 = 0;
+        private static int xi_1 = 1;
+
+        [Test, TestCaseSource("ErrorCases")]
+        public void Test_ComputeError(List<Vector4D> XCs, List<Vector4D> Ps, Vector4D translation, int xi, double intersection_weight, double distance_weight, double expected)
+        {
+            IntersectionTermError errorMetric = new IntersectionTermError(distance_weight, intersection_weight);
+            double actual = errorMetric.ComputeError(XCs, Ps, translation, xi);
+
+            Assert.That(actual, Is.EqualTo(expected).Within(precision));
+        }
+        static object[] ErrorCases =
+        {
+            new object[] {
+                Xc_0, P,  translation_0, xi_0, intersection_weight_0, distance_weight_0, +8.587005082531676e-01
+            },
+            new object[] {
+                Xc_0, P,  translation_0, xi_0, intersection_weight_0, distance_weight_1, +2.576101524759503e-01
+            },
+            new object[] {
+                Xc_0, P,  translation_0, xi_0, intersection_weight_1, distance_weight_0, +8.587005082531676e-01
+            },
+            new object[] {
+                Xc_0, P,  translation_0, xi_0, intersection_weight_1, distance_weight_1, +2.576101524759503e-01
+            },
+            new object[] {
+                Xc_0, P,  translation_0, xi_1, intersection_weight_0, distance_weight_0, +1.717401016506335e+00
+            },
+            new object[] {
+                Xc_0, P,  translation_0, xi_1, intersection_weight_0, distance_weight_1, +1.116310660729118e+00
+            },
+            new object[] {
+                Xc_0, P,  translation_0, xi_1, intersection_weight_1, distance_weight_0, +1.030440609903801e+00
+            },
+            new object[] {
+                Xc_0, P,  translation_0, xi_1, intersection_weight_1, distance_weight_1, +4.293502541265838e-01
+            },
+            new object[] {
+                Xc_1, P,  translation_0, xi_0, intersection_weight_0, distance_weight_0, +1.464418440559290e+02
+            },
+            new object[] {
+                Xc_1, P,  translation_0, xi_0, intersection_weight_0, distance_weight_1, +4.393255321677871e+01
+            },
+            new object[] {
+                Xc_1, P,  translation_0, xi_0, intersection_weight_1, distance_weight_0, +1.464418440559290e+02
+            },
+            new object[] {
+                Xc_1, P,  translation_0, xi_0, intersection_weight_1, distance_weight_1, +4.393255321677871e+01
+            },
+            new object[] {
+                Xc_1, P,  translation_0, xi_1, intersection_weight_0, distance_weight_0, +2.928836881118581e+02
+            },
+            new object[] {
+                Xc_1, P,  translation_0, xi_1, intersection_weight_0, distance_weight_1, +1.903743972727078e+02
+            },
+            new object[] {
+                Xc_1, P,  translation_0, xi_1, intersection_weight_1, distance_weight_0, +1.757302128671149e+02
+            },
+            new object[] {
+                Xc_1, P,  translation_0, xi_1, intersection_weight_1, distance_weight_1, +7.322092202796452e+01
+            },
+            new object[] {
+                Xc_0, P,  translation_1, xi_0, intersection_weight_0, distance_weight_0, +5.354724644324055e+01
+            },
+            new object[] {
+                Xc_0, P,  translation_1, xi_0, intersection_weight_0, distance_weight_1, +1.606417393297217e+01
+            },
+            new object[] {
+                Xc_0, P,  translation_1, xi_0, intersection_weight_1, distance_weight_0, +5.354724644324055e+01
+            },
+            new object[] {
+                Xc_0, P,  translation_1, xi_0, intersection_weight_1, distance_weight_1, +1.606417393297217e+01
+            },
+            new object[] {
+                Xc_0, P,  translation_1, xi_1, intersection_weight_0, distance_weight_0, +1.070944928864811e+02
+            },
+            new object[] {
+                Xc_0, P,  translation_1, xi_1, intersection_weight_0, distance_weight_1, +6.961142037621272e+01
+            },
+            new object[] {
+                Xc_0, P,  translation_1, xi_1, intersection_weight_1, distance_weight_0, +6.425669573188867e+01
+            },
+            new object[] {
+                Xc_0, P,  translation_1, xi_1, intersection_weight_1, distance_weight_1, +2.677362322162028e+01
+            },
+            new object[] {
+                Xc_1, P,  translation_1, xi_0, intersection_weight_0, distance_weight_0, +1.559106201398074e+02
+            },
+            new object[] {
+                Xc_1, P,  translation_1, xi_0, intersection_weight_0, distance_weight_1, +4.677318604194222e+01
+            },
+            new object[] {
+                Xc_1, P,  translation_1, xi_0, intersection_weight_1, distance_weight_0, +1.559106201398074e+02
+            },
+            new object[] {
+                Xc_1, P,  translation_1, xi_0, intersection_weight_1, distance_weight_1, +4.677318604194222e+01
+            },
+            new object[] {
+                Xc_1, P,  translation_1, xi_1, intersection_weight_0, distance_weight_0, +3.118212402796148e+02
+            },
+            new object[] {
+                Xc_1, P,  translation_1, xi_1, intersection_weight_0, distance_weight_1, +2.026838061817496e+02
+            },
+            new object[] {
+                Xc_1, P,  translation_1, xi_1, intersection_weight_1, distance_weight_0, +1.870927441677689e+02
+            },
+            new object[] {
+                Xc_1, P,  translation_1, xi_1, intersection_weight_1, distance_weight_1, +7.795531006990370e+01
+            }
+        };
 
     }
 }
