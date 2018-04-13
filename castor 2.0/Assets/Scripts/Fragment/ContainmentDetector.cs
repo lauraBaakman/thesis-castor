012dca09f74b7f1b452b3bcbcda31a140c7370fc
Laura Baakman <laurabaakman@gmail.com>
2018-04-13 15:39:46 +0200

    Test and test scene for the containment detector.

--- castor 2.0/Assets/Scripts/Fragment/ContainmentDetector.cs
+++ castor 2.0/Assets/Scripts/Fragment/ContainmentDetector.cs
@@ -1,9 +1,22 @@
 using UnityEngine;
+using Utils;
 
 [RequireComponent(typeof(MeshCollider))]
 public class ContainmentDetector : MonoBehaviour
 {
     void Start()
     {
+    }
+
+    /// <summary>
+    /// Verifies if the object contains the passed position. 
+    /// 
+    /// Note the position should be in worldspace.
+    /// </summary>
+    /// <returns><c>true</c>, if the position falls inside the object, <c>false</c> otherwise.</returns>
+    /// <param name="position">Position in worldspace.</param>
+    public bool GameObjectContains(Vector4D position)
+    {
+        return false;
     }
 }
---
+++ castor 2.0/Assets/Scripts/Fragment/Editor/ContainmentDetectorTest.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using Utils;
+
+public class ContainmentDetectorTest
+{
+    private static string sceneName = "Assets/Scenes/ContainmentDetectorTestScene.unity";
+    private static string demispherename = "hollowdemisphere";
+
+    [Test]
+    public void ContainmentDetectorTest_Point_Inside()
+    {
+        bool expected = true;
+        bool actual = ContainmentDetectorTest_Helper("position_inside");
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void ContainmentDetectorTest_Point_Outside()
+    {
+        bool expected = false;
+        bool actual = ContainmentDetectorTest_Helper("position_outside");
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void ContainmentDetectorTest_Point_Outside_In_Concavity()
+    {
+        bool expected = false;
+        bool actual = ContainmentDetectorTest_Helper("position_in_concavity");
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    private bool ContainmentDetectorTest_Helper(string point_name)
+    {
+        EditorSceneManager.OpenScene(sceneName);
+        GameObject demisphere = GameObject.Find(demispherename);
+
+        GameObject point = GameObject.Find(point_name);
+        Vector4D position = new Vector4D(point.transform.position, 1.0);
+
+        ContainmentDetector detector = demisphere.GetComponent<ContainmentDetector>();
+        return detector.GameObjectContains(position);
+    }
+}
