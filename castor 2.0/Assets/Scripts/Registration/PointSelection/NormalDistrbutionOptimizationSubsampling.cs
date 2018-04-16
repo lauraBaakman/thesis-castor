c3369aabf8cd05b3e8c28a5df92391e5d113bcfc
Laura Baakman <laurabaakman@gmail.com>
2018-04-16 16:51:54 +0200

    Set up for the normal optimization subsampling method.

---
+++ castor 2.0/Assets/Scripts/Registration/PointSelection/NormalDistrbutionOptimizationSubsampling.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace Registration
+{
+    /// <summary>
+    /// Normal distribution optimization subsampling as proposed by Rusinkiewicz, 
+    /// S., & Levoy, M. (2001). Efficient variants of the ICP algorithm. In 3-D 
+    /// Digital Imaging and Modeling, 2001. Proceedings. Third International 
+    /// Conference on (pp. 145-152). IEEE..
+    /// </summary>
+    public class NormalDistributionOptimizationSubsampling : IPointSampler
+    {
+        public List<Point> Sample(SamplingInformation samplingInfo)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
