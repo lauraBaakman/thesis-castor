121788a9c78f926d903cf69e9561bff908418168
Laura Baakman <laurabaakman@gmail.com>
2018-04-19 12:11:16 +0200

    Moved normals to their own file

---
+++ castor 2.0/Assets/Scripts/Utils/Normal.cs
@@ -0,0 +1,37 @@
+﻿using Registration;
+using UnityEngine;
+
+namespace Utils
+{
+    public class Normal
+    {
+        private static float magnitudeFactor = 0.5f;
+
+        public readonly Vector3 Start;
+        public readonly Vector3 End;
+
+        public Normal(Point point) :
+        this(
+                position: point.Position,
+                direction: point.Normal
+            )
+        { }
+
+        public Normal(Vector3 position, Vector3 direction)
+        {
+            Start = position;
+            End = ComputeEnd(position, direction);
+        }
+
+        public static float MagnitudeFactor
+        {
+            get { return magnitudeFactor; }
+            set { magnitudeFactor = value; }
+        }
+
+        private Vector3 ComputeEnd(Vector3 position, Vector3 direction)
+        {
+            return position + direction * magnitudeFactor * direction.magnitude;
+        }
+    }
+}
