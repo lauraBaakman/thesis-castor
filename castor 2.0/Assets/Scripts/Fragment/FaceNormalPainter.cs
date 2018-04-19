73a146831aa5705cb4fca8fd9b24bd4a2f631344
Laura Baakman <laurabaakman@gmail.com>
2018-04-19 12:46:03 +0200

    Component to draw the normals of a DCEL.

--- castor 2.0/Assets/Scripts/Fragment/FaceNormalPainter.cs
+++ castor 2.0/Assets/Scripts/Fragment/FaceNormalPainter.cs
@@ -1,4 +1,7 @@
 using UnityEngine;
+using Utils;
+using DoubleConnectedEdgeList;
+using System.Collections.Generic;
 
 namespace Fragment
 {
@@ -10,16 +13,84 @@ namespace Fragment
         public float normalScale = 1.0f;
         public Color normalColor = Color.white;
 
-        private Transform ReferenceTransform;
+        private List<Normal> normals;
 
-        void Start()
+        public void Start()
         {
-            Debug.Log("HI!");
+            Normal.MagnitudeFactor = normalScale;
+
+            normals = new List<Normal>();
+            DCEL dcel = GetComponent<DoubleConnectedEdgeListStorage>().DCEL;
+
+            foreach (Face face in dcel.Faces) normals.Add(Normal.FaceNormal(face));
         }
 
-        void Update()
+        public void OnRenderObject()
         {
+            RenderNormals();
+        }
+
+        private void RenderNormals()
+        {
+            CreateSetApplyNormalMaterial();
+
+            // Set transformation matrix to match our transform
+            GL.PushMatrix();
+            GL.MultMatrix(this.transform.localToWorldMatrix);
+
+            GL.Begin(GL.LINES);
+
+            DrawNormals();
+
+            GL.End();
+            GL.PopMatrix();
 
         }
+
+        private void CreateSetApplyNormalMaterial()
+        {
+            //Source https://docs.unity3d.com/ScriptReference/GL.html
+            Shader shader = Shader.Find("Hidden/Internal-Colored");
+
+            normalMaterial = new Material(shader)
+            {
+                hideFlags = HideFlags.HideAndDontSave
+            };
+
+            // Turn on alpha blending
+            normalMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+            normalMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+
+            // Turn backface culling off
+            normalMaterial.SetInt("_Cull", (int)UnityEngine.Rendering.CullMode.Off);
+
+            // Turn off depth writes
+            normalMaterial.SetInt("_ZWrite", 0);
+
+            // Apply the material
+            normalMaterial.SetPass(0);
+        }
+
+        private void DrawNormals()
+        {
+            foreach (Normal normal in normals) DrawNormal((normal));
+        }
+
+        private void DrawNormal(Normal normal)
+        {
+            GL.Color(normalColor);
+
+            GL.Vertex3(
+                normal.Start.x,
+                normal.Start.y,
+                normal.Start.z
+            );
+            GL.Vertex3(
+                normal.End.x,
+                normal.End.y,
+                normal.End.z
+            );
+        }
+
     }
 }
