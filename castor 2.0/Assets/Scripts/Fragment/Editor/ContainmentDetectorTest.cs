a79cd04a3d46bd30667108ca9e491c65dfaf82e8
Laura Baakman <laurabaakman@gmail.com>
2018-04-19 10:26:43 +0200

    Move containment detector tests to test namespace.

--- castor 2.0/Assets/Scripts/Fragment/Editor/ContainmentDetectorTest.cs
+++ castor 2.0/Assets/Scripts/Fragment/Editor/ContainmentDetectorTest.cs
@@ -3,50 +3,53 @@ using UnityEditor.SceneManagement;
 using NUnit.Framework;
 using Utils;
 
-public class ContainmentDetectorTest
+namespace Tests
 {
-    private static string sceneName = "Assets/Scenes/TestSceneContainmentDetector.unity";
-    private static string demispherename = "hollowdemisphere";
-
-    [Test]
-    public void ContainmentDetectorTest_Point_Inside()
+    public class ContainmentDetectorTest
     {
-        bool expected = true;
-        bool actual = ContainmentDetectorTest_Helper("position_inside");
+        private static string sceneName = "Assets/Scenes/TestSceneContainmentDetector.unity";
+        private static string demispherename = "hollowdemisphere";
 
-        Assert.AreEqual(expected, actual);
-    }
+        [Test]
+        public void ContainmentDetectorTest_Point_Inside()
+        {
+            bool expected = true;
+            bool actual = ContainmentDetectorTest_Helper("position_inside");
 
-    [Test]
-    public void ContainmentDetectorTest_Point_Outside()
-    {
-        bool expected = false;
-        bool actual = ContainmentDetectorTest_Helper("position_outside");
+            Assert.AreEqual(expected, actual);
+        }
 
-        Assert.AreEqual(expected, actual);
-    }
+        [Test]
+        public void ContainmentDetectorTest_Point_Outside()
+        {
+            bool expected = false;
+            bool actual = ContainmentDetectorTest_Helper("position_outside");
 
-    [Test]
-    public void ContainmentDetectorTest_Point_Outside_In_Concavity()
-    {
-        bool expected = false;
-        bool actual = ContainmentDetectorTest_Helper("position_in_concavity");
+            Assert.AreEqual(expected, actual);
+        }
 
-        Assert.AreEqual(expected, actual);
-    }
+        [Test]
+        public void ContainmentDetectorTest_Point_Outside_In_Concavity()
+        {
+            bool expected = false;
+            bool actual = ContainmentDetectorTest_Helper("position_in_concavity");
 
-    private bool ContainmentDetectorTest_Helper(string point_name)
-    {
-        EditorSceneManager.OpenScene(sceneName);
-        GameObject demisphere = GameObject.Find(demispherename);
+            Assert.AreEqual(expected, actual);
+        }
 
-        GameObject point = GameObject.Find(point_name);
-        Vector4D position = new Vector4D(point.transform.position, 1.0);
+        private bool ContainmentDetectorTest_Helper(string point_name)
+        {
+            EditorSceneManager.OpenScene(sceneName);
+            GameObject demisphere = GameObject.Find(demispherename);
 
-        ContainmentDetector detector = demisphere.GetComponent<ContainmentDetector>();
-        //Awake/update is not run by the test
-        detector.collider = demisphere.GetComponent<MeshCollider>();
+            GameObject point = GameObject.Find(point_name);
+            Vector4D position = new Vector4D(point.transform.position, 1.0);
 
-        return detector.GameObjectContains(position);
+            ContainmentDetector detector = demisphere.GetComponent<ContainmentDetector>();
+            //Awake/update is not run by the test
+            detector.collider = demisphere.GetComponent<MeshCollider>();
+
+            return detector.GameObjectContains(position);
+        }
     }
 }
