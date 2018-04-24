1c3c2f71accdf07e66f5461c85aad7f74f0b204d
Laura Baakman <laurabaakman@gmail.com>
2018-04-24 09:18:57 +0200

    Added a button and a button script to run the experiment.

---
+++ castor 2.0/Assets/Scripts/Buttons/ExperimentButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Buttons
+{
+    public class ExperimentButton : AbstractButton
+    {
+        protected override void ExecuteButtonAction()
+        {
+            Debug.Log("Time to execute the experiment button action!");
+        }
+
+        protected override bool HasDetectedKeyBoardShortCut()
+        {
+            return Input.GetButtonDown("Experiment");
+        }
+    }
+}
