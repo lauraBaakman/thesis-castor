74b05689072b3e2f7e284d3ae9f9b68325476335
Laura Baakman <laurabaakman@gmail.com>
2018-04-26 14:16:05 +0200

    MonoBehaviour to delete a fragment, to be attatched to a fragment

---
+++ castor 2.0/Assets/Scripts/Fragment/FragmentDestroyer.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using RTEditor;
+using UnityEngine;
+
+namespace Fragment
+{
+    public class FragmentDestroyer : MonoBehaviour
+    {
+        public void DestroyFragment()
+        {
+            foreach (Transform childTransform in this.transform)
+            {
+                DeleteGameObject(childTransform.gameObject);
+            }
+            DeleteGameObject(gameObject);
+            EditorObjectSelection.Instance.ClearSelection(allowUndoRedo: false);
+        }
+
+        private void DeleteGameObject(GameObject child)
+        {
+            child.SetActive(false);
+            child.DestroyAllChildren();
+            Destroy(child, 2.0f);
+        }
+    }
+
+}
+
