2a7910d7f8e373e8dca6ebbbbf48031cdda7bd0e
Laura Baakman <laurabaakman@gmail.com>
2018-04-28 12:08:26 +0200

    Move Serializable transform to its own file.

--- castor 2.0/Assets/Scripts/Registration/Settings.cs
+++ castor 2.0/Assets/Scripts/Registration/Settings.cs
@@ -3,6 +3,7 @@ using System.Collections.ObjectModel;
 using UnityEngine;
 using Registration.Error;
 using System.IO;
+using Utils;
 
 namespace Registration
 {
@@ -116,22 +117,6 @@ namespace Registration
         [System.Serializable]
         public class SerializableSettings
         {
-
-            [System.Serializable]
-            public class SerializableTransform
-            {
-                public string name;
-                public string world_space_position;
-                public string world_space_eulerAngles;
-
-                public SerializableTransform(Transform transform)
-                {
-                    name = transform.name;
-                    world_space_position = transform.position.ToString();
-                    world_space_eulerAngles = transform.eulerAngles.ToString();
-                }
-            }
-
             [System.Serializable]
             public class SerializableCorrespondences
             {
---
+++ castor 2.0/Assets/Scripts/Utils/SerializableTransform.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Utils
+{
+    [System.Serializable]
+    public class SerializableTransform
+    {
+        public string name;
+        public string world_space_position;
+        public string world_space_eulerAngles;
+
+        public SerializableTransform(Transform transform)
+        {
+            name = transform.name;
+            world_space_position = transform.position.ToString();
+            world_space_eulerAngles = transform.eulerAngles.ToString();
+        }
+    }
+}
+
