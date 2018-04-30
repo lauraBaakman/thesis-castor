a2f11bfbb82bde61e215394489b2398376bd7dd0
Laura Baakman <laurabaakman@gmail.com>
2018-04-30 16:34:03 +0200

    Implemented the method that copies vertices to uv

--- castor 2.0/Assets/Scripts/Utils/MeshUtils.cs
+++ castor 2.0/Assets/Scripts/Utils/MeshUtils.cs
@@ -6,7 +6,19 @@ namespace Utils
     {
         public static void CopyVerticesToUV2AndUV3(Mesh mesh)
         {
-            return;
+            Vector2[] uv2 = new Vector2[mesh.vertices.Length];
+            Vector2[] uv3 = new Vector2[mesh.vertices.Length];
+
+            Vector3 vertex;
+
+            for (int i = 0; i < mesh.vertices.Length; i++)
+            {
+                vertex = mesh.vertices[i];
+                uv2[i] = new Vector2(vertex.x, vertex.y);
+                uv3[i] = new Vector2(vertex.z, 0);
+            }
+            mesh.uv2 = uv2;
+            mesh.uv3 = uv3;
         }
     }
 }
