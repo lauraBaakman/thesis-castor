58e2c45248f3692b834437934f3d38f328e2ed46
Laura Baakman <laurabaakman@gmail.com>
2018-04-30 16:15:56 +0200

    Function in static class to copy verties to uv2 and uv3.

---
+++ castor 2.0/Assets/Scripts/Utils/MeshUtils.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Utils
+{
+    public static class MeshUtils
+    {
+        public static void CopyVerticesToUV2AndUV3(Mesh mesh)
+        {
+            return;
+        }
+    }
+}
