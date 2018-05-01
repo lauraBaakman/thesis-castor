3ded2a8b25301f71bc59aac8fb434e6813c1a623
Laura Baakman <laurabaakman@gmail.com>
2018-05-01 12:59:21 +0200

    Script to attatch to the statistics button.

---
+++ castor 2.0/Assets/Scripts/Buttons/StatisticsButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Buttons
+{
+    public class StatisticsButton : AbstractButton
+    {
+        protected override void ExecuteButtonAction()
+        {
+            Debug.Log("StatisticsButton:ExecuteButtonAction");
+        }
+
+        protected override bool HasDetectedKeyBoardShortCut()
+        {
+            return Input.GetButtonDown("Statistics");
+        }
+    }
+}
