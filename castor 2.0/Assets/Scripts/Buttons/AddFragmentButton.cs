ab0ebaacf843c3aaf58e73d80aec5de3d8bef50f
Laura Baakman <laurabaakman@gmail.com>
2018-05-04 14:58:53 +0200

    use the fragments that break stuff.

--- castor 2.0/Assets/Scripts/Buttons/AddFragmentButton.cs
+++ castor 2.0/Assets/Scripts/Buttons/AddFragmentButton.cs
@@ -25,8 +25,8 @@ namespace Buttons
 
         private void LoadTestFragments()
         {
-            importer.Import("/Users/laura/Repositories/thesis-castor/castor 2.0/Assets/Models/RoughFracturedCube/roughfracturedcube_part1_translationrotation.obj");
-            importer.Import("/Users/laura/Repositories/thesis-castor/castor 2.0/Assets/Models/RoughFracturedCube/roughfracturedcube_part2.obj");
+            importer.Import("/Users/laura/Repositories/thesis-experiment/simulated/test_data_small/obj/9528401f-a2c9-3502-a795-afc276db60ef.obj");
+            importer.Import("/Users/laura/Repositories/thesis-experiment/simulated/test_data_small/obj/locked_fragment_f90e89025f4cad9d1da70ddfd85c2368.obj");
         }
 
         private void NotifyUser(IO.ReadResult result)
