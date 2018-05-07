859eef57932a6ff8f20dabc8a018499980b1f77a
Laura Baakman <laurabaakman@gmail.com>
2018-05-07 10:27:41 +0200

    Implement Transformatin matrix from unit quaternion.

--- castor 2.0/Assets/Scripts/Utils/MatrixUtils.cs
+++ castor 2.0/Assets/Scripts/Utils/MatrixUtils.cs
@@ -6,7 +6,28 @@ namespace Utils
     {
         public static Matrix4x4 TransformationMatrixFromUnitQuaternion(Quaternion unitQuaternion)
         {
-            throw new NotImplementedException();
+            //Use Wheeler notation to represnt the quaternion elements
+            float s = unitQuaternion.w;
+
+            float u = unitQuaternion.x;
+            float v = unitQuaternion.y;
+            float w = unitQuaternion.z;
+
+            Matrix4x4 matrix = Matrix4x4.identity;
+
+            matrix.m00 = s * s + u * u - v * v - w * w;
+            matrix.m01 = 2 * (u * v - s * w);
+            matrix.m02 = 2 * (u * w + s * v);
+
+            matrix.m10 = 2 * (u * v + s * w);
+            matrix.m11 = s * s - u * u + v * v - w * w;
+            matrix.m12 = 2 * (v * w - s * u);
+
+            matrix.m20 = 2 * (u * w - s * v);
+            matrix.m21 = 2 * (v * w + s * u);
+            matrix.m22 = s * s - u * u - v * v + w * w;
+
+            return matrix;
         }
 
         public static Matrix4x4 TransformationMatrixFromTranslation(Vector3 translation)
