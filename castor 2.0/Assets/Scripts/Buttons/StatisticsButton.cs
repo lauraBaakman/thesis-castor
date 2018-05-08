536acd30812c659cf6347a5fe0b77e39852e6b4d
Laura Baakman <laurabaakman@gmail.com>
2018-05-08 16:32:20 +0200

    Fix the CompletedAllRuns method so that we can close the streamwriter correctly

--- castor 2.0/Assets/Scripts/Buttons/StatisticsButton.cs
+++ castor 2.0/Assets/Scripts/Buttons/StatisticsButton.cs
@@ -7,120 +7,120 @@ using System;
 
 namespace Buttons
 {
-    public class StatisticsButton : AbstractButton
-    {
-        private static string initialPath;
+	public class StatisticsButton : AbstractButton
+	{
+		private static string initialPath;
 
-        private string directory;
+		private string directory;
 
-        private List<StatisticsComputer.Run> runs;
+		private List<StatisticsComputer.Run> runs;
 
-        public StatisticsComputer statisticsComputer;
+		public StatisticsComputer statisticsComputer;
 
-        protected override void Awake()
-        {
-            base.Awake();
-            initialPath = Application.isEditor ? "/Users/laura/Repositories/thesis-experiment/simulated" : null;
-        }
+		protected override void Awake()
+		{
+			base.Awake();
+			initialPath = Application.isEditor ? "/Users/laura/Repositories/thesis-experiment/simulated" : null;
+		}
 
-        protected override void ExecuteButtonAction()
-        {
-            Reset();
-            RetrieveExperimentResultFolder();
-        }
+		protected override void ExecuteButtonAction()
+		{
+			Reset();
+			RetrieveExperimentResultFolder();
+		}
 
-        protected override bool HasDetectedKeyBoardShortCut()
-        {
-            return Input.GetButtonDown("Statistics");
-        }
+		protected override bool HasDetectedKeyBoardShortCut()
+		{
+			return Input.GetButtonDown("Statistics");
+		}
 
-        private void Reset()
-        {
-            directory = "";
-            runs = null;
-        }
+		private void Reset()
+		{
+			directory = "";
+			runs = null;
+		}
 
-        private void RetrieveExperimentResultFolder()
-        {
-            //FileBrowser.ShowLoadDialog(
-            //    onSuccess: (path) => StartCoroutine(ProcessExperimentResultsFolder(path)),
-            //    onCancel: () => { },
-            //    folderMode: true,
-            //    initialPath: initialPath,
-            //    title: "Select a results directory.",
-            //    loadButtonText: "Select"
-            //);
-            Debug.Log("Temporarily using a fixed directory");
-            StartCoroutine(ProcessExperimentResultsFolder("/Users/laura/Repositories/thesis-experiment/simulated/test_data_small/results_05-01_15-51-22-393"));
-        }
+		private void RetrieveExperimentResultFolder()
+		{
+			//FileBrowser.ShowLoadDialog(
+			//    onSuccess: (path) => StartCoroutine(ProcessExperimentResultsFolder(path)),
+			//    onCancel: () => { },
+			//    folderMode: true,
+			//    initialPath: initialPath,
+			//    title: "Select a results directory.",
+			//    loadButtonText: "Select"
+			//);
+			Debug.Log("Temporarily using a fixed directory");
+			StartCoroutine(ProcessExperimentResultsFolder("/Users/laura/Repositories/thesis-experiment/simulated/test_data_small/results_05-07_14-18-38-387"));
+		}
 
-        private IEnumerator<object> ProcessExperimentResultsFolder(string inputDirectory)
-        {
-            StartCoroutine(FindObjectIDs(inputDirectory));
-            yield return new WaitWhile(() => runs == null);
+		private IEnumerator<object> ProcessExperimentResultsFolder(string inputDirectory)
+		{
+			StartCoroutine(FindObjectIDs(inputDirectory));
+			yield return new WaitWhile(() => runs == null);
 
-            statisticsComputer.Init();
+			statisticsComputer.Init();
 
-            foreach (StatisticsComputer.Run run in runs)
-            {
-                StartCoroutine(statisticsComputer.Compute(run));
-                yield return new WaitUntil(() => statisticsComputer.Done);
-            }
+			foreach (StatisticsComputer.Run run in runs)
+			{
+				StartCoroutine(statisticsComputer.Compute(run));
+				yield return new WaitUntil(() => statisticsComputer.Done);
+			}
 
-            WriteNewCSVDataFile();
-        }
+			WriteNewCSVDataFile();
+		}
 
-        private void WriteNewCSVDataFile()
-        {
-            throw new NotImplementedException();
-        }
+		private void WriteNewCSVDataFile()
+		{
+			throw new NotImplementedException();
+		}
 
-        private IEnumerator<object> FindObjectIDs(string inputDirectory)
-        {
-            this.directory = inputDirectory;
-            yield return null;
+		private IEnumerator<object> FindObjectIDs(string inputDirectory)
+		{
+			this.directory = inputDirectory;
+			yield return null;
 
-            string csvDataFile = GetCSVDataFile();
-            yield return null;
+			string csvDataFile = GetCSVDataFile();
+			yield return null;
 
-            List<Dictionary<string, object>> csvData = new CSVFileReader().Read(csvDataFile);
-            yield return null;
+			List<Dictionary<string, object>> csvData = new CSVFileReader().Read(csvDataFile);
+			yield return null;
 
-            List<StatisticsComputer.Run> intermediateRuns = new List<StatisticsComputer.Run>(csvData.Count);
-            foreach (Dictionary<string, object> row in csvData)
-            {
-                intermediateRuns.Add(ExtractRun(row));
-                yield return null;
-            }
+			List<StatisticsComputer.Run> localRuns = new List<StatisticsComputer.Run>(csvData.Count);
+			foreach (Dictionary<string, object> row in csvData)
+			{
+				localRuns.Add(ExtractRun(row));
+				yield return null;
+			}
 
-            this.runs = intermediateRuns;
-        }
+			this.runs = localRuns;
+		}
 
-        private string GetCSVDataFile()
-        {
-            string[] csvFiles = System.IO.Directory.GetFiles(directory, "*.csv");
-            ValidateCSVFiles(csvFiles);
-            return csvFiles[0];
-        }
+		private string GetCSVDataFile()
+		{
+			string[] csvFiles = System.IO.Directory.GetFiles(directory, "*.csv");
+			ValidateCSVFiles(csvFiles);
+			return csvFiles[0];
+		}
 
-        private void ValidateCSVFiles(string[] csvFiles)
-        {
-            if (csvFiles.Length != 1)
-            {
-                Debug.LogError(
-                    string.Format(
-                        "Unexpectedly found {0} csv file(s), expected exactly one file.",
-                        csvFiles.Length
-                    )
-                );
-            }
-        }
+		private void ValidateCSVFiles(string[] csvFiles)
+		{
+			if (csvFiles.Length != 1)
+			{
+				Debug.LogError(
+					string.Format(
+						"Unexpectedly found {0} csv file(s), expected exactly one file.",
+						csvFiles.Length
+					)
+				);
+			}
+		}
 
-        private StatisticsComputer.Run ExtractRun(Dictionary<string, object> csvRow)
-        {
-            string id = csvRow["id"] as string;
-            string path = Path.Combine(directory, String.Format("{0}.obj", id));
-            return new StatisticsComputer.Run(objPath: path);
-        }
-    }
+		private StatisticsComputer.Run ExtractRun(Dictionary<string, object> csvRow)
+		{
+			string id = csvRow["id"] as string;
+			string path = Path.Combine(directory, String.Format("{0}.obj", id));
+			return new StatisticsComputer.Run(objPath: path);
+		}
+	}
 }
--- castor 2.0/Assets/Scripts/Experiment/ExperimentRunner.cs
+++ castor 2.0/Assets/Scripts/Experiment/ExperimentRunner.cs
@@ -8,264 +8,268 @@ using System.Globalization;
 
 namespace Experiment
 {
-    public class ExperimentRunner : MonoBehaviour, IICPListener
-    {
-        private Configuration configuration;
+	public class ExperimentRunner : MonoBehaviour, IICPListener
+	{
+		private Configuration configuration;
 
-        public static string ExperimentFragmentPrefabPath = "ExperimentFragment";
+		public static string ExperimentFragmentPrefabPath = "ExperimentFragment";
+		private static string runDataFileName = "data.csv";
 
-        private GameObject staticFragment;
-        private List<RunExecuter.Run> runs;
+		private GameObject staticFragment;
+		private List<RunExecuter.Run> runs;
 
-        private List<Settings> ICPSettings;
+		private List<Settings> ICPSettings;
 
-        private IO.FragmentImporter fragmentImporter;
-        private IO.FragmentExporter fragmentExporter;
+		private IO.FragmentImporter fragmentImporter;
+		private IO.FragmentExporter fragmentExporter;
 
-        private int completedRunCount;
+		private int completedRunCount;
 
-        private string outputDirectory;
+		private string outputDirectory;
 
-        private StreamWriter runSetWriter;
+		private StreamWriter runSetWriter;
 
-        public void Init(Configuration configuration)
-        {
-            this.configuration = configuration;
+		public void Init(Configuration configuration)
+		{
+			this.configuration = configuration;
 
-            this.fragmentImporter = new IO.FragmentImporter(
-                this.gameObject, FragmentReaderCallBack,
-                copyVerticesToTexture: true,
-                randomizeTransform: false
-            );
-            this.fragmentExporter = new IO.FragmentExporter(FragmentWriterCallBack);
+			this.fragmentImporter = new IO.FragmentImporter(
+				this.gameObject, FragmentReaderCallBack,
+				copyVerticesToTexture: true,
+				randomizeTransform: false
+			);
+			this.fragmentExporter = new IO.FragmentExporter(FragmentWriterCallBack);
 
-            this.ICPSettings = new List<Settings>();
+			this.ICPSettings = new List<Settings>();
 
-            SetUp();
-        }
+			SetUp();
+		}
 
-        private void FragmentReaderCallBack(IO.ReadResult result)
-        {
-            if (result.Failed)
-            {
-                Ticker.Receiver.Instance.SendMessage(
-                    methodName: "OnMessage",
-                    value: result.ToTickerMessage(),
-                    options: SendMessageOptions.RequireReceiver
-                );
-            }
-        }
+		private void FragmentReaderCallBack(IO.ReadResult result)
+		{
+			if (result.Failed)
+			{
+				Ticker.Receiver.Instance.SendMessage(
+					methodName: "OnMessage",
+					value: result.ToTickerMessage(),
+					options: SendMessageOptions.RequireReceiver
+				);
+			}
+		}
 
-        private void FragmentWriterCallBack(IO.WriteResult result)
-        {
-            Ticker.Receiver.Instance.SendMessage(
-                methodName: "OnMessage",
-                value: result.ToTickerMessage(),
-                options: SendMessageOptions.RequireReceiver
-            );
-        }
+		private void FragmentWriterCallBack(IO.WriteResult result)
+		{
+			Ticker.Receiver.Instance.SendMessage(
+				methodName: "OnMessage",
+				value: result.ToTickerMessage(),
+				options: SendMessageOptions.RequireReceiver
+			);
+		}
 
-        private void SetUp()
-        {
-            HandleStaticFragment();
+		private void SetUp()
+		{
+			HandleStaticFragment();
 
-            GenerateICPSettings();
+			GenerateICPSettings();
 
-            this.runs = CollectRuns();
-        }
+			this.runs = CollectRuns();
+		}
 
-        private void GenerateICPSettings()
+		private void GenerateICPSettings()
 
-        {
-            ICPSettings.Add(
-                new Settings(
-                    this.gameObject.transform,
-                    new IGDTransformFinder(
-                        new IGDTransformFinder.Configuration(
-                            convergenceError: 0.001f,
-                            learningRate: 0.001f,
-                            maxNumIterations: 200,
-                            errorMetric: new Registration.Error.IntersectionTermError(0.5f, 0.5f)
-                        )
-                    )
-                )
-            );
+		{
+			ICPSettings.Add(
+				new Settings(
+					this.gameObject.transform,
+					new IGDTransformFinder(
+						new IGDTransformFinder.Configuration(
+							convergenceError: 0.001f,
+							learningRate: 0.001f,
+							maxNumIterations: 200,
+							errorMetric: new Registration.Error.IntersectionTermError(0.5f, 0.5f)
+						)
+					)
+				)
+			);
 
-            ICPSettings.Add(
-                new Settings(
-                    this.gameObject.transform,
-                    new IGDTransformFinder(
-                        new IGDTransformFinder.Configuration(
-                            convergenceError: 0.001f,
-                            learningRate: 0.001f,
-                            maxNumIterations: 200,
-                            errorMetric: new Registration.Error.WheelerIterativeError()
-                        )
-                    )
-                )
-            );
+			ICPSettings.Add(
+				new Settings(
+					this.gameObject.transform,
+					new IGDTransformFinder(
+						new IGDTransformFinder.Configuration(
+							convergenceError: 0.001f,
+							learningRate: 0.001f,
+							maxNumIterations: 200,
+							errorMetric: new Registration.Error.WheelerIterativeError()
+						)
+					)
+				)
+			);
 
-            ICPSettings.Add(
-                new Settings(
-                    this.gameObject.transform,
-                    new HornTransformFinder()
-                )
-            );
+			ICPSettings.Add(
+				new Settings(
+					this.gameObject.transform,
+					new HornTransformFinder()
+				)
+			);
 
-            ICPSettings.Add(
-                new Settings(
-                    this.gameObject.transform,
-                    new LowTransformFinder()
-                )
-            );
-        }
+			ICPSettings.Add(
+				new Settings(
+					this.gameObject.transform,
+					new LowTransformFinder()
+				)
+			);
+		}
 
-        private List<RunExecuter.Run> CollectRuns()
-        {
-            return RunExecuter.Run.FromCSV(configuration.configurations);
-        }
+		private List<RunExecuter.Run> CollectRuns()
+		{
+			return RunExecuter.Run.FromCSV(configuration.configurations);
+		}
 
-        private string CreateResultsDirectory()
-        {
-            string basePath = configuration.outputDirectory;
-            string directoryName = CreateResultsDirectoryName();
+		private string CreateResultsDirectory()
+		{
+			string basePath = configuration.outputDirectory;
+			string directoryName = CreateResultsDirectoryName();
 
-            string directory = Path.Combine(basePath, directoryName);
+			string directory = Path.Combine(basePath, directoryName);
 
-            Directory.CreateDirectory(directory);
+			Directory.CreateDirectory(directory);
 
-            return directory;
-        }
+			return directory;
+		}
 
-        private string CreateResultsDirectoryName()
-        {
-            System.DateTime now = System.DateTime.Now.ToLocalTime();
-            return string.Format(
-                "results_{0}", now.ToString("MM-dd_HH-mm-ss-fff"));
-        }
+		private string CreateResultsDirectoryName()
+		{
+			System.DateTime now = System.DateTime.Now.ToLocalTime();
+			return string.Format(
+				"results_{0}", now.ToString("MM-dd_HH-mm-ss-fff"));
+		}
 
-        private void HandleStaticFragment()
-        {
-            this.staticFragment = Import(configuration.lockedFragmentFile);
+		private void HandleStaticFragment()
+		{
+			this.staticFragment = Import(configuration.lockedFragmentFile);
 
-            Lock(staticFragment);
-        }
+			Lock(staticFragment);
+		}
 
-        private GameObject Import(string path)
-        {
-            return fragmentImporter.Import(path, prefabPath: ExperimentFragmentPrefabPath);
-        }
+		private GameObject Import(string path)
+		{
+			return fragmentImporter.Import(path, prefabPath: ExperimentFragmentPrefabPath);
+		}
 
-        private void Lock(GameObject fragment)
-        {
-            fragment.SendMessage(
-                methodName: "OnToggledLockedState",
-                value: true,
-                options: SendMessageOptions.DontRequireReceiver
-            );
-        }
+		private void Lock(GameObject fragment)
+		{
+			fragment.SendMessage(
+				methodName: "OnToggledLockedState",
+				value: true,
+				options: SendMessageOptions.DontRequireReceiver
+			);
+		}
 
-        private void Write(GameObject fragment)
-        {
-            string path = System.IO.Path.Combine(
-                path1: this.outputDirectory,
-                path2: Path.GetFileName(configuration.lockedFragmentFile)
-            );
-            fragmentExporter.Export(fragment, path);
-        }
+		private void Write(GameObject fragment)
+		{
+			string path = System.IO.Path.Combine(
+				path1: this.outputDirectory,
+				path2: Path.GetFileName(configuration.lockedFragmentFile)
+			);
+			fragmentExporter.Export(fragment, path);
+		}
 
-        private bool CompletedAllRuns()
-        {
-            return completedRunCount == runs.Count;
-        }
+		private bool CompletedAllRuns()
+		{
+			return completedRunCount == (runs.Count - 1);
+		}
 
-        public IEnumerator<object> Execute()
-        {
-            RunExecuter executer = new RunExecuter(listener: this.gameObject, staticFragment: staticFragment,
-                                                   fragmentImporter: fragmentImporter, fragmentExporter: fragmentExporter);
+		public IEnumerator<object> Execute()
+		{
+			RunExecuter executer = new RunExecuter(listener: this.gameObject, staticFragment: staticFragment,
+												   fragmentImporter: fragmentImporter, fragmentExporter: fragmentExporter);
 
-            RunExecuter.Run run;
+			RunExecuter.Run run;
 
-            foreach (Settings ICPSetting in this.ICPSettings)
-            {
-                this.outputDirectory = CreateResultsDirectory();
-                yield return null;
+			foreach (Settings ICPSetting in this.ICPSettings)
+			{
+				this.outputDirectory = CreateResultsDirectory();
+				yield return null;
 
-                Write(staticFragment);
-                yield return null;
+				Write(staticFragment);
+				yield return null;
 
-                ICPSetting.ToJson(Path.Combine(this.outputDirectory, "settings.json"));
-                yield return null;
+				ICPSetting.ToJson(Path.Combine(this.outputDirectory, "settings.json"));
+				yield return null;
 
-                SetUpRunSetWriter();
-                yield return null;
+				SetUpRunSetWriter();
+				yield return null;
 
-                for (completedRunCount = 0; completedRunCount < runs.Count; completedRunCount++)
-                {
-                    run = runs[completedRunCount];
+				for (completedRunCount = 0; completedRunCount < runs.Count; completedRunCount++)
+				{
+					run = runs[completedRunCount];
 
-                    run.ICPSettings = ICPSetting;
-                    executer.OutputDirectory = this.outputDirectory;
+					run.ICPSettings = ICPSetting;
+					executer.OutputDirectory = this.outputDirectory;
 
-                    runSetWriter.Write(string.Format("{0}, ", run.id));
-                    yield return null;
+					runSetWriter.Write(string.Format("{0}, ", run.id));
+					yield return null;
 
-                    StartCoroutine(executer.Execute(run));
-                    yield return new WaitUntil(executer.IsCurrentRunFinished);
-                }
-            }
-        }
+					StartCoroutine(executer.Execute(run));
+					yield return new WaitUntil(executer.IsCurrentRunFinished);
+				}
+			}
+		}
 
-        private void SetUpRunSetWriter()
-        {
-            runSetWriter = new StreamWriter(Path.Combine(this.outputDirectory, "data.csv"));
-            runSetWriter.WriteLine(
-                string.Format(
-                    "{0}, {1}, {2}, {3}",
-                    "id", "termination message", "termination error", "termination iteration"
-                )
-            );
-        }
+		private void SetUpRunSetWriter()
+		{
+			runSetWriter = new StreamWriter(Path.Combine(this.outputDirectory, runDataFileName));
+			runSetWriter.WriteLine(
+				string.Format(
+					"{0}, {1}, {2}, {3}",
+					"id", "termination message", "termination error", "termination iteration"
+				)
+			);
+		}
 
-        private void CleanUpRunSetWriter()
-        {
-            runSetWriter.Close();
-        }
+		private void CleanUpRunSetWriter()
+		{
+			runSetWriter.Close();
+			runSetWriter.Dispose();
+		}
 
-        #region ICPInterface
-        public void OnPreparationStepCompleted(ICPPreparationStepCompletedMessage message) { }
+		#region ICPInterface
+		public void OnPreparationStepCompleted(ICPPreparationStepCompletedMessage message) { }
 
-        public void OnStepCompleted(ICPStepCompletedMessage message) { }
+		public void OnStepCompleted(ICPStepCompletedMessage message) { }
 
-        public void OnICPTerminated(ICPTerminatedMessage message)
-        {
-            //This function is also called when we are not running an experiment for some reason
-            if (runSetWriter == null) return;
+		public void OnICPTerminated(ICPTerminatedMessage message)
+		{
+			//This function is also called when we are not running an experiment for some reason
+			if (runSetWriter == null) return;
 
-            runSetWriter.WriteLine(string.Format(
-                "'{0}', {1}, {2}",
-                message.Message,
-                message.errorAtTermination.ToString("E10", CultureInfo.InvariantCulture),
-                message.terminationIteration));
-            if (CompletedAllRuns()) CleanUpRunSetWriter();
-        }
-        #endregion
+			string line = string.Format(
+				"'{0}', {1}, {2}",
+				message.Message,
+				message.errorAtTermination.ToString("E10", CultureInfo.InvariantCulture),
+				message.terminationIteration
+			);
+			runSetWriter.WriteLine(line);
+			if (CompletedAllRuns()) CleanUpRunSetWriter();
+		}
+		#endregion
 
-        [System.Serializable]
-        public class Configuration
-        {
-            public string lockedFragmentFile;
-            public string outputDirectory;
-            public string configurations;
-            public string id;
+		[System.Serializable]
+		public class Configuration
+		{
+			public string lockedFragmentFile;
+			public string outputDirectory;
+			public string configurations;
+			public string id;
 
-            public static Configuration FromJson(string path)
-            {
-                string json_string = File.ReadAllText(path);
-                Configuration configuration = JsonUtility.FromJson<Configuration>(json_string);
+			public static Configuration FromJson(string path)
+			{
+				string json_string = File.ReadAllText(path);
+				Configuration configuration = JsonUtility.FromJson<Configuration>(json_string);
 
-                return configuration;
-            }
-        }
-    }
+				return configuration;
+			}
+		}
+	}
 }
