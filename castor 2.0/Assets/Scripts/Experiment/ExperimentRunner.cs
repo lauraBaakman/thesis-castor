4eeaed7ec728d33df8e23096004a2f86a50abbc3
Laura Baakman <laurabaakman@gmail.com>
2018-06-06 11:33:51 +0200

    Girl scout

--- castor 2.0/Assets/Scripts/Experiment/ExperimentRunner.cs
+++ castor 2.0/Assets/Scripts/Experiment/ExperimentRunner.cs
@@ -29,11 +29,19 @@ namespace Experiment
 
 		private string outputDirectory;
 
+		/// <summary>
+		/// Build the path where for the results.csv file.
+		/// </summary>
+		/// <value>The run data path.</value>
 		private string RunDataPath
 		{
 			get { return Path.Combine(this.outputDirectory, runDataFileName); }
 		}
 
+		/// <summary>
+		/// Initialze the experiment runnen for the passed configuration.
+		/// </summary>
+		/// <param name="configuration">Configuration.</param>
 		public void Init(Configuration configuration)
 		{
 			this.configuration = configuration;
@@ -50,6 +58,11 @@ namespace Experiment
 			SetUp();
 		}
 
+		/// <summary>
+		/// Call back for the fragment reader, sends a message to the ticker 
+		/// upon failure.
+		/// </summary>
+		/// <param name="result">Result.</param>
 		private void FragmentReaderCallBack(IO.ReadResult result)
 		{
 			if (result.Failed)
@@ -62,6 +75,11 @@ namespace Experiment
 			}
 		}
 
+		/// <summary>
+		/// Call back for the fragmen writer, sends a message to the ticker 
+		/// upon failure.
+		/// </summary>
+		/// <param name="result">Result.</param>
 		private void FragmentWriterCallBack(IO.WriteResult result)
 		{
 			Ticker.Receiver.Instance.SendMessage(
@@ -71,6 +89,9 @@ namespace Experiment
 			);
 		}
 
+		/// <summary>
+		/// Set up of the Experiment runner for the specific experiment.
+		/// </summary>
 		private void SetUp()
 		{
 			HandleStaticFragment();
@@ -80,8 +101,10 @@ namespace Experiment
 			this.runs = CollectRuns();
 		}
 
+		/// <summary>
+		/// Generates the different ICP settings that should be tested in the experiment.
+		/// </summary>
 		private void GenerateICPSettings()
-
 		{
 			ICPSettings.Add(
 				new Settings(
@@ -130,16 +153,30 @@ namespace Experiment
 			);
 		}
 
+		/// <summary>
+		/// Collect als the model fragment files form the configuration.
+		/// </summary>
+		/// <returns>The runs.</returns>
 		private List<RunExecuter.Run> CollectRuns()
 		{
 			return RunExecuter.Run.FromCSV(configuration);
 		}
 
+		/// <summary>
+		/// Create the directory where the results should be stored.
+		/// </summary>
+		/// <param name="path">Path.</param>
 		private void CreateResultsDirectory(string path)
 		{
 			Directory.CreateDirectory(path);
 		}
 
+		/// <summary>
+		/// Creates the name of the results directory, this name depends on the
+		/// ICP settings whose results are stored in that directory.
+		/// </summary>
+		/// <returns>The results directory name.</returns>
+		/// <param name="ICPSetting">ICPS etting.</param>
 		private string CreateResultsDirectoryName(Settings ICPSetting)
 		{
 			string basePath = configuration.OutputDirectory;
@@ -149,17 +186,31 @@ namespace Experiment
 		}
 
 
+		/// <summary>
+		/// Load the static fragment and lock it to make sure that ICP treats it
+		/// as the static fragment.
+		/// </summary>
 		private void HandleStaticFragment()
 		{
 			this.staticFragment = Import(configuration.LockedFragmentFile);
 			Lock(staticFragment);
 		}
 
+		/// <summary>
+		/// Import a game object that is stored in an obj file at the specified 
+		/// string.
+		/// </summary>
+		/// <returns>The import.</returns>
+		/// <param name="path">Path.</param>
 		private GameObject Import(string path)
 		{
 			return fragmentImporter.Import(path, prefabPath: ExperimentFragmentPrefabPath);
 		}
 
+		/// <summary>
+		/// Lock the specified fragment.
+		/// </summary>
+		/// <param name="fragment">Fragment.</param>
 		private void Lock(GameObject fragment)
 		{
 			fragment.SendMessage(
@@ -169,6 +220,11 @@ namespace Experiment
 			);
 		}
 
+		/// <summary>
+		/// Write the specified fragment ot file. The path is determiend based on 
+		/// the name of the fragment.
+		/// </summary>
+		/// <param name="fragment">Fragment.</param>
 		private void Write(GameObject fragment)
 		{
 			string path = System.IO.Path.Combine(
@@ -178,16 +234,29 @@ namespace Experiment
 			fragmentExporter.Export(fragment, path);
 		}
 
+		/// <summary>
+		/// Returns true if all runs, i.e. if ICP has run for every combination 
+		/// of a modelfragment and an ICP configuration.
+		/// </summary>
+		/// <returns><c>true</c>, if all runs was completeded, <c>false</c> otherwise.</returns>
 		private bool CompletedAllRuns()
 		{
 			return results.Count == runs.Count;
 		}
 
+		/// <summary>
+		/// Returns true if the results directory already exists.
+		/// </summary>
+		/// <returns><c>true</c>, if directory exists was resultsed, <c>false</c> otherwise.</returns>
 		private bool ResultsDirectoryExists()
 		{
 			return Directory.Exists(this.outputDirectory);
 		}
 
+		/// <summary>
+		/// Handles the set up of the experiment for a specific ICP configuration.
+		/// </summary>
+		/// <param name="settings">Settings.</param>
 		private void SetUpForSettings(Settings settings)
 		{
 			if (ResultsDirectoryExists()) SetUpForContinuation();
@@ -196,6 +265,11 @@ namespace Experiment
 			WriteTimeStampToRunDataFile();
 		}
 
+		/// <summary>
+		/// This function handles the set up if this is the first time that we 
+		/// run this ICP configuration with these data.
+		/// </summary>
+		/// <param name="settings">Settings.</param>
 		private void SetUpForFirstTime(Settings settings)
 		{
 			CreateResultsDirectory(this.outputDirectory);
@@ -208,11 +282,21 @@ namespace Experiment
 
 			WriteHeaderToFile();
 		}
+
+		/// <summary>
+		/// This function handles the set up if we are continuing the experiment, 
+		/// i.e. the result direcotry already existst and has a csv file with 
+		/// termination errors for some of the fragments.
+		/// </summary>
 		private void SetUpForContinuation()
 		{
 			this.results = Results.FromFile(RunDataPath);
 		}
 
+		/// <summary>
+		/// Execute the experiment.
+		/// </summary>
+		/// <returns>The execute.</returns>
 		public IEnumerator<object> Execute()
 		{
 			RunExecuter executer = new RunExecuter(listener: this.gameObject, staticFragment: staticFragment,
@@ -253,6 +337,9 @@ namespace Experiment
 				options: SendMessageOptions.RequireReceiver);
 		}
 
+		/// <summary>
+		/// Writes the time stamp to run data file.
+		/// </summary>
 		private void WriteTimeStampToRunDataFile()
 		{
 			string timestamp = string.Format(
@@ -262,6 +349,9 @@ namespace Experiment
 			WriteToRunDataFile(timestamp, append: true);
 		}
 
+		/// <summary>
+		/// Writes the header to file.
+		/// </summary>
 		private void WriteHeaderToFile()
 		{
 			WriteToRunDataFile(
@@ -273,6 +363,11 @@ namespace Experiment
 			);
 		}
 
+		/// <summary>
+		/// Writes to run data to file.
+		/// </summary>
+		/// <param name="line">Line.</param>
+		/// <param name="append">If set to <c>true</c> append.</param>
 		private void WriteToRunDataFile(string line, bool append)
 		{
 			//Close the stream after every write to handle a not so gracious shut down
@@ -283,13 +378,32 @@ namespace Experiment
 		}
 
 		#region ICPInterface
+		/// <summary>
+		/// Listener that is triggered when the preparation step of ICP has been
+		///  completed. No need to do anything.
+		/// </summary>
+		/// <param name="message">Message.</param>
 		public void OnPreparationStepCompleted(ICPPreparationStepCompletedMessage message) { }
 
+		/// <summary>
+		/// Listener that is triggered when a ICP step has been completed. No 
+		/// need to do anything.
+		/// </summary>
+		/// <param name="message">Message.</param>
 		public void OnStepCompleted(ICPStepCompletedMessage message) { }
 
+		/// <summary>
+		/// Listener that is triggerd when the ICP algorithm has completed its 
+		/// current run. I.e. given the current settings it has registered the 
+		/// modelfragment to the static fragment.
+		/// 
+		/// If we are actually running the experiment store the results and write 
+		/// the results to the results file.
+		/// </summary>
+		/// <param name="message">Message.</param>
 		public void OnICPTerminated(ICPTerminatedMessage message)
 		{
-			//This function is also called when we are not running an experiment for some reason
+			//This function is also called when we are not running an experiment
 			if (results == null) return;
 
 			results.AddResult(message);
@@ -323,11 +437,21 @@ namespace Experiment
 				this.ID = jsonConfiguration.id;
 			}
 
+			/// <summary>
+			/// Given a relative path return the absolute path.
+			/// </summary>
+			/// <returns>The absolute path.</returns>
+			/// <param name="relativePath">The relative path.</param>
 			public string RelativePathToAbsolute(string relativePath)
 			{
 				return Path.GetFullPath(Path.Combine(this.WorkingDirectory, relativePath));
 			}
 
+			/// <summary>
+			/// Read a configuration from json.
+			/// </summary>
+			/// <returns>The configuation.</returns>
+			/// <param name="path">The path of the json file</param>
 			public static Configuration FromJson(string path)
 			{
 				_Configuration json_config = _Configuration.FromJson(path);
