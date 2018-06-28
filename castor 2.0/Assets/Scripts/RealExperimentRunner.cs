c39064f39c3665f31f34c9dc6aa88bbd6bbbf43d
Laura Baakman <laurabaakman@gmail.com>
2018-06-28 11:07:02 +0200

    Read all fragments and gather fragmetns for pair creation

--- castor 2.0/Assets/Scripts/RealExperimentRunner.cs
+++ castor 2.0/Assets/Scripts/RealExperimentRunner.cs
@@ -4,12 +4,15 @@ using System;
 using IO;
 using System.Collections;
 using System.IO;
+using System.Collections.Generic;
 
 public class RealExperimentRunner : RTEditor.MonoSingletonBase<RealExperimentRunner>, IICPStartEndListener
 {
 	public GameObject FragmentsRoot;
 
 	private bool continueCoroutine = false;
+
+	private string inputDirectory;
 
 	private string outputDirectory;
 	public string OutputDirectory
@@ -38,20 +41,13 @@ public class RealExperimentRunner : RTEditor.MonoSingletonBase<RealExperimentRun
 
 	public void Run(string inputDirectory, string outputDirectory, string ICPMethod)
 	{
-		Debug.Log("Run!");
-
 		this.OutputDirectory = outputDirectory;
+		this.inputDirectory = inputDirectory;
 		this.exporter = new FragmentsExporter(FragmentsRoot, WroteCurrentConfigurationToFile);
 
 		RealExperimentLogger.Instance.CreateLogFile(outputDirectory);
 
-		LoadFragments(inputDirectory);
-
-		//GatherFragments();
-
-		//this.exporter.ExportFragments(
-		//	directory: Path.Combine(OutputDirectory, counter.ToString())
-		//);
+		ProcessFragmentsAndStartRegistration();
 	}
 
 	private void WroteCurrentConfigurationToFile(WriteResult result)
@@ -59,22 +55,37 @@ public class RealExperimentRunner : RTEditor.MonoSingletonBase<RealExperimentRun
 		RegisterNextPair();
 	}
 
-	private void LoadFragments(string inputDirectory)
+	private void ProcessFragmentsAndStartRegistration()
 	{
-		string[] objFiles = Directory.GetFiles(inputDirectory, "*.obj");
-		StartCoroutine(ImportFragments(objFiles));
+		StartCoroutine(ImportFragments((finished) =>
+		{
+			if (finished)
+			{
+				CreateFragmentPairs();
+				ExportFragments();
+				RegisterNextPair();
+			}
+		}));
 	}
 
-	private IEnumerator ImportFragments(string[] objFiles)
+	private IEnumerator ImportFragments(System.Action<bool> callback)
 	{
+		string[] objFiles = Directory.GetFiles(inputDirectory, "*.obj");
+		yield return null;
+		callback(false);
+
 		continueCoroutine = false;
 		IO.FragmentImporter importer = new IO.FragmentImporter(FragmentsRoot, ImportedFragment);
 		foreach (string currentFile in objFiles)
 		{
 			importer.Import(currentFile);
 			yield return new WaitUntil(() => this.continueCoroutine);
+			callback(false);
 		}
 		continueCoroutine = false;
+
+		yield return null;
+		callback(true);
 	}
 
 	private void ImportedFragment(IO.ReadResult result)
@@ -90,12 +101,33 @@ public class RealExperimentRunner : RTEditor.MonoSingletonBase<RealExperimentRun
 
 	private void CreateFragmentPairs()
 	{
-		throw new NotImplementedException();
+		List<GameObject> fragments = GetAllFragments();
+		foreach (GameObject fragment in fragments)
+		{
+			Debug.Log(fragment.name);
+		}
+	}
+
+	private List<GameObject> GetAllFragments()
+	{
+		List<GameObject> fragments = new List<GameObject>();
+		foreach (Transform child in FragmentsRoot.transform)
+			//Add only fragments, not the other children of 'Fragments'
+			if (child.GetComponent<MeshFilter>() != null) fragments.Add(child.gameObject);
+
+		return fragments;
 	}
 
 	private void RegisterNextPair()
 	{
 		throw new NotImplementedException();
+	}
+
+	private void ExportFragments()
+	{
+		this.exporter.ExportFragments(
+		  directory: Path.Combine(OutputDirectory, counter.ToString())
+		);
 	}
 
 	private void Terminate()
