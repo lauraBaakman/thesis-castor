d80d781c6ea8dcf71cad438c8032929f2d6d3723
Laura Baakman <laurabaakman@gmail.com>
2018-06-28 09:51:08 +0200

    Attatched Singleton to run the real experiment to a general singleton object

---
+++ castor 2.0/Assets/Scripts/RealExperimentRunner.cs
@@ -0,0 +1,53 @@
+﻿using Registration.Messages;
+using UnityEngine;
+using System;
+using IO;
+using System.Collections;
+
+public class RealExperimentRunner : RTEditor.MonoSingletonBase<RealExperimentRunner>, IICPStartEndListener
+{
+	public void OnICPStarted(ICPStartedMessage message)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void OnICPTerminated(ICPTerminatedMessage message)
+	{
+		throw new NotImplementedException();
+	}
+
+
+	private void Terminate()
+	{
+		throw new NotImplementedException();
+	}
+
+	IEnumerator Start()
+	{
+		Debug.Log("Start before yield");
+		yield return new WaitForSeconds(5);
+		Debug.Log("Start after yield");
+	}
+
+	private void DoNothing(WriteResult result) { }
+
+	public void Run(string inputDirectory, string outputDirectory)
+	{
+		throw new NotImplementedException();
+	}
+
+	private void RegisterTopPair()
+	{
+		throw new NotImplementedException();
+	}
+
+	private void LoadFragments(string inputDirectory)
+	{
+		throw new NotImplementedException();
+	}
+
+	private void GatherFragments()
+	{
+		throw new NotImplementedException();
+	}
+}
